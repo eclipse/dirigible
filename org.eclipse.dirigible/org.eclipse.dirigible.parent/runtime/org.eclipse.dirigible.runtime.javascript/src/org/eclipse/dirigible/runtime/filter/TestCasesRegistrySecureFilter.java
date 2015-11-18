81bebf5e352172e270579dfae7db23040f3154e0
delchev <delchevn@gmail.com>
2015-11-18 19:47:14 +0200

    Extract JavaScript related functionality (Rhino based) to a separated plugin https://bugs.eclipse.org/bugs/show_bug.cgi?id=482494

---
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.javascript/src/org/eclipse/dirigible/runtime/filter/TestCasesRegistrySecureFilter.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2015 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * SAP - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.dirigible.runtime.filter;
+
+public class TestCasesRegistrySecureFilter extends AbstractRegistrySecureFilter {
+
+	private static final String WEB_SECURED_MAPPING = "/services/test-secured"; //$NON-NLS-1$
+
+	@Override
+	protected String getSecuredMapping() {
+		return WEB_SECURED_MAPPING;
+	}
+
+}
--- org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.web/src/org/eclipse/dirigible/runtime/web/WebExecutor.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.web/src/org/eclipse/dirigible/runtime/web/WebExecutor.java
@@ -1,12 +1,11 @@
-/******************************************************************************* 
+/*******************************************************************************
  * Copyright (c) 2015 SAP and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0 
- * which accompanies this distribution, and is available at 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
  * Contributors:
- *   SAP - initial API and implementation
+ * SAP - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.dirigible.runtime.web;
@@ -23,12 +22,11 @@ import org.eclipse.dirigible.repository.api.IEntity;
 import org.eclipse.dirigible.repository.api.IRepository;
 import org.eclipse.dirigible.repository.api.IResource;
 import org.eclipse.dirigible.repository.logging.Logger;
-import org.eclipse.dirigible.runtime.js.RepositoryModuleSourceProvider;
 import org.eclipse.dirigible.runtime.scripting.AbstractScriptExecutor;
 import org.eclipse.dirigible.runtime.scripting.Module;
 
 public class WebExecutor extends AbstractScriptExecutor {
-	
+
 	private static final Logger logger = Logger.getLogger(WebExecutor.class);
 
 	private IRepository repository;
@@ -39,58 +37,50 @@ public class WebExecutor extends AbstractScriptExecutor {
 		logger.debug("entering: constructor()");
 		this.repository = repository;
 		this.rootPaths = rootPaths;
-		if (this.rootPaths == null || this.rootPaths.length == 0) {
+		if ((this.rootPaths == null) || (this.rootPaths.length == 0)) {
 			this.rootPaths = new String[] { null, null };
 		}
 		logger.debug("exiting: constructor()");
 	}
 
 	@Override
-	public Object executeServiceModule(HttpServletRequest request, HttpServletResponse response,
-			Object input, String module, Map<Object, Object> executionContext) throws IOException {
+	public Object executeServiceModule(HttpServletRequest request, HttpServletResponse response, Object input, String module,
+			Map<Object, Object> executionContext) throws IOException {
 
 		logger.debug("entering: executeServiceModule()"); //$NON-NLS-1$
 		logger.debug("module=" + module); //$NON-NLS-1$
-		
+
 		if (module == null) {
 			throw new IOException("Web module cannot be null");
 		}
 
 		Module scriptingModule = retrieveModule(this.repository, module, null, this.rootPaths);
 		byte[] result = scriptingModule.getContent();
-		
+
 		result = preprocessContent(result, getResource(repository, scriptingModule.getPath()));
-		
+
 		response.getWriter().print(new String(result));
 		response.getWriter().flush();
 		logger.debug("exiting: executeServiceModule()");
 		return result;
 	}
-	
+
 	protected byte[] preprocessContent(byte[] rawContent, IEntity entity) throws IOException {
 		return rawContent;
 	}
-	
-	protected byte[] buildResourceData(final IEntity entity, final HttpServletRequest request,
-			final HttpServletResponse response) throws IOException {
+
+	protected byte[] buildResourceData(final IEntity entity, final HttpServletRequest request, final HttpServletResponse response)
+			throws IOException {
 		byte[] data = new byte[] {};
 		data = readResourceData((IResource) entity);
 		return data;
 	}
-	
+
 	protected byte[] readResourceData(IResource resource) throws IOException {
 		return resource.getContent();
 	}
 
-	protected void beforeExecution(HttpServletRequest request, HttpServletResponse response,
-			String module, Context context) {
-	}
-
-	private RepositoryModuleSourceProvider createRepositoryModuleSourceProvider() {
-		RepositoryModuleSourceProvider repositoryModuleSourceProvider = null;
-		repositoryModuleSourceProvider = new RepositoryModuleSourceProvider(this, repository,
-				rootPaths);
-		return repositoryModuleSourceProvider;
+	protected void beforeExecution(HttpServletRequest request, HttpServletResponse response, String module, Context context) {
 	}
 
 	@Override
