6ca92b3ea859db1d999acad675e00386bcaf15e6
delchev <delchevn@gmail.com>
2015-11-22 09:51:20 +0200

    Added String based generate method to Velocity based generator https://bugs.eclipse.org/bugs/show_bug.cgi?id=482502

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/ide/org.eclipse.dirigible.ide.template.velocity/src/org/eclipse/dirigible/ide/template/velocity/VelocityGenerator.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/ide/org.eclipse.dirigible.ide.template.velocity/src/org/eclipse/dirigible/ide/template/velocity/VelocityGenerator.java
@@ -1,12 +1,11 @@
-/******************************************************************************* 
+/*******************************************************************************
  * Copyright (c) 2015 SAP and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0 
- * which accompanies this distribution, and is available at 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
  * Contributors:
- *   SAP - initial API and implementation
+ * SAP - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.dirigible.ide.template.velocity;
@@ -17,6 +16,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Iterator;
 import java.util.Map;
@@ -25,7 +26,7 @@ import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 
 public class VelocityGenerator {
-	
+
 	private static final String COULD_NOT_EVALUATE_TEMPLATE = Messages.getString("VelocityGenerator.COULD_NOT_EVALUATE_TEMPLATE"); //$NON-NLS-1$
 	private VelocityEngine engine;
 
@@ -34,13 +35,25 @@ public class VelocityGenerator {
 		try {
 			engine.init();
 		} catch (Throwable e) {
-//			logger.error(e.getMessage(), e);
+			// logger.error(e.getMessage(), e);
 			e.printStackTrace();
 		}
 	}
 
-	public void generate(Reader in, Writer out, Map<String, Object> parameters,
-			String tag) throws VelocityGeneratorException {
+	/**
+	 * Produce a generated content based on a template and input parameters
+	 *
+	 * @param in
+	 *            the template reader
+	 * @param out
+	 *            the result as Writer
+	 * @param parameters
+	 *            input parameters
+	 * @param tag
+	 *            logging tag
+	 * @throws VelocityGeneratorException
+	 */
+	public void generate(Reader in, Writer out, Map<String, Object> parameters, String tag) throws VelocityGeneratorException {
 		try {
 			final VelocityContext context = new VelocityContext();
 			prepareContextData(parameters, context);
@@ -59,9 +72,20 @@ public class VelocityGenerator {
 		}
 	}
 
-	public void generate(InputStream in, OutputStream out,
-			Map<String, Object> parameters, String tag)
-			throws VelocityGeneratorException {
+	/**
+	 * Produce a generated content based on a template and input parameters
+	 *
+	 * @param in
+	 *            the template input stream
+	 * @param out
+	 *            the result as OutputStream
+	 * @param parameters
+	 *            input parameters
+	 * @param tag
+	 *            logging tag
+	 * @throws VelocityGeneratorException
+	 */
+	public void generate(InputStream in, OutputStream out, Map<String, Object> parameters, String tag) throws VelocityGeneratorException {
 		try {
 
 			final VelocityContext context = new VelocityContext();
@@ -73,8 +97,7 @@ public class VelocityGenerator {
 			writer.close();
 		} catch (Exception ex) {
 			ex.printStackTrace();
-			throw new VelocityGeneratorException(COULD_NOT_EVALUATE_TEMPLATE,
-					ex);
+			throw new VelocityGeneratorException(COULD_NOT_EVALUATE_TEMPLATE, ex);
 		} finally {
 			try {
 				in.close();
@@ -85,13 +108,47 @@ public class VelocityGenerator {
 		}
 	}
 
-	private void prepareContextData(Map<String, Object> parameters,
-			VelocityContext context) {
-		Iterator<Map.Entry<String, Object>> iterator = parameters.entrySet()
-				.iterator();
+	/**
+	 * Produce a generated content based on a template and input parameters
+	 *
+	 * @param in
+	 *            the template String
+	 * @param parameters
+	 *            input parameters
+	 * @param tag
+	 *            logging tag
+	 * @return the result as String
+	 * @throws VelocityGeneratorException
+	 */
+	public String generate(String in, Map<String, Object> parameters, String tag) throws VelocityGeneratorException {
+		String result = "";
+		try {
+			final VelocityContext context = new VelocityContext();
+			prepareContextData(parameters, context);
+			final StringReader reader = new StringReader(in);
+			final StringWriter writer = new StringWriter();
+			engine.evaluate(context, writer, tag, reader);
+			writer.flush();
+			StringBuffer buff = writer.getBuffer();
+			result = buff.toString();
+			writer.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			throw new VelocityGeneratorException(COULD_NOT_EVALUATE_TEMPLATE, ex);
+		}
+		return result;
+	}
+
+	/**
+	 * Put the input parameters to the Velocity Context for processing
+	 * 
+	 * @param parameters
+	 * @param context
+	 */
+	private void prepareContextData(Map<String, Object> parameters, VelocityContext context) {
+		Iterator<Map.Entry<String, Object>> iterator = parameters.entrySet().iterator();
 		while (iterator.hasNext()) {
-			Map.Entry<String, Object> entry = (Map.Entry<String, Object>) iterator
-					.next();
+			Map.Entry<String, Object> entry = iterator.next();
 			context.put(entry.getKey(), entry.getValue());
 		}
 	}
