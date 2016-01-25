87062cd4aa648d431b29dfc25c846a49c19094cc
ThuF <jordan_p_pavlov@abv.bg>
2016-01-25 18:22:57 +0200

    Adds "MobileApplications" template

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/ide/org.eclipse.dirigible.ide.repository.ui/src/org/eclipse/dirigible/ide/repository/ui/viewer/ArtifactLabelProvider.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/ide/org.eclipse.dirigible.ide.repository.ui/src/org/eclipse/dirigible/ide/repository/ui/viewer/ArtifactLabelProvider.java
@@ -1,22 +1,20 @@
-/******************************************************************************* 
+/*******************************************************************************
  * Copyright (c) 2015 SAP and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0 
- * which accompanies this distribution, and is available at 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
  * Contributors:
- *   SAP - initial API and implementation
+ * SAP - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.dirigible.ide.repository.ui.viewer;
-
-import org.eclipse.swt.graphics.Image;
 
 import org.eclipse.dirigible.repository.api.ICollection;
 import org.eclipse.dirigible.repository.api.ICommonConstants;
 import org.eclipse.dirigible.repository.api.IEntity;
 import org.eclipse.dirigible.repository.api.IResource;
+import org.eclipse.swt.graphics.Image;
 
 public class ArtifactLabelProvider extends AbstractArtifactLabelProvider {
 
@@ -55,8 +53,7 @@ public class ArtifactLabelProvider extends AbstractArtifactLabelProvider {
 
 	protected Image getEntityImage(IEntity entity) {
 		if (entity instanceof IResource) {
-			final String resourceName = ((IResource) entity).getName()
-					.toLowerCase();
+			final String resourceName = ((IResource) entity).getName().toLowerCase();
 			return getResourceImage(resourceName);
 		}
 		if (entity instanceof ICollection) {
@@ -79,27 +76,22 @@ public class ArtifactLabelProvider extends AbstractArtifactLabelProvider {
 	protected Image getCollectionImageByName(String name) {
 		if (name.equals(ICommonConstants.ARTIFACT_TYPE.DATA_STRUCTURES)) {
 			return createImage(TYPE_COLLECTION_DS_ICON_URL);
-		}
-		if (name.equals(ICommonConstants.ARTIFACT_TYPE.EXTENSION_DEFINITIONS)) {
+		} else if (name.equals(ICommonConstants.ARTIFACT_TYPE.EXTENSION_DEFINITIONS)) {
 			return createImage(TYPE_COLLECTION_EXT_ICON_URL);
-		}
-		if (name.equals(ICommonConstants.ARTIFACT_TYPE.INTEGRATION_SERVICES)) {
+		} else if (name.equals(ICommonConstants.ARTIFACT_TYPE.INTEGRATION_SERVICES)) {
 			return createImage(TYPE_COLLECTION_IS_ICON_URL);
-		}
-		if (name.equals(ICommonConstants.ARTIFACT_TYPE.SCRIPTING_SERVICES)) {
+		} else if (name.equals(ICommonConstants.ARTIFACT_TYPE.SCRIPTING_SERVICES)) {
 			return createImage(TYPE_COLLECTION_SS_ICON_URL);
-		}
-		if (name.equals(ICommonConstants.ARTIFACT_TYPE.SECURITY_CONSTRAINTS)) {
+		} else if (name.equals(ICommonConstants.ARTIFACT_TYPE.SECURITY_CONSTRAINTS)) {
 			return createImage(TYPE_COLLECTION_SEC_ICON_URL);
-		}
-		if (name.equals(ICommonConstants.ARTIFACT_TYPE.TEST_CASES)) {
+		} else if (name.equals(ICommonConstants.ARTIFACT_TYPE.TEST_CASES)) {
 			return createImage(TYPE_COLLECTION_TEST_ICON_URL);
-		}
-		if (name.equals(ICommonConstants.ARTIFACT_TYPE.WEB_CONTENT)) {
+		} else if (name.equals(ICommonConstants.ARTIFACT_TYPE.WEB_CONTENT)) {
 			return createImage(TYPE_COLLECTION_WEB_ICON_URL);
-		}
-		if (name.equals(ICommonConstants.ARTIFACT_TYPE.WIKI_CONTENT)) {
+		} else if (name.equals(ICommonConstants.ARTIFACT_TYPE.WIKI_CONTENT)) {
 			return createImage(TYPE_COLLECTION_WIKI_ICON_URL);
+		} else if (name.equals(ICommonConstants.ARTIFACT_TYPE.MOBILE_APPLICATIONS)) {
+			return createImage(TYPE_COLLECTION_MOBILE_ICON_URL);
 		}
 		return createImage(TYPE_COLLECTION_ICON_URL);
 	}
