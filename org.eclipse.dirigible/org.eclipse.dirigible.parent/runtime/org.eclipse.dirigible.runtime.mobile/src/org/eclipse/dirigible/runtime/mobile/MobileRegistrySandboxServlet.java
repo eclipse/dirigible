2dd1a7c1b29d6343efa697642fee42ef2f0aada1
ThuF <jordan_p_pavlov@abv.bg>
2016-01-26 10:48:38 +0200

    Adds "Runtime - Mobile" plugin

---
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.mobile/src/org/eclipse/dirigible/runtime/mobile/MobileRegistrySandboxServlet.java
@@ -0,0 +1,18 @@
+/******************************************************************************* 
+ * Copyright (c) 2015 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   SAP - initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.dirigible.runtime.mobile;
+
+public class MobileRegistrySandboxServlet extends MobileRegistryServlet {
+
+	private static final long serialVersionUID = -4753993860109765189L;
+
+}
