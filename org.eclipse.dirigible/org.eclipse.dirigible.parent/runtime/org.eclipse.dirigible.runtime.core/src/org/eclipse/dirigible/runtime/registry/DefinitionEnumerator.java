08e44134febca220a29fee36f28e8317662d6120
delchev <delchevn@gmail.com>
2016-10-25 11:59:56 +0300

    tests enumerator fix

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.core/src/org/eclipse/dirigible/runtime/registry/DefinitionEnumerator.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.core/src/org/eclipse/dirigible/runtime/registry/DefinitionEnumerator.java
@@ -1,12 +1,11 @@
-/******************************************************************************* 
+/*******************************************************************************
  * Copyright (c) 2015 SAP and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0 
- * which accompanies this distribution, and is available at 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
  * Contributors:
- *   SAP - initial API and implementation
+ * SAP - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.dirigible.runtime.registry;
@@ -22,9 +21,8 @@ import org.eclipse.dirigible.repository.api.IResource;
 public class DefinitionEnumerator {
 
 	private final List<String> list = new ArrayList<String>();
-	
-	public DefinitionEnumerator(final String repositoryPath, final ICollection collection,
-			final String fileExtension) throws IOException {
+
+	public DefinitionEnumerator(final String repositoryPath, final ICollection collection, final String fileExtension) throws IOException {
 		enumerateJsDefinitions(repositoryPath, collection, fileExtension);
 	}
 
@@ -32,16 +30,15 @@ public class DefinitionEnumerator {
 		return list;
 	}
 
-	private void enumerateJsDefinitions(final String repositoryPath, final ICollection collection,
-			final String fileExtension) throws IOException {
+	private void enumerateJsDefinitions(final String repositoryPath, final ICollection collection, final String fileExtension) throws IOException {
 		if (collection.exists()) {
-			for (final IResource resource : collection.getResources()) {
-				if (resource != null && resource.getName() != null) {
+			List<IResource> resources = collection.getResources();
+			for (final IResource resource : resources) {
+				if ((resource != null) && (resource.getName() != null)) {
 					if (resource.getName().endsWith(fileExtension)) {
 						String collecationPath = collection.getPath();
 						if (collecationPath.length() >= repositoryPath.length()) {
-							final String fullPath = collecationPath.substring(repositoryPath.length())
-									+ IRepository.SEPARATOR + resource.getName();
+							final String fullPath = collecationPath.substring(repositoryPath.length()) + IRepository.SEPARATOR + resource.getName();
 							this.list.add(fullPath);
 						}
 					}
--- org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.javascript/src/org/eclipse/dirigible/runtime/registry/TestCasesRegistryServlet.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.javascript/src/org/eclipse/dirigible/runtime/registry/TestCasesRegistryServlet.java
@@ -1,37 +1,39 @@
-/******************************************************************************* 
+/*******************************************************************************
  * Copyright (c) 2015 SAP and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0 
- * which accompanies this distribution, and is available at 
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
  * Contributors:
- *   SAP - initial API and implementation
+ * SAP - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.dirigible.runtime.registry;
 
+import org.eclipse.dirigible.repository.api.ICommonConstants;
+import org.eclipse.dirigible.repository.api.IRepository;
+
 public class TestCasesRegistryServlet extends AbstractRegistryServiceServlet {
 
-    private static final long serialVersionUID = 7634737455357411422L;
+	private static final long serialVersionUID = 7634737455357411422L;
 
-    @Override
-    protected String getServicesFolder() {
-        return "/TestCases/";
-    }
+	@Override
+	protected String getServicesFolder() {
+		return ICommonConstants.ARTIFACT_TYPE.TEST_CASES + IRepository.SEPARATOR;
+	}
 
-    @Override
-    protected String getServletMapping() {
-        return "/test/";
-    }
+	@Override
+	protected String getServletMapping() {
+		return "/test/";
+	}
 
-    @Override
-    protected String getFileExtension() {
-        return ".js";
-    }
+	@Override
+	protected String getFileExtension() {
+		return ".js";
+	}
 
-    @Override
-    protected String getRequestProcessingFailedMessage() {
-        return Messages.getString("TestCasesRegistryServlet.REQUEST_PROCESSING_FAILED_S");
-    }
+	@Override
+	protected String getRequestProcessingFailedMessage() {
+		return Messages.getString("TestCasesRegistryServlet.REQUEST_PROCESSING_FAILED_S");
+	}
 }
