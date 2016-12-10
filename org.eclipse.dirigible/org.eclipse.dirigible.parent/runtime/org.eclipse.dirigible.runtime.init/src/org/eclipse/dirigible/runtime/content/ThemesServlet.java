35a922fd5aeba066078be8ce14ec3e3682444f97
delchev <delchevn@gmail.com>
2016-12-10 20:53:50 +0200

    fixed: https://hudson.eclipse.org/dirigible/job/dirigible-nightly/638/analysisResult/NORMAL/module.1534279339/file.1199148866/source.535/#30

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.init/src/org/eclipse/dirigible/runtime/content/ThemesServlet.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.init/src/org/eclipse/dirigible/runtime/content/ThemesServlet.java
@@ -8,17 +8,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.eclipse.dirigible.repository.api.ICommonConstants;
 import org.eclipse.dirigible.repository.logging.Logger;
 
 public class ThemesServlet extends HttpServlet {
+
+	private static final String NAME_PARAM = "name"; //$NON-NLS-1$
+	private static final String DEFAULT_THEME = "default"; //$NON-NLS-1$
+	private static final String SLASH = "/"; //$NON-NLS-1$
 	private static final long serialVersionUID = 3669759124907091014L;
+
 	private static final Logger logger = Logger.getLogger(ThemesServlet.class);
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String cookieValue = "default";
-		String themName = req.getParameter("name");
+		String cookieValue = DEFAULT_THEME;
+		String themName = req.getParameter(NAME_PARAM);
+		themName = StringEscapeUtils.escapeHtml(themName);
+		themName = StringEscapeUtils.escapeJavaScript(themName);
 		if ((null != themName) && !themName.isEmpty()) {
 			themName = themName.trim();
 
@@ -37,6 +45,8 @@ public class ThemesServlet extends HttpServlet {
 				}
 			}
 		}
+		cookieValue = StringEscapeUtils.escapeHtml(cookieValue);
+		cookieValue = StringEscapeUtils.escapeJavaScript(cookieValue);
 		resp.getWriter().println(cookieValue.trim());
 		resp.getWriter().flush();
 		resp.getWriter().close();
@@ -45,7 +55,7 @@ public class ThemesServlet extends HttpServlet {
 	private void setCookieUser(HttpServletResponse resp, String themeName) {
 		Cookie cookie = new Cookie(ICommonConstants.COOKIE_THEME, themeName);
 		cookie.setMaxAge(30 * 24 * 60 * 60);
-		cookie.setPath("/");
+		cookie.setPath(SLASH);
 		resp.addCookie(cookie);
 	}
 
