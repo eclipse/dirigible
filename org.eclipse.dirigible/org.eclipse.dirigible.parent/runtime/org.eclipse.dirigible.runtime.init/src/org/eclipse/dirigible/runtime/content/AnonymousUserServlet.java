ef602d11a01c018a0cf3122927683e2a70070b3b
delchev <delchevn@gmail.com>
2016-12-10 20:47:43 +0200

    fixed: https://hudson.eclipse.org/dirigible/job/dirigible-nightly/638/analysisResult/NORMAL/module.1534279339/file.-646849706/type.679852700/source.498/#56

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.init/src/org/eclipse/dirigible/runtime/content/AnonymousUserServlet.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.init/src/org/eclipse/dirigible/runtime/content/AnonymousUserServlet.java
@@ -10,25 +10,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringEscapeUtils;
 import org.eclipse.dirigible.repository.api.ICommonConstants;
 import org.eclipse.dirigible.repository.logging.Logger;
 
 public class AnonymousUserServlet extends HttpServlet {
+
+	private static final String INVALID_USER_NAME_S = "Invalid user name: %s"; //$NON-NLS-1$
+	private static final String USER_PARAM = "user"; //$NON-NLS-1$
+	private static final String INVALID_COOKIE = "Invalid cookie."; //$NON-NLS-1$
+	private static final String USE_ANONYMOUS_USER_XXX = "Use .../anonymous?user=xxx"; //$NON-NLS-1$
+	private static final String SLASH = "/"; //$NON-NLS-1$
+	private static final String REDIRECT_REGISTRY = "web/registry/index.html"; //$NON-NLS-1$
+	private static final String REDIRECT_IDE = "index.html?perspective=workspace&git="; //$NON-NLS-1$
+	private static final String GIT_PARAM = "git"; //$NON-NLS-1$
+	private static final String REDIRECT_PARAM = "redirect"; //$NON-NLS-1$
 	private static final long serialVersionUID = 3669759124907091014L;
 	private static final Logger logger = Logger.getLogger(AnonymousUserServlet.class);
-	private static final String REGEX_USER_NAME = "[A-Za-z0-9_]{3,64}";
+	private static final String REGEX_USER_NAME = "[A-Za-z0-9_]{3,64}"; //$NON-NLS-1$
 	private static final Pattern PATTERN_USER_NAME = Pattern.compile(REGEX_USER_NAME);
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String cookieValue = null;
-		String userName = req.getParameter("user");
+		String userName = req.getParameter(USER_PARAM);
+		userName = StringEscapeUtils.escapeHtml(userName);
+		userName = StringEscapeUtils.escapeJavaScript(userName);
 		if ((null != userName) && !userName.isEmpty()) {
 			userName = userName.trim();
 			final Matcher userNameMatcher = PATTERN_USER_NAME.matcher(userName);
 			if (!userNameMatcher.matches()) {
-				logger.error("The provided username <" + userName + "> is invalid. It must match: " + REGEX_USER_NAME);
-				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid user name: " + userName);
+				logger.error(String.format("The provided username <%s> is invalid. It must match: %s", userName, REGEX_USER_NAME));
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, String.format(INVALID_USER_NAME_S, userName));
 				return;
 			}
 			// if there is valid user name, then force the setting of the cookie
@@ -48,24 +61,26 @@ public class AnonymousUserServlet extends HttpServlet {
 			if (cookieValue == null) {
 				// no cookie and no parameter, then fail
 				logger.error("User has not been provided neither as cookie nor as parameter");
-				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Use .../anonymous?user=xxx");
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, USE_ANONYMOUS_USER_XXX);
 				return;
 			}
 
 			final Matcher cookieUserNameMatcher = PATTERN_USER_NAME.matcher(cookieValue);
 			if (!cookieUserNameMatcher.matches()) {
 				logger.error("Invalid user name was provided as cookie: " + cookieValue);
-				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid cookie.");
+				resp.sendError(HttpServletResponse.SC_BAD_REQUEST, INVALID_COOKIE);
 				return;
 			}
 		}
-		String redirect = req.getParameter("redirect");
+		String redirect = req.getParameter(REDIRECT_PARAM);
 		if ((redirect != null) && !"".equals(redirect.trim())) {
-			String git = req.getParameter("git");
+			String git = req.getParameter(GIT_PARAM);
 			if ((git != null) && !"".equals(git.trim())) {
-				resp.sendRedirect("index.html?perspective=workspace&git=" + git);
+				git = StringEscapeUtils.escapeHtml(git);
+				git = StringEscapeUtils.escapeJavaScript(git);
+				resp.sendRedirect(REDIRECT_IDE + git);
 			} else {
-				resp.sendRedirect("web/registry/index.html");
+				resp.sendRedirect(REDIRECT_REGISTRY);
 			}
 		} else {
 			resp.getWriter().println(cookieValue);
@@ -77,7 +92,7 @@ public class AnonymousUserServlet extends HttpServlet {
 	private void setCookieUser(HttpServletResponse resp, String userName) {
 		Cookie cookie = new Cookie(ICommonConstants.COOKIE_ANONYMOUS_USER, userName);
 		cookie.setMaxAge(30 * 24 * 60 * 60);
-		cookie.setPath("/");
+		cookie.setPath(SLASH);
 		resp.addCookie(cookie);
 	}
 
