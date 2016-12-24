58356d48897a8a0794c73b5cec00ae37a6d9d8dc
delchev <delchevn@gmail.com>
2016-12-24 15:52:34 +0200

    all in the bash script https://bugs.eclipse.org/bugs/show_bug.cgi?id=509701

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.content/git-checkout.sh
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.content/git-checkout.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# This script is the part of integration GIT to ANT. Once launched it 
-# should return the name of the current branch or the current commit (if 
-# GIT is the detached HEAD mode). Further the printed name is appended to 
-# the name of the resulting directory. To initialize this feature you need 
-# to run ANT with the option "-Dusing.git=". 
+#exec 2>/dev/null
 
-exec 2>/dev/null
+# template_web_new_or_edit
+if cd target/template_web_new_or_edit; then git pull; cd ../..; else git clone https://github.com/dirigiblelabs/template_web_new_or_edit.git target/template_web_new_or_edit; fi
+cp -r target/template_web_new_or_edit/template_web_new_or_edit/WebContentForEntity src/content/db/dirigible/templates
 
-if cd target/template_web_new_or_edit; then git pull; else git clone https://github.com/dirigiblelabs/template_web_new_or_edit.git target/template_web_new_or_edit; fi
+# zip the content
+cd src/content/db/
+zip -r ../../../content.zip *
+
+# copy to the init plugin
+cd ../../..
+cp content.zip ../org.eclipse.dirigible.runtime.init/src
