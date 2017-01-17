833786c421792419be26dcfa657b368467b57a41
delchev <delchevn@gmail.com>
2017-01-17 18:11:31 +0200

    url connection parameters at onOpen

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/bridge/org.eclipse.dirigible.bridge/src/org/eclipse/dirigible/ide/bridge/WebSocketTunnel.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/bridge/org.eclipse.dirigible.bridge/src/org/eclipse/dirigible/ide/bridge/WebSocketTunnel.java
@@ -23,6 +23,7 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.WebSocketContainer;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 import org.slf4j.Logger;
@@ -31,13 +32,11 @@ import org.slf4j.LoggerFactory;
 /**
  * Web Socket Tunnel Endpoint
  * // connect to the dirigible tunnelling endpoint
- * var wsSocket = new WebSocket("ws://dirigible-host:8080/tunnel");
- * // send initial command to connect the external url
- * wsSocket.send('ws://external-host:8080/xxx');
+ * var wsSocket = new WebSocket("ws://dirigible-host:8080/tunnel/ws/example.com/8080/myservice");
  * // continue the work as the external endpoint requires
  * wsSocket.send('xxx...');
  */
-@ServerEndpoint("/tunnel")
+@ServerEndpoint("/tunnel/{protocol}/{host}/{port}/{path}")
 public class WebSocketTunnel {
 
 	private static final String PARAM_CLIENT = "client";
@@ -47,8 +46,21 @@ public class WebSocketTunnel {
 	static Map<String, Session> INBOUND_SESSIONS = Collections.synchronizedMap(new HashMap<String, Session>());
 
 	@OnOpen
-	public void onOpen(Session session) throws IOException {
-		session.getBasicRemote().sendText("send the url to the websocket you want to connect to e.g. ws://localhost:8080/mywebsocket");
+	public void onOpen(Session session, @PathParam("protocol") String protocol, @PathParam("host") String host, @PathParam("port") String port,
+			@PathParam("path") String path) throws IOException {
+		try {
+			Session client;
+			WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+			String uri = protocol + "://" + host + ":" + port + "/" + path.replace("~", "/");
+			logger.debug("Connecting to " + uri);
+			client = container.connectToServer(WebSocketTunnelClient.class, URI.create(uri));
+			session.getUserProperties().put(PARAM_CLIENT, client);
+			INBOUND_SESSIONS.put(client.getId(), session);
+			logger.debug("Connected to " + uri);
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			session.getBasicRemote().sendText(e.getMessage());
+		}
 	}
 
 	@OnMessage
@@ -61,17 +73,7 @@ public class WebSocketTunnel {
 				logger.error(e.getMessage(), e);
 			}
 		} else {
-			try {
-				WebSocketContainer container = ContainerProvider.getWebSocketContainer();
-				String uri = message;
-				logger.debug("Connecting to " + uri);
-				client = container.connectToServer(WebSocketTunnelClient.class, URI.create(uri));
-				session.getUserProperties().put(PARAM_CLIENT, client);
-				INBOUND_SESSIONS.put(client.getId(), session);
-				logger.debug("Connected to " + uri);
-			} catch (Exception e) {
-				logger.error(e.getMessage(), e);
-			}
+
 		}
 	}
 
