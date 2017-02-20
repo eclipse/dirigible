8cce439dd67672a743501edd7ea726801e4fd5b8
delchev <delchevn@gmail.com>
2017-02-20 13:03:07 +0200

    non argument fix for console

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.core/src/org/eclipse/dirigible/runtime/scripting/Console.java
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.core/src/org/eclipse/dirigible/runtime/scripting/Console.java
@@ -7,27 +7,50 @@ public class Console {
 	private static Logger logger = Logger.getLogger(Console.class);
 
 	public void error(String message, Object... args) {
-		logger.error(String.format(message, args));
+		if (args.length > 0) {
+			logger.error(String.format(message, args));
+		} else {
+			logger.error(message);
+		}
 	}
 
 	public void info(String message, Object... args) {
-		logger.info(String.format(message, args));
+		if (args.length > 0) {
+			logger.info(String.format(message, args));
+		} else {
+			logger.info(message);
+		}
 	}
 
 	public void log(String message, Object... args) {
-		logger.info(String.format(message, args));
+		logger.info(message);
+		for (Object arg : args) {
+			logger.info(arg != null ? arg.toString() : null);
+		}
 	}
 
 	public void warn(String message, Object... args) {
-		logger.warn(String.format(message, args));
+		if (args.length > 0) {
+			logger.warn(String.format(message, args));
+		} else {
+			logger.warn(message);
+		}
 	}
 
 	public void debug(String message, Object... args) {
-		logger.debug(String.format(message, args));
+		if (args.length > 0) {
+			logger.debug(String.format(message, args));
+		} else {
+			logger.debug(message);
+		}
 	}
 
 	public void trace(String message, Object... args) {
-		logger.error(String.format(message, args));
+		if (args.length > 0) {
+			logger.error(String.format(message, args));
+		} else {
+			logger.error(message);
+		}
 		StringBuilder buff = new StringBuilder();
 		for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
 			buff.append(element.toString()).append(System.getProperty("line.separator"));
