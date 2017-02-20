b87e2c5a3a4b41048c68ca39dec9d719353c3909
ThuF <jordan_p_pavlov@abv.bg>
2017-02-20 11:08:22 +0200

    Introduce structured Registry content update

---
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.content/updateRegistryContent.py
@@ -0,0 +1,23 @@
+import json
+import os
+
+class RegistryContent:
+    def update(self):
+        self.updateContent('content/api.json')
+        self.updateContent('content/registry.json')
+        self.updateContent('content/misc.json')
+        self.updateContent('content/templates/ScriptingServices.json')
+        self.updateContent('content/templates/WebContent.json')
+        self.updateContent('content/templates/WebContentForEntity.json')
+
+    def updateContent(self, fileName):
+		with open(fileName) as data_file:
+		    templates = json.load(data_file)
+
+		for i in range(0, len(templates)):
+		    os.system("if cd " + templates[i]['path'] + "; then git pull; cd ../..; else git clone " + templates[i]['repository'] + " " + templates[i]['path'] + "; fi")
+		    for j in range(0, len(templates[i]['content'])):
+		        os.system("cp -r " + templates[i]['content'][j]['source'] + " " + templates[i]['content'][j]['target'])
+
+registryContent = RegistryContent()
+registryContent.update()
