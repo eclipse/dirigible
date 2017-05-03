9dc671368b05303e5f05b5da6ab5bfab374d4b05
delchev <delchevn@gmail.com>
2017-05-03 17:18:55 +0300

    Mini packaging option added

--- org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.content/git-checkout.sh
+++ org.eclipse.dirigible/org.eclipse.dirigible.parent/runtime/org.eclipse.dirigible.runtime.content/git-checkout.sh
@@ -3,23 +3,28 @@
 #exec 2>/dev/null
 
 ####### Updates Registry's content
-python updateRegistryContent.py
+#ßpython updateRegistryContent.py
 
 ####### Prepare content.zip files
 
 ### zip full content
 cd src/content/db/
-rm ../../../content.zip
-zip -r ../../../content.zip *
+rm ../../../content-all.zip
+zip -r ../../../content-all.zip *
+
+### zip runtime content
+rm ../../../content-runtime.zip
+zip -r ../../../content-runtime.zip dirigible/registry/public dirigible/registry/conf
 
 ### zip minimal content
 rm ../../../content-min.zip
-zip -r ../../../content-min.zip dirigible/registry/public dirigible/registry/conf
+zip -r ../../../content-min.zip dirigible/registry/public/ScriptingServices/core dirigible/registry/public/ScriptingServices/db dirigible/registry/public/ScriptingServices/doc dirigible/registry/public/ScriptingServices/io dirigible/registry/public/ScriptingServices/log dirigible/registry/public/ScriptingServices/net dirigible/registry/public/ScriptingServices/platform dirigible/registry/public/ScriptingServices/service dirigible/registry/public/ScriptingServices/utils
 
 ####### Copy content.zip files
 
 ### copy to releng
 cd ../../..
-cp content.zip ../../releng/all.tomcat/src/main/resources/content/repository.zip
-cp content-min.zip ../../releng/runtime.tomcat/src/main/resources/content/repository.zip
-cp content-min.zip ../../releng/air.tomcat/src/main/resources/content/repository.zip
+cp content-all.zip ../../releng/all.tomcat/src/main/resources/content/repository.zip
+cp content-runtime.zip ../../releng/runtime.tomcat/src/main/resources/content/repository.zip
+cp content-runtime.zip ../../releng/air.tomcat/src/main/resources/content/repository.zip
+cp content-min.zip ../../releng/mini.tomcat/src/main/resources/content/repository.zip
