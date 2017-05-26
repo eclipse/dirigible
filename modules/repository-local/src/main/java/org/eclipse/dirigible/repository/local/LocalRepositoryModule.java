63283ae55483b24bf8f2c49e609c2aeedaaa9ff6
delchev <delchevn@gmail.com>
2017-05-26 12:18:24 +0300

    java service fix

--- modules/repository-local/src/main/java/org/eclipse/dirigible/repository/local/LocalRepositoryModule.java
+++ modules/repository-local/src/main/java/org/eclipse/dirigible/repository/local/LocalRepositoryModule.java
@@ -1,9 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2017 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * SAP - initial API and implementation
+ *******************************************************************************/
+
 package org.eclipse.dirigible.repository.local;
 
 import org.eclipse.dirigible.commons.api.AbstractDirigibleModule;
 import org.eclipse.dirigible.commons.config.Configuration;
 import org.eclipse.dirigible.repository.api.IRepository;
 
+/**
+ * Module for managing Local Repository instantiation and binding
+ */
 public class LocalRepositoryModule extends AbstractDirigibleModule {
 
 	@Override
