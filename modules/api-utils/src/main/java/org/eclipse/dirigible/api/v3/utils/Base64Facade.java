4b60083ee4489094f74ce34d976760bc45475074
delchev <delchevn@gmail.com>
2017-07-21 19:36:31 +0300

    byte utils extracted

--- modules/api-utils/src/main/java/org/eclipse/dirigible/api/v3/utils/Base64Facade.java
+++ modules/api-utils/src/main/java/org/eclipse/dirigible/api/v3/utils/Base64Facade.java
@@ -3,27 +3,9 @@ package org.eclipse.dirigible.api.v3.utils;
 import java.util.ArrayList;
 
 import org.apache.commons.codec.binary.Base64;
+import org.eclipse.dirigible.api.v3.core.ByteUtils;
 
 public class Base64Facade {
-
-	private static final String ERROR_BYTE_ONLY_MESSAGE = "Stream operations can be performed only on byte arrays";
-
-	public static final String encode(ArrayList<Integer> input) {
-		byte[] byreArray = new byte[input.size()];
-		for (int i = 0; i < input.size(); i++) {
-			Object val = input.get(i);
-			if (!(val instanceof Integer)) {
-				throw new IllegalArgumentException(ERROR_BYTE_ONLY_MESSAGE);
-			}
-			Integer intValue = (Integer) val;
-			if (Byte.MIN_VALUE > intValue && Byte.MAX_VALUE < intValue) {
-				throw new IllegalArgumentException(ERROR_BYTE_ONLY_MESSAGE);
-			}
-			byte b = Byte.valueOf(intValue + "");
-			byreArray[i] = b;
-		}
-		return encode(byreArray);
-	}
 
 	public static final String encode(byte[] input) {
 		Base64 base64 = new Base64();
@@ -33,6 +15,12 @@ public class Base64Facade {
 	public static final byte[] decode(String input) {
 		Base64 base64 = new Base64();
 		return base64.decode(input);
+	}
+	
+	// overloaded for J2V8
+	public static final String encode(ArrayList<Integer> input) {
+		byte[] byreArray = ByteUtils.listToArray(input);
+		return encode(byreArray);
 	}
 
 }
