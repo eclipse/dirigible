bf2ff37fb5d001ef6c3ed0dbff5d06c19eca0841
ThuF <jordan.p.pavlov@gmail.com>
2017-08-25 19:27:31 +0300

    #35 "Preview" View subscription for "double click" events through message hub

--- ide/ide-preview/src/main/resources/ide-preview/preview.js
+++ ide/ide-preview/src/main/resources/ide-preview/preview.js
@@ -1,4 +1,26 @@
+var messageHub = new FramesMessageHub();
+var topic = 'fileselected';
+
 angular.module('preview', []).controller('PreviewController', function ($scope, $http) {
+
+	messageHub.subscribe(function(msg) {
+		var resourcePath = msg.data.path.substring(msg.data.path.indexOf('/', 1))
+		var url = window.top.location.protocol + '//' + window.top.location.host + '/services/v3';
+		var type = resourcePath.substring(resourcePath.lastIndexOf('.') + 1);
+		switch(type) {
+			case 'js':
+				url += '/js';
+				break;
+			case 'html':
+				url += '/web';
+				break;
+			default:
+				return;
+		}
+		url += resourcePath;
+		$scope.previewUrl = url;
+		$scope.$apply();
+	}, topic);
 
 }).config(function($sceProvider) {
     $sceProvider.enabled(false);
