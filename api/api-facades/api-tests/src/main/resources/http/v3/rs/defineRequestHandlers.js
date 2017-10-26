6e14eb33637c5c148b91f6581418d2b85177fde4
shturec <sthrakal@gmail.com>
2017-10-26 19:14:39 +0300

    + arestme

---
+++ api/api-facades/api-tests/src/main/resources/http/v3/rs/defineRequestHandlers.js
@@ -0,0 +1,33 @@
+/* eslint-env node, dirigible */
+
+const LOGGER = require('log/logging').getLogger('http.rs.tests');
+
+var rs = require('http/v3/rs').get();
+
+rs.addResourceHandler("", "get", function(ctx, io){
+	io.response.println('OK');
+}, undefined, undefined, function(){
+	LOGGER.info('Handling resource with GET');
+})
+rs.addResourceHandler("", "put", function(ctx, io){
+	io.response.println('OK');
+}, undefined, undefined, function(){
+	LOGGER.info('Handling resource with PUT');
+})
+rs.addResourceHandler("", "post", function(ctx, io){
+	io.response.println('OK');
+}, undefined, undefined, function(){
+	LOGGER.info('Handling resource with POST');
+})
+rs.addResourceHandler("", "delete", function(ctx, io){
+	io.response.println('OK');
+}, undefined, undefined, function(){
+	LOGGER.info('Handling resource with DELETE');
+})
+rs.addResourceHandler("a/{param1}/{param2}", "get", function(ctx, io){
+	LOGGER.info('Handling resource with GET and parameterized path');
+	io.response.println('OK');
+}, undefined, undefined, function(){
+	LOGGER.info('Handling resource with GET and parameterized path');
+})
+.service();
