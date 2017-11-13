831cfad236bb737173871a03c7c956e06b7b947e
delchev <delchevn@gmail.com>
2017-11-13 20:23:12 +0200

    aliases added for core api

---
+++ api/api-javascript/api-core/src/main/resources/core/modules.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2017 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * SAP - initial API and implementation
+ */
+
+exports.getConsole = function() {
+	var console = require('core/v3/console');
+	return console;
+};
+
+exports.getContext = function() {
+	var context = require('core/v3/context');
+	return context;
+};
+
+exports.getEnv = function() {
+	var env = require('core/v3/env');
+	return env;
+};
+
+exports.getExtensions = function() {
+	var extensions = require('core/v3/extensions');
+	return extensions;
+};
+
+exports.getGlobals = function() {
+	var globals = require('core/v3/globals');
+	return globals;
+};
+
+exports.getJava = function() {
+	var java = require('core/v3/java');
+	return java;
+};
+
