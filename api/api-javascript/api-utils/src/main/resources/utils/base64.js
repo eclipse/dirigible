fea4111fbeb1b99e2d4659a31121814703e6aa60
delchev <delchevn@gmail.com>
2017-11-13 09:44:14 +0200

    license headers for javascript api updated

--- api/api-javascript/api-utils/src/main/resources/utils/base64.js
+++ api/api-javascript/api-utils/src/main/resources/utils/base64.js
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2017 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -6,9 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  * Contributors:
  * SAP - initial API and implementation
- *******************************************************************************/
-
-/* eslint-env node, dirigible */
+ */
 
 var base64 = require('utils/v3/base64');
 for(var propertyName in base64) {
