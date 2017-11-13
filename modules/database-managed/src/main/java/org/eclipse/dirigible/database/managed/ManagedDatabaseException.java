299409463cefc8fa5cb18594b2a469a3de08a233
delchev <delchevn@gmail.com>
2017-11-13 15:40:00 +0200

    javadoc fix

--- modules/database-managed/src/main/java/org/eclipse/dirigible/database/managed/ManagedDatabaseException.java
+++ modules/database-managed/src/main/java/org/eclipse/dirigible/database/managed/ManagedDatabaseException.java
@@ -12,13 +12,11 @@ package org.eclipse.dirigible.database.managed;
 
 import org.eclipse.dirigible.database.api.DatabaseException;
 
-// TODO: Auto-generated Javadoc
 /**
- * The Class ManagedDatabaseException.
+ * The Managed Database Exception.
  */
 public class ManagedDatabaseException extends DatabaseException {
 
-	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -2161860568272479874L;
 
 	/**
@@ -31,10 +29,14 @@ public class ManagedDatabaseException extends DatabaseException {
 	/**
 	 * Instantiates a new managed database exception.
 	 *
-	 * @param message the message
-	 * @param cause the cause
-	 * @param enableSuppression the enable suppression
-	 * @param writableStackTrace the writable stack trace
+	 * @param message
+	 *            the message
+	 * @param cause
+	 *            the cause
+	 * @param enableSuppression
+	 *            the enable suppression
+	 * @param writableStackTrace
+	 *            the writable stack trace
 	 */
 	public ManagedDatabaseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
@@ -43,8 +45,10 @@ public class ManagedDatabaseException extends DatabaseException {
 	/**
 	 * Instantiates a new managed database exception.
 	 *
-	 * @param message the message
-	 * @param cause the cause
+	 * @param message
+	 *            the message
+	 * @param cause
+	 *            the cause
 	 */
 	public ManagedDatabaseException(String message, Throwable cause) {
 		super(message, cause);
@@ -53,7 +57,8 @@ public class ManagedDatabaseException extends DatabaseException {
 	/**
 	 * Instantiates a new managed database exception.
 	 *
-	 * @param message the message
+	 * @param message
+	 *            the message
 	 */
 	public ManagedDatabaseException(String message) {
 		super(message);
@@ -62,7 +67,8 @@ public class ManagedDatabaseException extends DatabaseException {
 	/**
 	 * Instantiates a new managed database exception.
 	 *
-	 * @param cause the cause
+	 * @param cause
+	 *            the cause
 	 */
 	public ManagedDatabaseException(Throwable cause) {
 		super(cause);
