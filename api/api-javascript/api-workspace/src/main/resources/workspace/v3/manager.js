02cf60775bdb889ceb3e4f5622aef35043507fd2
delchev <delchevn@gmail.com>
2018-03-15 19:24:42 +0200

    bpm aliases fixes

--- api/api-javascript/api-workspace/src/main/resources/workspace/v3/manager.js
+++ api/api-javascript/api-workspace/src/main/resources/workspace/v3/manager.js
@@ -32,44 +32,56 @@ exports.getWorkspacesNames = function() {
 	return workspacesNames;
 };
 
-exports.deletetWorkspace = function(name) {
-	java.call('org.eclipse.dirigible.api.v3.workspace.WorkspaceFacade', 'deletetWorkspace', [name], true);
+exports.deleteWorkspace = function(name) {
+	java.call('org.eclipse.dirigible.api.v3.workspace.WorkspaceFacade', 'deleteWorkspace', [name], true);
 };
 
 /**
  * Workspace object
  */
 function Workspace() {
-	
+
 	this.getProjects = function() {
 		var projectsInstance = java.invoke(this.uuid, 'getProjects', [], true);
 		var projects = new Projects();
 		projects.uuid = projectsInstance.uuid;
 		return projects;
 	};
-	
+
 	this.createProject = function(name) {
 		var projectInstance = java.invoke(this.uuid, 'createProject', [name], true);
 		var project = new Project();
 		project.uuid = projectInstance.uuid;
 		return project;
 	};
-	
+
 	this.getProject = function(name) {
 		var projectInstance = java.invoke(this.uuid, 'getProject', [name], true);
 		var project = new Project();
 		project.uuid = projectInstance.uuid;
 		return project;
 	};
-	
+
 	this.deleteProject = function(name) {
 		java.invoke(this.uuid, 'deleteProject', [name]);
 	};
-	
+
+	this.exists = function() {
+		return java.invoke(this.uuid, 'exists', []);
+	};
+
+	this.existsFolder = function(path) {
+		return java.invoke(this.uuid, 'existsFolder', [path]);
+	};
+
+	this.existsFile = function(path) {
+		return java.invoke(this.uuid, 'existsFile', [path]);
+	};
+
 	this.copyProject = function(source, target) {
 		java.invoke(this.uuid, 'copyProject', [source, target]);
 	};
-	
+
 	this.moveProject = function(source, target) {
 		java.invoke(this.uuid, 'moveProject', [source, target]);
 	};
@@ -118,9 +130,13 @@ function Project() {
 		folder.uuid = folderInstance.uuid;
 		return folder;
 	};
-	
+
+	this.exists = function() {
+		return java.invoke(this.uuid, 'exists', []);
+	};
+
 	this.existsFolder = function(path) {
-		return java.invoke(this.uuid, 'createFolder', [path]);
+		return java.invoke(this.uuid, 'existsFolder', [path]);
 	};
 	
 	this.getFolder = function(path) {
@@ -149,7 +165,7 @@ function Project() {
 	};
 	
 	this.existsFile = function(path) {
-		return java.invoke(this.uuid, 'createFile', [path]);
+		return java.invoke(this.uuid, 'existsFile', [path]);
 	};
 	
 	this.getFile = function(path) {
@@ -233,9 +249,13 @@ function Folder() {
 		folder.uuid = folderInstance.uuid;
 		return folder;
 	};
-	
+
+	this.exists = function() {
+		return java.invoke(this.uuid, 'exists', []);
+	};
+
 	this.existsFolder = function(path) {
-		return java.invoke(this.uuid, 'createFolder', [path]);
+		return java.invoke(this.uuid, 'existsFolder', [path]);
 	};
 	
 	this.getFolder = function(path) {
@@ -264,7 +284,7 @@ function Folder() {
 	};
 	
 	this.existsFile = function(path) {
-		return java.invoke(this.uuid, 'createFile', [path]);
+		return java.invoke(this.uuid, 'existsFile', [path]);
 	};
 	
 	this.getFile = function(path) {
@@ -324,6 +344,9 @@ function File() {
 		var output = java.call('org.eclipse.dirigible.api.v3.workspace.WorkspaceFacade', 'setContent', [this.uuid, JSON.stringify(input)]);
 		return output;
 	};
-	
+
+	this.exists = function() {
+		return java.invoke(this.uuid, 'exists', []);
+	};
 }
 
