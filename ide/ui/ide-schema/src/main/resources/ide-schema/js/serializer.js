e36a535a3034994b6f31636a1af624456f6c4ac1
delchev <delchevn@gmail.com>
2018-03-26 16:51:56 +0300

    workspace ui fixes

--- ide/ui/ide-schema/src/main/resources/ide-schema/js/serializer.js
+++ ide/ui/ide-schema/src/main/resources/ide-schema/js/serializer.js
@@ -74,79 +74,56 @@ function createSchema(graph) {
 }
 
 function createSchemaJson(graph) {
-	var schema = [];
-	schema.push('{"schema":{\n');
-	schema.push(' "structures":[\n');
+	var root = {};
+	root.schema = {};
+	root.schema.structures = [];
 	var parent = graph.getDefaultParent();
 	var childCount = graph.model.getChildCount(parent);
 
 	for (var i=0; i<childCount; i++) {
-		if (i>0) {
-			schema.push(',');
-		}
 		var child = graph.model.getChildAt(parent, i);
-		
+		var structure = {};
 		if (!graph.model.isEdge(child)) {
-			schema.push('  {"name":"'+child.value.name+'", "type":"'+child.value.type.toUpperCase()+'", "columns":[\n');
-			
-			var columnCount = graph.model.getChildCount(child);
+			structure.name = child.value.name;
+			structure.type = child.value.type.toUpperCase();
+			structure.columns = [];
 
+			var columnCount = graph.model.getChildCount(child);
 			if (columnCount > 0) {
 				for (var j=0; j<columnCount; j++) {
-					if (j>0) {
-						schema.push(',');
-					}
-					var column = graph.model.getChildAt(child, j).value;
-					
-					if (column.isSQL) {
-						schema.push('    {"query":"'+column.name+'"}\n');
+					var childColumn = graph.model.getChildAt(child, j).value;
+					var column = {};
+					if (childColumn.isSQL) {
+						column.query = childColumn.name;
 					} else {
-						schema.push('    {"name":"'+column.name+'", "type":"'+column.type+'"');
-						if (column.columnLength !== null) {
-							schema.push(', "length":"'+column.columnLength+'"');
-						}
-						if (column.notNull) {
-							schema.push(', "nullable":"false"');
-						}
-						if (column.primaryKey) {
-							schema.push(', "primaryKey":"true"');
-						}
-						if (column.autoIncrement) {
-							schema.push(', "identity":"true"');
-						}
-						if (column.unique) {
-							schema.push(', "unique":"true"');
-						}
-						if (column.defaultValue !== null) {
-							schema.push(', "defaultValue":"'+column.defaultValue+'"');
-						}
-						if (column.precision !== null) {
-							schema.push(', "precision":"'+column.precision+'"');
-						}
-						if (column.scale !== null) {
-							schema.push(', "scale":"'+column.scale+'"');
-						}
-						schema.push('}\n');
+						column.name = childColumn.name;
+						column.type = childColumn.type;
+						column.length = childColumn.columnLength;
+						column.nullable = childColumn.notNull ? !childColumn.notNull : true;
+						column.primaryKey = childColumn.primaryKey ? childColumn.primaryKey : false;
+						column.identity = childColumn.autoIncrement ? childColumn.autoIncrement : false;
+						column.unique = childColumn.unique ? childColumn.unique : false;
+						column.defaultValue = childColumn.defaultValue ? childColumn.defaultValue : null;
+						column.precision = childColumn.precision ? childColumn.precision : null;
+						column.scale = childColumn.scale ? childColumn.scale : null;
 					}
+					structure.columns.push(column);
 				}
 			}
-			schema.push('  ]}\n');
 		} else {
-			schema.push('  {"name":"'+child.source.parent.value.name+'_' 
-				+child.target.parent.value.name+'", "type":"foreignKey", ');
-			schema.push('"table":"'+child.source.parent.value.name+'", ');
-			schema.push('"constraintName":"'+child.source.parent.value.name+'_' 
-				+ child.target.parent.value.name+'", ');
-			schema.push('"columns":"'+child.source.value.name+'", '+
-				'"referencedTable":"'+child.target.parent.value.name+'", '+
-				'"referencedColumns":"'+child.target.value.name+'"}\n');
+			structure.name = child.source.parent.value.name+'_'+child.target.parent.value.name;
+			structure.type = 'foreignKey';
+			structure.table = child.source.parent.value.name;
+			structure.constraintName = child.source.parent.value.name+'_'+child.target.parent.value.name;
+			structure.columns = child.source.value.name;
+			structure.referencedTable = child.target.parent.value.name;
+			structure.referencedColumns = child.target.value.name;
 		}
+		
+		root.schema.structures.push(structure);
 	}
-	schema.push(' ]\n');
-	schema.push('}}');
 	
-	var serialized = schema.join('');
-	serialized = JSON.stringify(JSON.parse(serialized), null, 4);
+	var serialized = JSON.stringify(root, null, 4);
 	
 	return serialized;
 }
