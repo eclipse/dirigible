a989fd92c6e88ec54ee8eb97c74e55a6da0b88af
delchev <delchevn@gmail.com>
2018-05-02 16:07:35 +0300

    auto-upgrade for security synchronizer form 3.1.x to 3.2.x

--- modules/core/core-security/src/main/java/org/eclipse/dirigible/core/security/synchronizer/SecuritySynchronizer.java
+++ modules/core/core-security/src/main/java/org/eclipse/dirigible/core/security/synchronizer/SecuritySynchronizer.java
@@ -15,6 +15,11 @@ import static java.text.MessageFormat.format;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -24,6 +29,7 @@ import java.util.Set;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import javax.sql.DataSource;
 
 import org.apache.commons.io.IOUtils;
 import org.eclipse.dirigible.commons.api.module.StaticInjector;
@@ -57,6 +63,11 @@ public class SecuritySynchronizer extends AbstractSynchronizer {
 
 	@Inject
 	private SecurityCoreService securityCoreService;
+	
+	@Inject
+	private DataSource dataSource;
+	
+	private volatile boolean upgradePassed; 
 
 	/**
 	 * Force synchronization.
@@ -123,6 +134,9 @@ public class SecuritySynchronizer extends AbstractSynchronizer {
 		synchronized (SecuritySynchronizer.class) {
 			logger.trace("Synchronizing Roles and Access artifacts...");
 			try {
+				if (!upgradePassed) {
+					upgradePassed = checkUpgrade();
+				}
 				clearCache();
 				synchronizePredelivered();
 				synchronizeRegistry();
@@ -133,6 +147,28 @@ public class SecuritySynchronizer extends AbstractSynchronizer {
 			}
 			logger.trace("Done synchronizing Roles and Access artifacts.");
 		}
+	}
+
+	private boolean checkUpgrade() throws SQLException {
+		// from 3.1.x to 3.2.x
+		try (Connection connection = dataSource.getConnection()) {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM DIRIGIBLE_SECURITY_ACCESS");
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnCount = rsmd.getColumnCount();
+
+			for (int i = 1; i <= columnCount; i++ ) {
+			  String name = rsmd.getColumnName(i);
+			  if ("ACCESS_URI".equals(name)) {
+				  logger.warn("Upgrading Security Access Synchronizer from 3.1.x version to 3.2.x ...");
+				  Statement drop = connection.createStatement();
+				  drop.executeUpdate("DROP TABLE DIRIGIBLE_SECURITY_ACCESS");
+				  logger.warn("Upgrade of Security Access Synchronizer from 3.1.x version to 3.2.x passed successfully.");
+				  break;
+			  }
+			}
+		}
+		return true;
 	}
 
 	/**
