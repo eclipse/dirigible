b036bb2375f5c740c872e8b09f1b1a65c9d90704
delchev <delchevn@gmail.com>
2018-05-08 18:58:12 +0300

    extensions and extension points views added

---
+++ ide/ui/ide-extensions/src/main/resources/ide-extensions/views/extensions/extensions.js
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2017 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * SAP - initial API and implementation
+ */
+
+exports.getView = function() {
+	return {
+		"id": "extensions",
+		"name": "Extensions",
+		"factory": "frame",
+		"region": "center-top",
+		"label": "Extensions",
+		"link": "../ide-extensions/views/extensions/index.html"
+	};
+};
