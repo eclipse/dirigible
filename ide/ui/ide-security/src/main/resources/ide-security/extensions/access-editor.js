4357ebe8b6a6176d14474827d292ece42b9d2fca
delchev <delchevn@gmail.com>
2018-06-26 12:59:22 +0300

    access and roles editors added

---
+++ ide/ui/ide-security/src/main/resources/ide-security/extensions/access-editor.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * SAP - initial API and implementation
+ */
+
+exports.getEditor = function() {
+	var editor = {
+			"id":"access",
+			"name":"Access",
+			"factory":"frame",
+			"region":"center-top",
+			"label":"Access",
+			"link":"../ide-security/editors/access/editor.html",
+			"contentTypes":["application/json+access"]
+	};
+	return editor;
+};
---
+++ ide/ui/ide-security/src/main/resources/ide-security/extensions/roles-editor.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * SAP - initial API and implementation
+ */
+
+exports.getEditor = function() {
+	var editor = {
+			"id":"roles",
+			"name":"Roles",
+			"factory":"frame",
+			"region":"center-top",
+			"label":"Roles",
+			"link":"../ide-security/editors/roles/editor.html",
+			"contentTypes":["application/json+roles"]
+	};
+	return editor;
+};
