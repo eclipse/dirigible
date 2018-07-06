4ead86e1544e39597cdf2fd08a8ef9a7b33016b5
delchev <delchevn@gmail.com>
2018-07-06 20:39:43 +0300

    distro for the embedded dirigible cases

--- modules/services/service-core/src/main/java/org/eclipse/dirigible/runtime/core/listener/DirigibleServletContextListener.java
+++ modules/services/service-core/src/main/java/org/eclipse/dirigible/runtime/core/listener/DirigibleServletContextListener.java
@@ -10,36 +10,16 @@
 
 package org.eclipse.dirigible.runtime.core.listener;
 
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
-import java.util.ServiceLoader;
 
 import javax.servlet.ServletContextEvent;
 
-import org.apache.cxf.interceptor.security.SecureAnnotationsInterceptor;
-import org.apache.cxf.jaxrs.swagger.Swagger2Feature;
-import org.eclipse.dirigible.commons.api.content.ClasspathContentLoader;
-import org.eclipse.dirigible.commons.api.module.DirigibleModulesInstallerModule;
-import org.eclipse.dirigible.commons.api.module.StaticInjector;
-import org.eclipse.dirigible.commons.api.service.AbstractExceptionHandler;
-import org.eclipse.dirigible.commons.api.service.IRestService;
-import org.eclipse.dirigible.commons.config.Configuration;
-import org.eclipse.dirigible.core.messaging.service.SchedulerManager;
-import org.eclipse.dirigible.core.scheduler.api.SchedulerException;
-import org.eclipse.dirigible.core.scheduler.manager.SchedulerInitializer;
-import org.eclipse.dirigible.runtime.core.services.GsonMessageBodyHandler;
+import org.eclipse.dirigible.runtime.core.initializer.DirigibleInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
-
-import io.swagger.models.auth.BasicAuthDefinition;
-import io.swagger.models.auth.SecuritySchemeDefinition;
 
 /**
  * This class handles the initialization of all Guice modules and all REST API
@@ -49,9 +29,9 @@ public class DirigibleServletContextListener extends GuiceServletContextListener
 
 	private static final Logger logger = LoggerFactory.getLogger(DirigibleServletContextListener.class);
 
-	private static final HashSet<Object> services = new HashSet<Object>();
-
 	private Injector injector;
+	
+	private static DirigibleInitializer initializer = new DirigibleInitializer();
 
 	/*
 	 * (non-Javadoc)
@@ -59,14 +39,7 @@ public class DirigibleServletContextListener extends GuiceServletContextListener
 	 */
 	@Override
 	protected Injector getInjector() {
-		logger.trace("Initializing Guice Injector with modules for dependency injection...");
-
-		injector = Guice.createInjector(new DirigibleModulesInstallerModule());
-		StaticInjector.setInjector(injector);
-
-		logger.trace("Guice Injector with modules for dependency injection initialized.");
-
-		return injector;
+		return initializer.getInjector();
 	}
 
 	/*
@@ -75,157 +48,8 @@ public class DirigibleServletContextListener extends GuiceServletContextListener
 	 */
 	@Override
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
-		logger.info("---------- Initializing Eclipse Dirigible Platform... ----------");
 		super.contextInitialized(servletContextEvent);
-
-		Configuration.load("/dirigible-core.properties");
-		Configuration.load("/dirigible.properties");
-
-		loadPredeliveredContent();
-
-		registerRestServicesForCxf();
-
-		startupScheduler();
-
-		startupMessaging();
-		
-		printAllConfigurations();
-
-		logger.info("---------- Eclipse Dirigible Platform initialized. ----------");
-	}
-
-	private void printAllConfigurations() {
-		String[] keys = Configuration.getKeys();
-		for (String key: keys) {
-			String value = Configuration.get(key);
-			logger.info("Configuration: {}={}", key, value);			
-		}
-	}
-
-	/**
-	 * Load predelivered content.
-	 */
-	private void loadPredeliveredContent() {
-		logger.trace("Loading the predelivered content...");
-		try {
-			ClasspathContentLoader.load();
-		} catch (IOException e) {
-			logger.error("Failed loading the predelivered content", e);
-		}
-		logger.trace("Done loading predelivered content.");
-	}
-
-	/**
-	 * Register rest services for cxf.
-	 */
-	private void registerRestServicesForCxf() {
-		logger.trace("Registering REST services...");
-
-		getServices().add(new SecureAnnotationsInterceptor());
-		getServices().add(new GsonMessageBodyHandler<Object>());
-
-		addRestServices();
-		addExceptionHandlers();
-		addSwagger();
-
-		logger.trace("Done registering REST services.");
-	}
-
-	/**
-	 * Adds the rest services.
-	 */
-	private void addRestServices() {
-		for (IRestService next : ServiceLoader.load(IRestService.class)) {
-			getServices().add(injector.getInstance(next.getType()));
-			logger.info("REST service registered {}.", next.getType());
-		}
-	}
-
-	/**
-	 * Adds the exception handlers.
-	 */
-	private void addExceptionHandlers() {
-		for (AbstractExceptionHandler<?> next : ServiceLoader.load(AbstractExceptionHandler.class)) {
-			getServices().add(injector.getInstance(next.getType()));
-			logger.info("Exception Handler registered {}.", next.getType());
-		}
-	}
-
-	/**
-	 * Adds the swagger.
-	 */
-	private void addSwagger() {
-		Swagger2Feature feature = new Swagger2Feature();
-
-		// customize some of the properties
-		feature.setBasePath("/services/v3");
-		feature.setPrettyPrint(true);
-		feature.setDescription("Eclipse Dirigible API of the core RESTful services provided by the application development platform itself");
-		feature.setVersion("3.0");
-		feature.setTitle("Eclipse Dirigible - RESTful Services API");
-		feature.setContact("dirigible-dev@eclipse.org");
-		feature.setLicense("Eclipse Public License - v 1.0");
-		feature.setLicenseUrl("https://www.eclipse.org/legal/epl-v10.html");
-		Map<String, SecuritySchemeDefinition> securityDefinitions = new HashMap<String, SecuritySchemeDefinition>();
-		BasicAuthDefinition auth = new BasicAuthDefinition();
-		auth.setType("basic");
-		securityDefinitions.put("basicAuth", auth);
-		feature.setSecurityDefinitions(securityDefinitions);
-		feature.setPrettyPrint(true);
-
-		getServices().add(feature);
-	}
-
-	/**
-	 * Startup scheduler.
-	 */
-	private void startupScheduler() {
-		logger.info("Starting Scheduler...");
-		try {
-			injector.getInstance(SchedulerInitializer.class).initialize();
-		} catch (SchedulerException | SQLException | IOException e) {
-			logger.error("Failed starting Scheduler", e);
-		}
-		logger.info("Done starting Scheduler.");
-	}
-
-	/**
-	 * Shutdown scheduler.
-	 */
-	private void shutdownScheduler() {
-		logger.trace("Shutting down Scheduler...");
-		try {
-			SchedulerInitializer.shutdown();
-		} catch (SchedulerException e) {
-			logger.error("Failed shutting down Scheduler", e);
-		}
-		logger.trace("Done shutting down Scheduler.");
-	}
-
-	/**
-	 * Startup messaging.
-	 */
-	private void startupMessaging() {
-		logger.info("Starting Message Broker...");
-		try {
-			injector.getInstance(SchedulerManager.class).initialize();
-		} catch (Exception e) {
-			logger.error("Failed starting Messaging", e);
-		}
-		logger.info("Done starting Message Broker.");
-	}
-
-	/**
-	 * Shutdown messaging.
-	 */
-	private void shutdownMessaging() {
-		logger.trace("Shutting down Message Broker...");
-		try {
-			SchedulerManager.shutdown();
-		} catch (Exception e) {
-			logger.error("Failed shutting down Message Broker", e);
-		}
-		logger.trace("Done shutting down Message Broker.");
+		initializer.initialize();
 	}
 
 	/**
@@ -234,7 +58,7 @@ public class DirigibleServletContextListener extends GuiceServletContextListener
 	 * @return all singleton services.
 	 */
 	public static HashSet<Object> getServices() {
-		return services;
+		return initializer.getServices();
 	}
 
 	/*
@@ -243,15 +67,8 @@ public class DirigibleServletContextListener extends GuiceServletContextListener
 	 */
 	@Override
 	public void contextDestroyed(ServletContextEvent servletContextEvent) {
-		logger.info("Shutting down Eclipse Dirigible Platform...");
-
-		shutdownScheduler();
-
-		shutdownMessaging();
-
+		initializer.destory();
 		super.contextDestroyed(servletContextEvent);
-
-		logger.info("Eclipse Dirigible Platform shut down.");
 	}
 
 }
