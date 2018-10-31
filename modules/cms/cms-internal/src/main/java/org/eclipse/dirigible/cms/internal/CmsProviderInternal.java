27c0debedade6b40a1054ba1b5f9ad673f947852
delchev <delchevn@gmail.com>
2018-10-31 12:02:33 +0200

    Fixed: #361 - Update Tomcat version to 8.5.34

--- modules/cms/cms-internal/src/main/java/org/eclipse/dirigible/cms/internal/CmsProviderInternal.java
+++ modules/cms/cms-internal/src/main/java/org/eclipse/dirigible/cms/internal/CmsProviderInternal.java
@@ -15,7 +15,7 @@ public class CmsProviderInternal implements ICmsProvider {
 	/** The Constant DIRIGIBLE_CMS_INTERNAL_ROOT_FOLDER_IS_ABSOLUTE. */
 	public static final String DIRIGIBLE_CMS_INTERNAL_ROOT_FOLDER_IS_ABSOLUTE = "DIRIGIBLE_CMS_INTERNAL_ROOT_FOLDER_IS_ABSOLUTE"; //$NON-NLS-1$
 
-	private static final String CMIS = "cmis"; //$NON-NLS-1$
+	private static final String CMS = "cms"; //$NON-NLS-1$
 
 	/** The Constant NAME. */
 	public static final String NAME = "repository"; //$NON-NLS-1$
@@ -31,7 +31,7 @@ public class CmsProviderInternal implements ICmsProvider {
 		String rootFolder = Configuration.get(DIRIGIBLE_CMS_INTERNAL_ROOT_FOLDER);
 		boolean absolute = Boolean.parseBoolean(Configuration.get(DIRIGIBLE_CMS_INTERNAL_ROOT_FOLDER_IS_ABSOLUTE));
 
-		String repositoryFolder = rootFolder + File.separator + CMIS;
+		String repositoryFolder = rootFolder + File.separator + CMS;
 
 		IRepository repository = new LocalRepository(repositoryFolder, absolute);
 		this.cmisRepository = CmisRepositoryFactory.createCmisRepository(repository);
