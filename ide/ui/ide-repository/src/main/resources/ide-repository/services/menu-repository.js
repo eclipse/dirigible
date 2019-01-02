3c47724520232b7c8f70340e1a30daa69069d1a7
delchev <delchevn@gmail.com>
2019-01-02 21:10:02 +0200

    years update

--- ide/ui/ide-repository/src/main/resources/ide-repository/services/menu-repository.js
+++ ide/ui/ide-repository/src/main/resources/ide-repository/services/menu-repository.js
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 2017 SAP and others.
+ * Copyright (c) 2010-2019 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
  * Contributors:
- * SAP - initial API and implementation
+ *   SAP - initial API and implementation
  */
-
 var extensions = require('core/v3/extensions');
 var response = require('http/v3/response');
 
