96489562b83731534f99ea16e45cadebe1bb87dd
delchev <delchevn@gmail.com>
2019-01-02 20:44:44 +0200

    years update

--- ext/ext-v3-qunit/src/main/resources/qunit/qunit-2.0.1.js
+++ ext/ext-v3-qunit/src/main/resources/qunit/qunit-2.0.1.js
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2010-2019 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   SAP - initial API and implementation
+ */
 /*!
  * QUnit 2.0.1
  * https://qunitjs.com/
@@ -1617,7 +1627,7 @@ function errorString( error ) {
 }
 
 // Test for equality any JavaScript type.
-// Author: Philippe Rathé <prathe@gmail.com>
+// Author: Philippe Rathï¿½ <prathe@gmail.com>
 QUnit.equiv = ( function() {
 
 	// Stack to decide between skip/abort functions
