d6449d0647ebdde6f5bab57be28905b0f32d2479
delchev <delchevn@gmail.com>
2019-01-02 19:30:25 +0200

    license years update

--- ext/ext-v3-qunit/src/main/resources/qunit/reporters/reporter-junit.js
+++ ext/ext-v3-qunit/src/main/resources/qunit/reporters/reporter-junit.js
@@ -1,13 +1,13 @@
-/*
- * Copyright (c) 2010-2018 SAP and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/**
+ * JUnit reporter for QUnit
  *
- * Contributors:
- *   SAP - initial API and implementation
+ * https://github.com/JamesMGreene/QUnit-reporter-junit
+ *
+ * Copyright jQuery Foundation and other contributors
+ * Released under the MIT license.
+ * https://jquery.org/license/
  */
+
 (function() {
 	'use strict';
 	
