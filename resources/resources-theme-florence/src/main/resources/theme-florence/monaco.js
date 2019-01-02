f9c8bff2f36c0714a38a1f38862486a8c88a3851
delchev <delchevn@gmail.com>
2019-01-02 21:26:16 +0200

    years update

--- resources/resources-theme-florence/src/main/resources/theme-florence/monaco.js
+++ resources/resources-theme-florence/src/main/resources/theme-florence/monaco.js
@@ -1,1 +1,11 @@
+/*
+ * Copyright (c) 2010-2019 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   SAP - initial API and implementation
+ */
 var monacoTheme = 'vs-light';
