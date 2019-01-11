835e88b3c7fb376063cc9db24e5255cce0e726fe
ThuF <jordan.p.pavlov@gmail.com>
2019-01-11 17:14:14 +0200

    Fix #383 Busy Dialog during Dirigible Initialisation

--- modules/core/core-scheduler/src/main/java/org/eclipse/dirigible/core/scheduler/api/AbstractSynchronizerJob.java
+++ modules/core/core-scheduler/src/main/java/org/eclipse/dirigible/core/scheduler/api/AbstractSynchronizerJob.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2018 SAP and others.
+ * Copyright (c) 2010-2019 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,8 @@
  */
 package org.eclipse.dirigible.core.scheduler.api;
 
+import org.eclipse.dirigible.commons.config.HealthStatus;
+import org.eclipse.dirigible.commons.config.HealthStatus.Jobs.JobStatus;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -26,6 +28,7 @@ public abstract class AbstractSynchronizerJob implements Job {
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		getSynchronizer().synchronize();
+		HealthStatus.getInstance().getJobs().setStatus(context.getJobDetail().getKey().getName(), JobStatus.Succeeded);
 	}
 
 	/**
