f324411c040f1397aee250980363e20208d9b298
delchev <delchevn@gmail.com>
2019-03-01 10:53:55 +0200

    tests update

---
+++ modules/repository/repository-api-test/src/main/java/org/eclipse/dirigible/repository/generic/RepositoryGenericDeletePrefixTest.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2010-2019 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   SAP - initial API and implementation
+ */
+package org.eclipse.dirigible.repository.generic;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import org.eclipse.dirigible.repository.api.ICollection;
+import org.eclipse.dirigible.repository.api.IRepository;
+import org.junit.Test;
+
+/**
+ * The Class RepositoryGenericRepositoryTest.
+ */
+public class RepositoryGenericDeletePrefixTest {
+
+	/** The repository. */
+	protected IRepository repository;
+
+	
+
+	/**
+	 * Test remove collection.
+	 */
+	@Test
+	public void testRemoveCollection() {
+		if (repository == null) {
+			return;
+		}
+
+		try {
+			ICollection collectionAbcd = repository.createCollection("/abcd"); //$NON-NLS-1$
+			ICollection collectionAbcdEfgh = repository.createCollection("/abcd/efgh"); //$NON-NLS-1$
+			ICollection collectionAbcd2 = repository.createCollection("/abcd2"); //$NON-NLS-1$
+			ICollection collectionAbcd2Efgh = repository.createCollection("/abcd2/efgh"); //$NON-NLS-1$
+			assertNotNull(collectionAbcd);
+			assertNotNull(collectionAbcd2);
+			repository.removeCollection("/abcd"); //$NON-NLS-1$
+			collectionAbcd = repository.getCollection("/abcd"); //$NON-NLS-1$
+			assertNotNull(collectionAbcd);
+			assertFalse(collectionAbcd.exists());
+			collectionAbcd2 = repository.getCollection("/abcd2"); //$NON-NLS-1$
+			assertNotNull(collectionAbcd2);
+			assertTrue(collectionAbcd2.exists());
+			collectionAbcdEfgh = repository.getCollection("/abcd/efgh"); //$NON-NLS-1$
+			assertNotNull(collectionAbcdEfgh);
+			assertFalse(collectionAbcdEfgh.exists());
+			collectionAbcd2Efgh = repository.getCollection("/abcd2/efgh"); //$NON-NLS-1$
+			assertNotNull(collectionAbcd2Efgh);
+			assertTrue(collectionAbcd2Efgh.exists());
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail(e.getMessage());
+		}
+	}
+
+}
