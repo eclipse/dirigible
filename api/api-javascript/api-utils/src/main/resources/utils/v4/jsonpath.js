26879b79956e16932980eed130ab9d481523702f
ThuF <jordan.p.pavlov@gmail.com>
2019-07-31 12:59:22 +0300

    [API] HTTP Resposne - set the default characters encoding to UTF-8

--- api/api-javascript/api-utils/src/main/resources/utils/v4/jsonpath.js
+++ api/api-javascript/api-utils/src/main/resources/utils/v4/jsonpath.js
@@ -1,14 +1,4 @@
 /*
- * Copyright (c) 2010-2019 SAP and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   SAP - initial API and implementation
- */
-/*
 * Forked from source published at https://github.com/s3u with MIT
 * license as of 5/30/2019 and adapted for Dirigible
 * Contributors:
