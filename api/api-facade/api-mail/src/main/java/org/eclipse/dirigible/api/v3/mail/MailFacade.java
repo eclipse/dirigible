db4b69d4f74cbd7e733992b6248f2498cabf2c22
ThuF <jordan.p.pavlov@gmail.com>
2019-11-22 16:18:32 +0200

    Fix #108 Mail API v4

---
+++ api/api-facade/api-mail/src/main/java/org/eclipse/dirigible/api/v3/mail/MailFacade.java
@@ -0,0 +1,186 @@
+/**
+ * Copyright (c) 2010-2019 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   SAP - initial API and implementation
+ */
+package org.eclipse.dirigible.api.v3.mail;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.eclipse.dirigible.commons.api.scripting.IScriptingFacade;
+import org.eclipse.dirigible.commons.config.Configuration;
+
+/**
+ * The Class MailFacade.
+ */
+public class MailFacade implements IScriptingFacade {
+
+	// Mail properties
+	private static final String MAIL_USERNAME = "mail.username";
+	private static final String MAIL_PASSWORD = "mail.password";
+
+	private static final String MAIL_TRANSPORT_PROTOCOL = "mail.transport.protocol";
+
+	// SMTPS properties
+	private static final String MAIL_SMTPS_HOST = "mail.smtps.host";
+	private static final String MAIL_SMTPS_PORT = "mail.smtps.port";
+	private static final String MAIL_SMTPS_AUTH = "mail.smtps.auth";
+
+	// SMTP properties
+	private static final String MAIL_SMTP_HOST = "mail.smtp.host";
+	private static final String MAIL_SMTP_PORT = "mail.smtp.port";
+	private static final String MAIL_SMTP_AUTH = "mail.smtp.auth";
+	private static final String MAIL_SMTP_SOCKET_FACTORY_PORT = "mail.smtp.socketFactory.port";
+	private static final String MAIL_SMTP_SOCKET_FACTORY_CLASS = "mail.smtp.socketFactory.class";
+
+	// Dirigible mail properties
+	private static final String DIRIGIBLE_MAIL_USERNAME = "DIRIGIBLE_MAIL_USERNAME";
+	private static final String DIRIGIBLE_MAIL_PASSWORD = "DIRIGIBLE_MAIL_PASSWORD";
+
+	private static final String DIRIGIBLE_MAIL_TRANSPORT_PROTOCOL = "DIRIGIBLE_MAIL_TRANSPORT_PROTOCOL";
+
+	// SMTP properties
+	private static final String DIRIGIBLE_MAIL_SMTPS_HOST = "DIRIGIBLE_MAIL_SMTPS_HOST";
+	private static final String DIRIGIBLE_MAIL_SMTPS_PORT = "DIRIGIBLE_MAIL_SMTPS_PORT";
+	private static final String DIRIGIBLE_MAIL_SMTPS_AUTH = "DIRIGIBLE_MAIL_SMTPS_AUTH";
+
+	// SMTP properties
+	private static final String DIRIGIBLE_MAIL_SMTP_HOST = "DIRIGIBLE_MAIL_SMTP_HOST";
+	private static final String DIRIGIBLE_MAIL_SMTP_PORT = "DIRIGIBLE_MAIL_SMTP_PORT";
+	private static final String DIRIGIBLE_MAIL_SMTP_AUTH = "DIRIGIBLE_MAIL_SMTP_AUTH";
+	private static final String DIRIGIBLE_MAIL_SMTP_SOCKET_FACTORY_PORT = "DIRIGIBLE_MAIL_SMTP_SOCKET_FACTORY_PORT";
+	private static final String DIRIGIBLE_MAIL_SMTP_SOCKET_FACTORY_CLASS = "DIRIGIBLE_MAIL_SMTP_SOCKET_FACTORY_CLASS";
+
+	// Default values
+	private static final String DEFAULT_SUBTYPE = "plain";
+	private static final String DEFAULT_MAIL_TRANSPORT_PROTOCOL = "smtps";
+
+	public static MailClient getInstance() {
+		return getInstance(getDefaultProperties());
+	}
+
+	public static MailClient getInstance(Properties properties) {
+		return new MailClient(properties); 
+	}
+
+	private static Properties getDefaultProperties() {
+		Properties properties = new Properties();
+
+		addValue(properties, MAIL_USERNAME, DIRIGIBLE_MAIL_USERNAME);
+		addValue(properties, MAIL_PASSWORD, DIRIGIBLE_MAIL_PASSWORD);
+		
+		addValue(properties, MAIL_TRANSPORT_PROTOCOL, DIRIGIBLE_MAIL_TRANSPORT_PROTOCOL, DEFAULT_MAIL_TRANSPORT_PROTOCOL);
+
+		addValue(properties, MAIL_SMTPS_HOST, DIRIGIBLE_MAIL_SMTPS_HOST);
+		addValue(properties, MAIL_SMTPS_PORT, DIRIGIBLE_MAIL_SMTPS_PORT);
+		addValue(properties, MAIL_SMTPS_AUTH, DIRIGIBLE_MAIL_SMTPS_AUTH);
+		
+		addValue(properties, MAIL_SMTP_HOST, DIRIGIBLE_MAIL_SMTP_HOST);
+		addValue(properties, MAIL_SMTP_PORT, DIRIGIBLE_MAIL_SMTP_PORT);
+		addValue(properties, MAIL_SMTP_PORT, DIRIGIBLE_MAIL_SMTP_PORT);
+		addValue(properties, MAIL_SMTP_AUTH, DIRIGIBLE_MAIL_SMTP_AUTH);
+		addValue(properties, MAIL_SMTP_SOCKET_FACTORY_PORT, DIRIGIBLE_MAIL_SMTP_SOCKET_FACTORY_PORT);
+		addValue(properties, MAIL_SMTP_SOCKET_FACTORY_CLASS, DIRIGIBLE_MAIL_SMTP_SOCKET_FACTORY_CLASS);
+
+		return properties;
+	}
+
+	private static void addValue(Properties properties, String key, String evnKey) {
+		addValue(properties, key, evnKey, null);
+	}
+
+	private static void addValue(Properties properties, String key, String evnKey, String defaultValue) {
+		String value = Configuration.get(evnKey);
+		if (value != null) {
+			properties.put(key, value);
+		} else if (defaultValue != null) {
+			properties.put(key, defaultValue);
+		}
+	}
+
+	public static class MailClient {
+		private Properties properties;
+
+		public MailClient(Properties properties) {
+			this.properties = properties;
+		}
+
+		/**
+		 * Send an email.
+		 *
+		 * @param from the sender
+		 * @param to the to receiver
+		 * @param cc the cc receiver
+		 * @param bcc the bcc receiver
+		 * @param subject the subject
+		 * @param content the content
+		 * @param subType the subType
+		 * @throws MessagingException 
+		 */
+		public void send(String from, String[] to, String[] cc, String[] bcc, String subject, String content, String subType) throws MessagingException {
+			Session session = getSession(this.properties);
+			Transport transport = session.getTransport();
+			transport.connect();
+			
+			MimeMessage mimeMessage = createMimeMessage(session, from, to, cc, bcc, subject, content, subType);
+			transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
+			transport.close();
+		}
+
+		private Session getSession(Properties properties) {
+			String username = properties.getProperty(MAIL_USERNAME);
+			String password = properties.getProperty(MAIL_PASSWORD);
+			Authenticator authenticator = new Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			};
+			return Session.getInstance(properties, authenticator);
+		}
+	}
+
+	private static MimeMessage createMimeMessage(Session smtpSession, String from, String to[], String cc[], String bcc[], String subjectText, String mailText, String subType)
+			throws MessagingException {
+
+		MimeMessage mimeMessage = new MimeMessage(smtpSession);
+		mimeMessage.setFrom(InternetAddress.parse(from)[0]);
+		for(String next : to) {
+			mimeMessage.addRecipients(RecipientType.TO, InternetAddress.parse(next));
+		}
+		for(String next : cc) {
+			mimeMessage.addRecipients(RecipientType.CC, InternetAddress.parse(next));
+		}
+		for(String next : bcc) {
+			mimeMessage.addRecipients(RecipientType.BCC, InternetAddress.parse(next));
+		}
+		mimeMessage.setSubject(subjectText, "UTF-8"); //$NON-NLS-1$
+
+		MimeMultipart multiPart = new MimeMultipart("alternative"); //$NON-NLS-1$
+		MimeBodyPart part = new MimeBodyPart();
+		part.setText(mailText, "utf-8", getSubType(subType)); //$NON-NLS-1$
+		multiPart.addBodyPart(part);
+		mimeMessage.setContent(multiPart);
+
+		return mimeMessage;
+	}
+
+	private static String getSubType(String subType) {
+		return subType != null ? subType : DEFAULT_SUBTYPE;
+	}
+}
---
+++ api/api-javascript/api-mail/src/main/resources/mail/v4/client.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2010-2019 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   SAP - initial API and implementation
+ */
+exports.getClient = function(properties) {
+	var native = null;
+	if (properties) {
+		native = new org.eclipse.dirigible.api.v3.mail.MailFacade.getInstance(toJavaProperties(properties));
+	} else {
+		native = new org.eclipse.dirigible.api.v3.mail.MailFacade.getInstance();
+	}
+	return new MailClient(native);
+};
+
+exports.send = function(from, recipients, subject, text, subType) {
+	var mailClient = this.getClient();
+	mailClient.send(from, recipients, subject, text, subType);
+};
+
+function MailClient(native) {
+	this.native = native;
+
+	this.send = function(from, recipients, subject, text, subType) {
+		var to = [];
+		var cc = [];
+		var bcc = [];
+		if (typeof recipients === "string") {
+			to.push(recipients);
+		} else if (typeof recipients === "object") {
+			to = parseRecipients(recipients, "to");
+			cc = parseRecipients(recipients, "cc");
+			bcc = parseRecipients(recipients, "bcc");
+		} else {
+			var errorMessage = "Invalid 'recipients' format: " + JSON.stringify(recipients);
+			console.error(errorMessage);
+			throw new Error(errorMessage);
+		}
+		this.native.send(from, to, cc, bcc, subject, text, subType ? subType : "plain");
+	};
+}
+
+function toJavaProperties(properties) {
+	var javaProperties = new java.util.Properties();
+	Object.keys(properties).forEach(function(e) {
+		javaProperties.put(e, properties[e]);
+	});
+	return javaProperties;
+}
+
+function parseRecipients(recipients, type) {
+	var objectType = typeof recipients[type];
+	if (objectType === "string") {
+		return [recipients[type]];
+	} else if (Array.isArray(recipients[type])) {
+		return recipients[type];
+	} else if (objectType === "undefined") {
+		return [];
+	}
+	var errorMessage = "Invalid 'recipients." + type + "' format: [" + recipients[type] + "|" + objectType + "]";
+	console.error(errorMessage);
+	throw new Error(errorMessage);
+}
