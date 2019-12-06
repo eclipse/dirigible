19cf083d5896b2d7bb96074b1e020a616b0f8e4a
DimoHG <dimo.georgiev@outlook.com>
2019-12-06 13:53:12 +0200

    Added empty constructor
    Signed-off-by: DimoHG <dimo.georgiev@outlook.com>

--- modules/database/database-persistence/src/main/java/org/eclipse/dirigible/database/persistence/model/PersistenceTableColumnModel.java
+++ modules/database/database-persistence/src/main/java/org/eclipse/dirigible/database/persistence/model/PersistenceTableColumnModel.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2018 SAP and others.
+ * Copyright (c) 2010-2019 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -81,6 +81,12 @@ public class PersistenceTableColumnModel {
 		this.unique = unique;
 		this.identity = identity;
 		this.enumerated = enumerated;
+	}
+
+	/**
+	 * Empty constructor
+	 */
+	public PersistenceTableColumnModel() {
 	}
 
 	/**
