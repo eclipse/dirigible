c634c79b1631847ee7f7c14e3bea7d2270ffd8d4
delchev <delchevn@gmail.com>
2020-01-17 19:06:19 +0200

    alter table including foreign keys support

--- modules/database/database-sql/src/main/java/org/eclipse/dirigible/database/sql/builders/table/AlterTableBuilder.java
+++ modules/database/database-sql/src/main/java/org/eclipse/dirigible/database/sql/builders/table/AlterTableBuilder.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2019 SAP and others.
+ * Copyright (c) 2010-2020 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,12 @@
  */
 package org.eclipse.dirigible.database.sql.builders.table;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.eclipse.dirigible.database.sql.DataType;
 import org.eclipse.dirigible.database.sql.ISqlDialect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,6 +28,8 @@ public class AlterTableBuilder extends AbstractTableBuilder<AlterTableBuilder> {
 	private static final Logger logger = LoggerFactory.getLogger(AlterTableBuilder.class);
 
 	private String action = null;
+	
+	private List<String[]> foreignKeys = new ArrayList<String[]>();
 
 	/**
 	 * Instantiates a new creates the table builder.
@@ -53,6 +61,31 @@ public class AlterTableBuilder extends AbstractTableBuilder<AlterTableBuilder> {
 	protected String getAction() {
 		return action;
 	}
+	
+	/**
+	 * Gets the foreignKeys list
+	 * 
+	 * @return the foreignKeys
+	 */
+	public List<String[]> getForeignKeys() {
+		return foreignKeys;
+	}
+	
+	/**
+	 * Foreign Key
+	 * 
+	 * @param name the name of the foreign key
+	 * @param columns the local columns
+	 * @param referenceTable the reference table
+	 * @param referenceColumns the referenced columns
+	 * @return the AlterTableBuilder object
+	 */
+	public AlterTableBuilder foreignKey(String name, String columns, String referenceTable, String referenceColumns) {
+		logger.trace("foreignKey: " + name + ", columns: " + columns + ", referenceTable: " + referenceTable + ", referenceColumns: " + referenceColumns);
+		String[] foreignKey = new String[] { name,  columns, referenceTable, referenceColumns};
+		this.foreignKeys.add(foreignKey);
+		return this;
+	}
 
 	/*
 	 * (non-Javadoc)
@@ -73,12 +106,22 @@ public class AlterTableBuilder extends AbstractTableBuilder<AlterTableBuilder> {
 
 		if (KEYWORD_ADD.equals(this.action)) {
 			sql.append(KEYWORD_ADD);
-			// COLUMNS
-			generateColumns(sql);
+			if (!getColumns().isEmpty()) {
+				// COLUMNS
+				generateColumns(sql);
+			} else if (!getForeignKeys().isEmpty()) {
+				// FOREIGN KEYS
+				generateForeignKeys(sql);
+			}
 		} else if (KEYWORD_DROP.equals(this.action)) {
 			sql.append(KEYWORD_DROP);
-			// COLUMNS
-			generateColumnNames(sql);
+			if (!getColumns().isEmpty()) {
+				// COLUMNS
+				generateColumnNames(sql);
+			} else if (!getForeignKeys().isEmpty()) {
+				// FOREIGN KEYS
+				generateForeignKeyNames(sql);
+			}
 		} else {
 			logger.error("Action for alter table must be present and can be only ADD or DROP.");
 		}
@@ -89,7 +132,28 @@ public class AlterTableBuilder extends AbstractTableBuilder<AlterTableBuilder> {
 
 		return generated;
 	}
+
+	private void generateForeignKeys(StringBuilder sql) {
+		if (this.foreignKeys.isEmpty()) {
+			throw new IllegalArgumentException("Foreign keys array is empty while trying to alter a table foreign keys - " + this.getTable());
+		}
+		StringBuilder snippet = new StringBuilder();
+		snippet.append(SPACE).append(KEYWORD_CONSTRAINT)
+			.append(SPACE).append(this.foreignKeys.get(0))
+			.append(SPACE).append(KEYWORD_FOREIGN).append(SPACE).append(KEYWORD_KEY)
+			.append(SPACE).append("(" + this.foreignKeys.get(1) + ")")
+			.append(SPACE).append(KEYWORD_REFERENCES)
+			.append(SPACE).append(this.foreignKeys.get(2)).append("(" + this.foreignKeys.get(3) + ")");
+		sql.append(snippet.toString());
+	}
 	
+	private void generateForeignKeyNames(StringBuilder sql) {
+		StringBuilder snippet = new StringBuilder();
+		snippet.append(SPACE).append(KEYWORD_FOREIGN).append(SPACE).append(KEYWORD_KEY)
+			.append(SPACE).append(this.foreignKeys.get(0));
+		sql.append(snippet.toString());		
+	}
+
 //	/**
 //	 * Traverse columns.
 //	 *
