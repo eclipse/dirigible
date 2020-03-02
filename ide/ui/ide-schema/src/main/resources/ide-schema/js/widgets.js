f8dbb53b0b4c1ceb2edc2375b8d9556282e9a54d
delchev <delchevn@gmail.com>
2020-03-02 14:00:04 +0200

    Fixed: #411 - Add tooltips for all icons

--- ide/ui/ide-schema/src/main/resources/ide-schema/js/widgets.js
+++ ide/ui/ide-schema/src/main/resources/ide-schema/js/widgets.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2019 SAP and others.
+ * Copyright (c) 2010-2020 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,7 @@
  */
 function addToolbarButton(editor, toolbar, action, label, image, isTransparent) {
 	var button = document.createElement('button');
+	button.title = label;
 	// button.style.fontSize = '10';
 	if (image !== null) {
 		var img = document.createElement('i');
