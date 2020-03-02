b0623db135385ff56e6c93e7b014179d52743a03
delchev <delchevn@gmail.com>
2020-03-02 14:36:05 +0200

    Fixed: #409 - SQL View - Add Execute Button

--- ide/ui/ide-database/src/main/resources/ide-database/explorer/explorer.js
+++ ide/ui/ide-database/src/main/resources/ide-database/explorer/explorer.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2019 SAP and others.
+ * Copyright (c) 2010-2020 SAP and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -236,6 +236,10 @@ angular.module('database', []).controller('DatabaseController', function ($scope
 		localStorage.setItem('DIRIGIBLE.database', JSON.stringify({"type": $scope.selectedDatabase, "name": $scope.selectedDatasource}));
 		messageHub.post($scope.selectedDatasource, 'database.datasource.selection.changed');
 		$scope.refreshDatabase();
+	};
+	
+	$scope.runSQL = function(evt) {
+		messageHub.post({}, 'database.sql.run');
 	};
 
 });
