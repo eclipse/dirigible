69db5106405b866a585ec7fd8b852fd3f2527e0a
delchev <delchevn@gmail.com>
2020-06-15 18:27:03 +0300

    soap v4 initial

---
+++ api/api-javascript/api-net/src/main/resources/net/v3/streams.js
@@ -0,0 +1,211 @@
+/*
+ * Copyright (c) 2010-2020 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *   SAP - initial API and implementation
+ */
+var rhino_and_nashorn_only = "Net Streams module is supported only with Mozilla Rhino and Nashorn";
+
+/**
+ * Read the stream content as a byte array
+ */
+exports.read = function(inputStream) {
+	var internalBytes;
+	if (__engine === "nashorn") {
+		internalBytes = $.getIOUtils().class.static.toByteArray(inputStream.getInternalObject());
+	} else if (__engine === "rhino") {
+		internalBytes = $.getIOUtils().toByteArray(inputStream.getInternalObject());
+	} else {
+		console.error(rhino_and_nashorn_only);
+		throw new Error(rhino_and_nashorn_only);
+	}
+	return exports.toJavaScriptBytes(internalBytes);
+};
+
+/**
+ * Read the stream to a byte array
+ */
+exports.write = function(outputStream, bytes) {
+	var internalBytes = exports.toJavaBytes(bytes);
+	if (__engine === "nashorn") {
+		$.getIOUtils().class.static.write(internalBytes, outputStream.getInternalObject());
+	} else if (__engine === "rhino") {
+		$.getIOUtils().write(internalBytes, outputStream.getInternalObject());
+	} else {
+		console.error(rhino_and_nashorn_only);
+		throw new Error(rhino_and_nashorn_only);
+	}
+};
+
+/**
+ * Read the stream content as a String
+ */
+exports.readText = function(inputStream) {
+	var internalBytes;
+	if (__engine === "nashorn") {
+		internalBytes = $.IOUtils.class.static.toByteArray(inputStream.getInternalObject());
+	} else if (__engine === "rhino") {
+		internalBytes = $.IOUtils.toByteArray(inputStream.getInternalObject());
+	} else {
+		console.error(rhino_and_nashorn_only);
+		throw new Error(rhino_and_nashorn_only);
+	}
+	return String.fromCharCode.apply(String, exports.toJavaScriptBytes(internalBytes));
+};
+
+/**
+ * Read the stream to a byte array
+ */
+exports.writeText = function(outputStream, text) {
+	if (__engine === "nashorn") {
+		$.getIOUtils().class.static.write(text, outputStream.getInternalObject());
+	} else if (__engine === "rhino") {
+		$.getIOUtils().write(text, outputStream.getInternalObject());
+	} else {
+		console.error(rhino_and_nashorn_only);
+		throw new Error(rhino_and_nashorn_only);
+	}
+};
+
+/**
+ * Copy the input stream content to an output stream
+ */
+exports.copy = function(inputStream, outputStream) {
+	if (__engine === "nashorn") {
+		$.getIOUtils().class.static.copy(inputStream.getInternalObject(), outputStream.getInternalObject());
+	} else if (__engine === "rhino") {
+		$.getIOUtils().copy(inputStream.getInternalObject(), outputStream.getInternalObject());
+	} else {
+		console.error(rhino_and_nashorn_only);
+		throw new Error(rhino_and_nashorn_only);
+	}
+};
+
+/**
+ * Copy the input stream large (>2GB) content to an output stream
+ */
+exports.copyLarge = function(inputStream, outputStream) {
+	if (__engine === "nashorn") {
+		$.getIOUtils().class.static.copyLarge(inputStream.getInternalObject(), outputStream.getInternalObject());
+	} else if (__engine === "rhino") {
+		$.getIOUtils().copyLarge(inputStream.getInternalObject(), outputStream.getInternalObject());
+	} else {
+		console.error(rhino_and_nashorn_only);
+		throw new Error(rhino_and_nashorn_only);
+	}
+};
+
+/**
+ * InputStream object. To be used internally by the API layer
+ */
+exports.InputStream = function(internalInputStream) {
+	this.internalInputStream = internalInputStream;
+	this.getInternalObject = inputStreamGetInternalObject;
+};
+
+function inputStreamGetInternalObject() {
+	return this.internalInputStream;
+}
+
+/**
+ * OutputStream object. To be used internally by the API layer
+ */
+exports.OutputStream = function(internalOutputStream) {
+	this.internalOutputStream = internalOutputStream;
+
+	this.getInternalObject = function() {
+		return this.internalOutputStream;
+	};
+};
+
+/**
+ * Create an ByteArrayInputStream for byte array provided
+ */
+exports.createByteArrayInputStream = function(bytes) {
+	return new exports.ByteArrayInputStream(bytes);
+};
+
+/**
+ * ByteArrayInputStream object.
+ */
+exports.ByteArrayInputStream = function(bytes) {
+	var internalBytes = exports.toJavaBytes(bytes);
+
+	this.internalInputStream = new java.io.ByteArrayInputStream(internalBytes);
+
+	this.getInternalObject = function() {
+		return this.internalInputStream;
+	};
+};
+
+/**
+ * Create a ByteArrayOutputStream
+ */
+exports.createByteArrayOutputStream = function() {
+	return new exports.ByteArrayOutputStream();
+};
+
+/**
+ * ByteArrayOutputStream object.
+ */
+exports.ByteArrayOutputStream = function() {
+	this.internalOutputStream = new java.io.ByteArrayOutputStream();
+
+	this.getInternalObject = function() {
+		return this.internalOutputStream;
+	};
+
+	this.getBytes = function() {
+		var internalBytes = this.internalOutputStream.toByteArray();
+		return exports.toJavaScriptBytes(internalBytes);
+	};
+
+	this.getText = function() {
+		var bytes = this.getBytes();
+		var text = String.fromCharCode.apply(String, bytes);
+		return text;
+	};
+};
+
+/**
+ * Convert the native JavaScript byte array to Java one. To be used internally by the API layer
+ */
+exports.toJavaBytes = function(bytes) {
+	var internalBytes = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, bytes.length);
+	for (var i=0; i<bytes.length; i++) {
+		internalBytes[i] = bytes[i];
+	}
+	return internalBytes;
+};
+
+/**
+ * Convert the Java byte array to a native JavaScript one. To be used internally by the API layer
+ */
+exports.toJavaScriptBytes = function(internalBytes) {
+	var bytes = [];
+	for (var i=0; i<internalBytes.length; i++) {
+		bytes.push(internalBytes[i]);
+	}
+	return bytes;
+};
+
+/**
+ * Converts a text to a byte array
+ */
+exports.textToByteArray = function(text) {
+	var internalString = new java.lang.String(text);
+	var internalBytes = internalString.getBytes();
+	return exports.toJavaScriptBytes(internalBytes);
+};
+
+/**
+ * Converts a text to a byte array
+ */
+exports.byteArrayToText = function(bytes) {
+	var internalBytes = exports.toJavaBytes(bytes);
+	return String.fromCharCode.apply(String, exports.toJavaScriptBytes(internalBytes));
+};
---
+++ api/api-javascript/api-net/src/main/resources/net/v4/soap.js
@@ -0,0 +1,256 @@
+/*
+ * Copyright (c) 2010-2020 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ *   SAP - initial API and implementation
+ */
+var streams = require("io/v4/streams");
+var request = require('http/v4/request');
+
+exports.createMessage = function() {
+	var internalFactory = javax.xml.soap.MessageFactory.newInstance();
+	var internalMessage = internalFactory.createMessage();
+	return new Message(internalMessage);
+};
+
+exports.parseMessage = function(mimeHeaders, inputStream) {
+	var internalFactory = javax.xml.soap.MessageFactory.newInstance();
+	if (inputStream.native) {
+		try {
+			var internalMessage = internalFactory.createMessage(mimeHeaders.native, inputStream.native);
+			var internalPart = internalMessage.getSOAPPart();
+			internalPart.getEnvelope();
+			return new Message(internalMessage);
+		} catch(e) {
+			console.error(e);
+			throw new Error("Input provided is null or in a worng format. HTTP method used must be POST. " + e.message);
+		}
+	}
+	throw new Error("Input provided is null.");
+};
+
+exports.parseRequest = function() {
+	if (request.getMethod().toUpperCase() !== "POST") {
+		throw new Error("HTTP method used must be POST.");	
+	}
+
+	var inputStream = request.getInput();
+	var mimeHeaders = exports.createMimeHeaders();
+	return exports.parseMessage(mimeHeaders, inputStream);
+};
+
+exports.createMimeHeaders = function() {
+	var internalMimeHeaders = javax.xml.soap.MimeHeaders();
+	return new MimeHeaders(internalMimeHeaders);
+};
+
+
+/**
+ * SOAP Message
+ */
+function Message(internalMessage) {
+	this.internalMessage = internalMessage;
+    this.native = internalMessage;
+
+	this.getPart = function() {
+		var internalPart = this.internalMessage.getSOAPPart();
+		return new Part(internalPart);
+	};
+
+	this.getMimeHeaders = function() {
+		var internalMimeHeaders = this.internalMessage.getMimeHeaders();
+		return new MimeHeaders(internalMimeHeaders);
+	};
+
+	this.save = function() {
+		this.internalMessage.saveChanges();
+	};
+
+	this.getText = function() {
+		var outputStream = streams.createByteArrayOutputStream();
+		this.internalMessage.writeTo(outputStream.native);
+		return outputStream.getText();
+	};
+}
+
+/**
+ * SOAP Part
+ */
+function Part(internalPart) {
+	this.internalPart = internalPart;
+    this.native = internalPart;
+
+	this.getEnvelope = function() {
+		var internalEnvelope = this.internalPart.getEnvelope();
+		return new Envelope(internalEnvelope);
+	};
+}
+
+/**
+ * SOAP Mime Headers
+ */
+function MimeHeaders(internalMimeHeaders) {
+	this.internalMimeHeaders = internalMimeHeaders;
+    this.native = internalMimeHeaders;
+
+	this.addHeader = function(name, value) {
+		this.internalMimeHeaders.addHeader(name, value);
+	};
+}
+
+/**
+ * SOAP Envelope
+ */
+function Envelope(internalEnvelope) {
+	this.internalEnvelope = internalEnvelope;
+    this.native = internalEnvelope;
+
+	this.addNamespaceDeclaration = function(prefix, uri) {
+		this.internalEnvelope.addNamespaceDeclaration(prefix, uri);
+	};
+
+	this.getBody = function() {
+		var internalBody = this.internalEnvelope.getBody();
+		return new Body(internalBody);
+	};
+
+	this.getHeader = function() {
+		var internalHeader = this.internalEnvelope.getHeader();
+		return new Header(internalHeader);
+	};
+
+	this.createName = function(localName, prefix, uri) {
+		var internalName = this.internalEnvelope.createName(localName, prefix, uri);
+		return new Name(internalName);
+	};
+}
+
+/**
+ * SOAP Body
+ */
+function Body(internalBody) {
+	this.internalBody = internalBody;
+    this.native = internalBody;
+
+	this.addChildElement = function(localName, prefix) {
+		var internalElement = this.internalBody.addChildElement(localName, prefix);
+		return new Element(internalElement);
+	};
+
+	this.getChildElements = function() {
+		var childElements = [];
+		var internalElementsIterator = this.internalBody.getChildElements();
+		while (internalElementsIterator.hasNext()) {
+			var internalElement = internalElementsIterator.next();
+			childElements.push(new Element(internalElement));
+		}
+		return childElements;
+	};
+}
+
+/**
+ * SOAP Header
+ */
+function Header(internalHeader) {
+	this.internalHeader = internalHeader;
+    this.native = internalHeader;
+
+	this.addHeaderElement = function(name) {
+		var internalElement = this.internalHeader.addHeaderElement(name.native);
+		return new Element(internalElement);
+	};
+}
+
+/**
+ * SOAP Name
+ */
+function Name(internalName) {
+	this.internalName = internalName;
+    this.native = internalName;
+
+	this.getLocalName = function() {
+		return this.internalName.getLocalName();
+	};
+
+	this.getPrefix = function() {
+		return this.internalName.getPrefix();
+	};
+
+	this.getQualifiedName = function() {
+		return this.internalName.getQualifiedName();
+	};
+
+	this.getURI = function() {
+		return this.internalName.getURI();
+	};
+}
+
+/**
+ * SOAP Element
+ */
+function Element(element) {
+	this.internalElement = element;
+    this.native = element;
+
+	this.addChildElement = function(localName, prefix) {
+		var internalElement = this.internalElement.addChildElement(localName, prefix);
+		return new Element(internalElement);
+	};
+
+	this.addTextNode = function(text) {
+		var internalElement = this.internalElement.addTextNode(text);
+		return new Element(internalElement);
+	};
+
+	this.addAttribute = function(name, value) {
+		var internalElement = this.internalElement.addAttribute(name.native, value);
+		return new Element(internalElement);
+	};
+
+	this.getChildElements = function() {
+		var childElements = [];
+		var internalElementsIterator = this.internalElement.getChildElements();
+		while (internalElementsIterator.hasNext()) {
+			var internalElement = internalElementsIterator.next();
+			childElements.push(new Element(internalElement));
+		}
+		return childElements;
+	};
+
+	this.getElementName = function() {
+		try {
+			var internalName = this.internalElement.getElementName();
+			return new Name(internalName);
+		} catch(e) {
+		//  can we assume that always an exception here means the element is not an SOAPElement
+		//	console.log(e);
+		}
+		return null;
+	};
+
+	this.getValue = function() {
+		return this.internalElement.getValue();
+	};
+
+	this.isSOAPElement = function() {
+		return this.getElementName() !== null;
+	};
+}
+
+/**
+ * Call a given SOAP endpoint with a given request message
+ */
+exports.call = function(message, url) {
+	var soapConnectionFactory = javax.xml.soap.SOAPConnectionFactory.newInstance();
+	var internalConnection = soapConnectionFactory.createConnection();
+	var internalResponse = internalConnection.call(message.native, url);
+	return new Message(internalResponse);
+};
+
+exports.trustAll = function() {
+	// TODO
+};
