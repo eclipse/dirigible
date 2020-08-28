b56d7778bea9a5ba26be61ca908f09a6ed634cb3
delchev <delchevn@gmail.com>
2020-08-28 11:43:07 +0300

    OData rest layer added by default in the full stack application

--- templates/template-application-angular/src/main/resources/template-application-angular/template/template.js
+++ templates/template-application-angular/src/main/resources/template-application-angular/template/template.js
@@ -12,25 +12,29 @@
 var schemaTemplateManager = require("template-application-schema/template/template");
 var feedTemplateManager = require("template-application-feed/template/template");
 var uiAngularjsTemplateManager = require("template-application-ui-angular/template/template");
+var odataTemplateManager = require("template-application-odata/template/template");
 
 exports.getTemplate = function (parameters) {
 	let schemaTemplate = schemaTemplateManager.getTemplate(parameters);
     let feedTemplate = feedTemplateManager.getTemplate(parameters);
     let uiAngularjsTemplate = uiAngularjsTemplateManager.getTemplate(parameters);
+    let odataTemplate = odataTemplateManager.getTemplate(parameters);
 
     let templateSources = [];
 	templateSources = templateSources.concat(schemaTemplate.sources);
     templateSources = templateSources.concat(feedTemplate.sources);
     templateSources = templateSources.concat(uiAngularjsTemplate.sources);
+    templateSources = templateSources.concat(odataTemplate.sources);
 
     let templateParameters = [];
 	templateParameters = templateParameters.concat(schemaTemplate.parameters);
     templateParameters = templateParameters.concat(feedTemplate.parameters);
     templateParameters = templateParameters.concat(uiAngularjsTemplate.parameters);
+    templateParameters = templateParameters.concat(odataTemplate.parameters);
 
     return {
-        name: "Full-stack Application (AngularJS)",
-        description: "Full-stack Application with a Database Schema, a set of REST Services and an AngularJS User Interfaces",
+        name: "Application - full stack (AngularJS)",
+        description: "Application - full stack with a Database Schema, a set of REST Services and an AngularJS User Interfaces",
         extension: "model",
         sources: templateSources,
         parameters: templateParameters
