b202d4c3019a12b77b3908c561c89594ee04be2e
delchev <delchevn@gmail.com>
2020-12-07 01:31:17 +0200

    Fixed: #673 - MongoDB based Full-stack Application Templates

---
+++ templates/template-application-ui-angular-mongodb/src/main/resources/template-application-ui-angular-mongodb/template/ui/manage.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+exports.getSources = function(parameters) {
+    return [{
+        location: "/template-application-ui-angular/ui/perspectives/views/manage/index.html.template", 
+        action: "generate",
+        rename: "ui/{{perspectiveName}}/views/{{fileName}}/index.html",
+        engine: "velocity",
+        collection: "uiManageModels"
+    }, {
+        location: "/template-application-ui-angular/ui/perspectives/views/manage/controller.js.template", 
+        action: "generate",
+        rename: "ui/{{perspectiveName}}/views/{{fileName}}/controller.js",
+        engine: "velocity",
+        collection: "uiManageModels"
+    }, {
+        location: "/template-application-ui-angular/ui/perspectives/views/manage/extensions/view.js.template", 
+        action: "generate",
+        rename: "ui/{{perspectiveName}}/views/{{fileName}}/extensions/view.js",
+        collection: "uiManageModels"
+    }, {
+		location: "/template-application-ui-angular/ui/perspectives/views/manage/extensions/view.extension.template", 
+		action: "generate",
+		rename: "ui/{{perspectiveName}}/views/{{fileName}}/extensions/view.extension",
+		collection: "uiManageModels"
+	}, {
+        location: "/template-application-ui-angular/ui/perspectives/views/manage/extensions/menu/item.extension.template", 
+        action: "generate",
+        rename: "ui/{{perspectiveName}}/views/{{fileName}}/extensions/menu/item.extension",
+        collection: "uiManageModels"
+    }, {
+        location: "/template-application-ui-angular/ui/perspectives/views/manage/extensions/menu/item.js.template", 
+        action: "generate",
+        rename: "ui/{{perspectiveName}}/views/{{fileName}}/extensions/menu/item.js",
+        collection: "uiManageModels"
+    }];
+};
---
+++ templates/template-application-ui-angular-mongodb/src/main/resources/template-application-ui-angular-mongodb/template/ui/reportBar.js
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+exports.getSources = function(parameters) {
+	return [{
+		location: "/template-application-ui-angular/ui/perspectives/views/report/bar/index.html.template", 
+		action: "generate",
+		rename: "ui/{{perspectiveName}}/views/{{fileName}}/index.html",
+		engine: "velocity",
+		collection: "uiReportBarsModels"
+	}, {
+		location: "/template-application-ui-angular/ui/perspectives/views/report/bar/controller.js.template", 
+		action: "generate",
+		rename: "ui/{{perspectiveName}}/views/{{fileName}}/controller.js",
+		engine: "velocity",
+		collection: "uiReportBarsModels"
+	}, {
+		location: "/template-application-ui-angular/ui/perspectives/views/report/bar/extensions/view.js.template", 
+		action: "generate",
+		rename: "ui/{{perspectiveName}}/views/{{fileName}}/extensions/view.js",
+		collection: "uiReportBarsModels"
+	}, {
+		location: "/template-application-ui-angular/ui/perspectives/views/report/bar/extensions/view.extension.template", 
+		action: "generate",
+		rename: "ui/{{perspectiveName}}/views/{{fileName}}/extensions/view.extension",
+		collection: "uiReportBarsModels"
+	}, {
+		location: "/template-application-ui-angular/ui/perspectives/views/report/bar/extensions/menu/item.extension.template", 
+		action: "generate",
+		rename: "ui/{{perspectiveName}}/views/{{fileName}}/extensions/menu/item.extension",
+		collection: "uiReportBarsModels"
+	}, {
+		location: "/template-application-ui-angular/ui/perspectives/views/report/bar/extensions/menu/item.js.template", 
+		action: "generate",
+		rename: "ui/{{perspectiveName}}/views/{{fileName}}/extensions/menu/item.js",
+		collection: "uiReportBarsModels"
+	}];
+};
