00a7a7c256ddcf1768b527b61e49adce8ce1eb7d
delchev <delchevn@gmail.com>
2021-01-27 13:45:51 +0200

    Fixed: #692 - In 'case sensitive' mode complex expression are put in quotes

--- modules/database/database-sql/src/main/java/org/eclipse/dirigible/database/sql/builders/AbstractSqlBuilder.java
+++ modules/database/database-sql/src/main/java/org/eclipse/dirigible/database/sql/builders/AbstractSqlBuilder.java
@@ -1,15 +1,18 @@
 /*
- * Copyright (c) 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * Copyright (c) 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-FileCopyrightText: 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.dirigible.database.sql.builders;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.dirigible.commons.config.Configuration;
 import org.eclipse.dirigible.database.sql.ISqlBuilder;
@@ -77,10 +80,26 @@ public abstract class AbstractSqlBuilder implements ISqlBuilder {
 	 * @return the encapsulated name
 	 */
 	protected String encapsulate(String name) {
-		if (!name.startsWith("\"")) {
+		if (!name.startsWith("\"")
+				&& !"*".equals(name.trim())
+				&& isColumn(name.trim())) {
 			name = "\"" + name + "\"";
 		}
 		return name;
+	}
+	
+	/**
+	 * Check whether the name is a column (one word) or it is complex expression containing functions, etc. (count(*))
+	 * @param name the name of the eventual column
+	 * @return true if it is one word
+	 */
+	protected boolean isColumn(String name) {
+		String pattern = "^([\\w\\-]+)";
+		Pattern r = Pattern.compile(pattern);
+		Matcher m = r.matcher(name);
+		m.find();
+		String found = m.group(1);
+		return name.equals(found);
 	}
 
 }
--- modules/database/database-sql/src/test/java/org/eclipse/dirigible/database/sql/test/SelectTest.java
+++ modules/database/database-sql/src/test/java/org/eclipse/dirigible/database/sql/test/SelectTest.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * Copyright (c) 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-FileCopyrightText: 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.dirigible.database.sql.test;
@@ -14,6 +14,7 @@ package org.eclipse.dirigible.database.sql.test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.eclipse.dirigible.commons.config.Configuration;
 import org.eclipse.dirigible.database.sql.SqlFactory;
 import org.junit.Test;
 
@@ -368,6 +369,54 @@ public class SelectTest {
 		
 		assertNotNull(sql);
 		assertEquals("SELECT * FROM CUSTOMERS", sql);
+	}
+	
+	public void selectStarCaseSensitive() {
+		Configuration.get("DIRIGIBLE_DATABASE_NAMES_CASE_SENSITIVE", "true");
+		try {
+			String sql = SqlFactory.getDefault()
+				.select()
+				.column("*")
+				.from("CUSTOMERS")
+				.build();
+			
+			assertNotNull(sql);
+			assertEquals("SELECT * FROM \"CUSTOMERS\"", sql);
+		} finally {
+			Configuration.get("DIRIGIBLE_DATABASE_NAMES_CASE_SENSITIVE", "false");
+		}
+	}
+	
+	public void selectCountCaseSensitive() {
+		Configuration.get("DIRIGIBLE_DATABASE_NAMES_CASE_SENSITIVE", "true");
+		try {
+			String sql = SqlFactory.getDefault()
+				.select()
+				.column("COUNT(*)")
+				.from("CUSTOMERS")
+				.build();
+			
+			assertNotNull(sql);
+			assertEquals("SELECT COUNT(*) FROM \"CUSTOMERS\"", sql);
+		} finally {
+			Configuration.get("DIRIGIBLE_DATABASE_NAMES_CASE_SENSITIVE", "false");
+		}
+	}
+	
+	public void selectColumnCaseSensitive() {
+		Configuration.get("DIRIGIBLE_DATABASE_NAMES_CASE_SENSITIVE", "true");
+		try {
+			String sql = SqlFactory.getDefault()
+				.select()
+				.column("FIRST_NAME")
+				.from("CUSTOMERS")
+				.build();
+			
+			assertNotNull(sql);
+			assertEquals("SELECT \"FIRST_NAME\" FROM \"CUSTOMERS\"", sql);
+		} finally {
+			Configuration.get("DIRIGIBLE_DATABASE_NAMES_CASE_SENSITIVE", "false");
+		}
 	}
 
 }
