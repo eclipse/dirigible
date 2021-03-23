3aeb3d42a8f64d486229911840e46d6de1066091
ThuF <jordan.p.pavlov@gmail.com>
2021-03-23 14:43:06 +0200

    Fixes #763 [API] PDF

---
+++ api/api-javascript/api-documents/src/main/resources/documents/extensions/modules.js
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2010-2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+exports.getContent = function() {
+	return [{
+		name: "documents/v4/pdf",
+		description: "PDF API"
+	}];
+};
