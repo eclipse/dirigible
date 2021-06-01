a7fdda48778acb794ea98d069d6c2d67cf32fdf6
StanZGenchev <stanislav.genchev@sap.com>
2021-06-01 20:10:46 +0300

    Updated ide-core
    Signed-off-by: StanZGenchev <stanislav.genchev@sap.com>

--- ide/ui/ide-core/src/main/resources/ide-core/ui/ui-layout.js
+++ ide/ui/ide-core/src/main/resources/ide-core/ui/ui-layout.js
@@ -8,46 +8,46 @@
  * Contributors:
  *   SAP - initial API and implementation
  */
-var ViewRegistry = function() {
-	
+var ViewRegistry = function () {
+
 	var _views = {};
-	
+
 	var _factories = {};
-	
-	this.factory = function(name, func){
-		if(arguments.length == 1)
+
+	this.factory = function (name, func) {
+		if (arguments.length == 1)
 			return _factories[name];
-			
-		if(typeof func !== 'function')
+
+		if (typeof func !== 'function')
 			throw Error('Not a function');
 		_factories[name] = func;
 		return this;
 	}
-	
-	this.factories = function(){
+
+	this.factories = function () {
 		return _factories;
 	}
-	
-	this.view = function(viewid, factory, region, label, settings){
-		if(viewid === undefined)
+
+	this.view = function (viewid, factory, region, label, settings) {
+		if (viewid === undefined)
 			throw Error('Illegal argument for viewid ' + viewid);
 		//get view
-		if(arguments.length == 1)
+		if (arguments.length == 1)
 			return _views[viewid];
 		//set view
-		if(factory === undefined)
+		if (factory === undefined)
 			throw Error('Illegal argument for factory ' + factory);
 		var componentName;
-		if(typeof factory === 'string')
+		if (typeof factory === 'string')
 			componentName = factory;
-		else if(typeof factory === 'function'){
+		else if (typeof factory === 'function') {
 			this.factory(viewid, factory);
 			componentName = viewid;
 		}
 		region = region || 'center-bottom';
 		settings = settings || [];
 		settings.label = label;
-		
+
 		_views[viewid] = {
 			id: viewid,
 			type: 'component',
@@ -57,19 +57,19 @@ var ViewRegistry = function() {
 		}
 		return this;
 	};
-	
-	this.views = function(){
+
+	this.views = function () {
 		return _views;
 	}
-	
+
 	return this;
-	
+
 };
 
-function LayoutController(viewRegistry, messageHub){
+function LayoutController(viewRegistry, messageHub) {
 
 	this.viewRegistry = viewRegistry;
-	
+
 	this.regions = {
 		'main': {
 			id: 'main',
@@ -103,17 +103,17 @@ function LayoutController(viewRegistry, messageHub){
 			defaultRegionId: 'main'
 		},
 		'center-top': {
-			id: 'center-top',				
+			id: 'center-top',
 			type: 'stack',
 			defaultRegionId: 'center'
 		},
 		'center-middle': {
-			id: 'center-middle',				
+			id: 'center-middle',
 			type: 'stack',
 			defaultRegionId: 'center'
 		},
 		'center-bottom': {
-			id: 'center-bottom',				
+			id: 'center-bottom',
 			type: 'stack',
 			defaultRegionId: 'center'
 		},
@@ -138,77 +138,77 @@ function LayoutController(viewRegistry, messageHub){
 			defaultRegionId: 'right'
 		}
 	}
-	
-	var parentIds = function(node, regionId, arr){
+
+	var parentIds = function (node, regionId, arr) {
 		arr = arr || [];
 		var _parentId = regionId || node.defaultRegionId;
-		if(!_parentId)
+		if (!_parentId)
 			return arr;
-		if(this.regions[_parentId]){
+		if (this.regions[_parentId]) {
 			arr.push(_parentId);
 			return parentIds.call(this, this.regions[_parentId], undefined, arr);
 		}
 		return arr;
 	};
-	
-	var copy = function(src){
-		return JSON.parse(JSON.stringify(src));	
+
+	var copy = function (src) {
+		return JSON.parse(JSON.stringify(src));
 	};
-	
-	var gridItem = function(top, id){
-		if(top.id === id)
+
+	var gridItem = function (top, id) {
+		if (top.id === id)
 			return top;
-		if(top.content){
-			var item = top.content.find(function(_item){
+		if (top.content) {
+			var item = top.content.find(function (_item) {
 				return gridItem(_item, id);
 			});
 			return item && gridItem(item, id)
 		}
 	};
-	
-	var addView = function(views, view, grid){
+
+	var addView = function (views, view, grid) {
 		var node;
 		var path = [view.id].concat(parentIds.call(this, view));
 		//build branches top-bottom				
-		path.reverse().forEach(function(compId, idx, arr){
+		path.reverse().forEach(function (compId, idx, arr) {
 			var _gridNode = gridItem(grid, compId);
-			if(_gridNode){
+			if (_gridNode) {
 				node = _gridNode;
 				return;//next
 			} else {
 				var child = this.regions[compId] || views[compId];
-				if(child){
+				if (child) {
 					child = copy(child);
-					if(!node.content)
-						node.content = [];	
-					if(!node.content.find(function(it){return it.id == child.id}))
+					if (!node.content)
+						node.content = [];
+					if (!node.content.find(function (it) { return it.id == child.id }))
 						node.content.push(child);
 					node = child;
 				}
 			}
 		}.bind(this));
 	}.bind(this);
-	
-	this.layoutViews = function(views){
+
+	this.layoutViews = function (views) {
 		var grid = copy(this.regions.main);
-		if(views){
-			Object.values(views).map(function(view){
-				if(view){
+		if (views) {
+			Object.values(views).map(function (view) {
+				if (view) {
 					view = copy(view);
-					addView(views, view, grid);			
+					addView(views, view, grid);
 				}
 				return view;
 			}.bind(this));
-		}	
+		}
 		return grid;
 	}.bind(this);
-	
+
 	this.messageHub = messageHub || new FramesMessageHub();
-	
+
 	this.listeners = {};
-	
-	this.addListener = function(eventType, callback){
-		if(!this.listeners[eventType]){
+
+	this.addListener = function (eventType, callback) {
+		if (!this.listeners[eventType]) {
 			this.listeners[eventType] = [];
 		}
 		var subscriber = this.messageHub.subscribe(callback.bind(this), eventType);
@@ -219,16 +219,16 @@ function LayoutController(viewRegistry, messageHub){
 		};
 		this.listeners[eventType].push(entry);
 		return entry.id;
-	};		
-	this.removeListener = function(id, eventType){
-		if(!this.listeners[eventType])
+	};
+	this.removeListener = function (id, eventType) {
+		if (!this.listeners[eventType])
 			return;
 		for (var i = this.listeners[eventType].length - 1; i >= 0; i--) {
-		  if (this.listeners[eventType][i].id == id) {
-			var subscriber = this.listeners[eventType][i].handler;
-			this.messageHub.unsibscribe(subscriber);
-			this.listeners[eventType].splice(i, 1);
-		  }
+			if (this.listeners[eventType][i].id == id) {
+				var subscriber = this.listeners[eventType][i].handler;
+				this.messageHub.unsibscribe(subscriber);
+				this.listeners[eventType].splice(i, 1);
+			}
 		}
 	};
 	/**
@@ -236,28 +236,31 @@ function LayoutController(viewRegistry, messageHub){
 	 * Providing an id and the reconstruct flag set to true will not retrive from local storage last state, but will subscribe and save the reconstructed instance future changes.
 	 * The layout will be always reconstructed on first init regardless of the reconstruct flag.
 	 */
-	this.init = function(containerEl, viewNames, id, reconstruct){
-		
+	this.init = function (containerEl, viewNames, id, reconstruct) {
+
 		this.containerEl = containerEl;
 		this.viewNames = viewNames;
 		id = id || $(containerEl).attr("id");
-		
-		if(id){
-			if(!reconstruct){
+
+		if (id) {
+			if (!reconstruct) {
 				//load from localStorage
-				var savedState = localStorage.getItem('DIRIGIBLE.IDE.GL.state.'+ id);
-				if(savedState !== null) {
+				var savedState = localStorage.getItem('DIRIGIBLE.IDE.GL.state.' + id);
+				if (savedState !== null) {
 					this.config = JSON.parse(savedState);
-				}				
+				}
 			}
-		} 
-		if(!id || reconstruct || !this.config){
+		}
+		if (!id || reconstruct || !this.config) {
 			//reconstruct (ignore previously saved state)
 			var views = {};
-			this.viewNames.forEach(function(viewName){
+			this.viewNames.forEach(function (viewName) {
 				views[viewName] = this.viewRegistry.view(viewName);
 			}.bind(this));
 			this.config = {
+				settings: {
+					showPopoutIcon: false
+				},
 				dimensions: {
 					headerHeight: 26,
 					borderWidth: 3
@@ -265,89 +268,89 @@ function LayoutController(viewRegistry, messageHub){
 				content: [this.layoutViews.call(this, views)]
 			};
 		}
-		
+
 		this.layout = new GoldenLayout(this.config, containerEl);
-		
-		Object.keys(this.viewRegistry.factories()).forEach(function(factoryname){
+
+		Object.keys(this.viewRegistry.factories()).forEach(function (factoryname) {
 			this.layout.registerComponent(factoryname, this.viewRegistry.factory(factoryname));
 		}.bind(this));
-		
-		if(id){
-			this.layout.on('stateChanged', function(){
+
+		if (id) {
+			this.layout.on('stateChanged', function () {
 				//TODO: debounce or do that only with save button! This fires a lot
-				var state = JSON.stringify( this.layout.toConfig() );
-				localStorage.setItem('DIRIGIBLE.IDE.GL.state.'+ id, state );
-			}.bind(this));			
+				var state = JSON.stringify(this.layout.toConfig());
+				localStorage.setItem('DIRIGIBLE.IDE.GL.state.' + id, state);
+			}.bind(this));
 		}
-		
-		this.layout.on('tabCreated', function(tab){
-		   tab.closeElement.off( 'click' ).click(function(){
-		   	if (tab.contentItem.config.componentName && tab.contentItem.config.componentName === 'editor') {
-		   		if (tab.contentItem.config.title && tab.contentItem.config.title.startsWith('*')) {
-			   		if (confirm('You have unsaved changes, are you sure you want to close ' + tab.contentItem.config.title.substring(1))) {
-			        	tab.contentItem.remove();
-			    	}	
-			   	} else {
-			   		tab.contentItem.remove();
-			   	}
-		   	} else {
-		   		if (confirm( 'Are you sure you want to close ' + tab.contentItem.config.title)) {
-		           tab.contentItem.remove();
-		        }
-		   	}
-		   });
+
+		this.layout.on('tabCreated', function (tab) {
+			tab.closeElement.off('click').click(function () {
+				if (tab.contentItem.config.componentName && tab.contentItem.config.componentName === 'editor') {
+					if (tab.contentItem.config.title && tab.contentItem.config.title.startsWith('*')) {
+						if (confirm('You have unsaved changes, are you sure you want to close ' + tab.contentItem.config.title.substring(1))) {
+							tab.contentItem.remove();
+						}
+					} else {
+						tab.contentItem.remove();
+					}
+				} else {
+					if (confirm('Are you sure you want to close ' + tab.contentItem.config.title)) {
+						tab.contentItem.remove();
+					}
+				}
+			});
 		});
-		
+
 		this.layout.init();
 	};
-	
-	this.openView = function(viewId/*, region*/){
+
+	this.openView = function (viewId/*, region*/) {
 		var viewDef = this.viewRegistry.view(viewId);
-		if(viewDef){
+		if (viewDef) {
 			var node;
 			var defaultPath = [viewId];
 			defaultPath = defaultPath.concat(parentIds.call(this, viewDef/*, region*/));//TODO: consider custom region
-			defaultPath.reverse().forEach(function(compId){
-				if(!node)
+			defaultPath.reverse().forEach(function (compId) {
+				if (!node)
 					node = this.layout.root;
 				else
 					node = this.layout.root.getItemsById(node.id || node.config.id)[0];
 				var child = node.getItemsById(compId)[0] || this.regions[compId] || this.viewRegistry.view(compId);
-				if(!node.isRoot && child && this.layout.root.getItemsById(child.id || child.config.id).length<1){
+				if (!node.isRoot && child && this.layout.root.getItemsById(child.id || child.config.id).length < 1) {
 					node.addChild(child);
 				}
 				node = child;
 			}.bind(this));
 		}
 	};
-	
-	this.openEditor = function(resourcePath, resourceLabel, contentType, editorId){
+
+	this.openEditor = function (resourcePath, resourceLabel, contentType, editorId) {
 		var newItemConfig = {
 			id: resourcePath,
 			title: resourceLabel,
 			type: 'component',
 			componentName: 'editor',
-			componentState:{ 
+			componentState: {
 				path: resourcePath,
 				editorId: editorId,
 				contentType: contentType
 			}
 		};
 		//is an editor available to stack new children to it?
-		if(this.layout.root.getItemsById('editor')[0]){
+		if (this.layout.root.getItemsById('editor')[0]) {
 			//is already open?
-			if(this.layout.root.getItemsById(newItemConfig.id).length){
+			if (this.layout.root.getItemsById(newItemConfig.id).length) {
 				//replace content
 				var panel = this.layout.root.getItemsById(newItemConfig.id)[0];
 				//panel.instance.setContent(newItemConfig.id);
 				panel.parent.setActiveContentItem(panel);
 			} else {
 				// open new tab
-				this.layout.root.getItemsById('editor')[0].parent.addChild( newItemConfig );
+				this.layout.root.getItemsById('editor')[0].parent.addChild(newItemConfig);
 			}
 		} else {
 			this.openView('editor');
-			this.layout.root.getItemsById('editor')[0].parent.addChild( newItemConfig );
+			this.layout.root.getItemsById('editor')[0].parent.addChild(newItemConfig);
 		}
 	};
 
@@ -362,42 +365,42 @@ function LayoutController(viewRegistry, messageHub){
 		}
 	}
 
-	this.closeEditor = function(editorId) {
+	this.closeEditor = function (editorId) {
 		var editor = this.layout.root.getItemsById(editorId)[0];
 		closeEditor(editor, this.layout.root);
 	};
 
-	this.closeOtherEditors = function(editorId) {
+	this.closeOtherEditors = function (editorId) {
 		var editorIds = this.layout.root.getItemsById('editor')[0].parent.config.content
 			.filter(e => e.id !== "editor" && e.id !== editorId)
 			.map(e => e.id);
 		var editors = [];
 		editorIds.forEach(e => editors.push(this.layout.root.getItemsById(e)[0]))
-		for (var i = 0; i < editors.length; i ++) {
+		for (var i = 0; i < editors.length; i++) {
 			closeEditor(editors[i], this.layout.root);
 		}
 	};
 
-	this.closeAllEditors = function() {
+	this.closeAllEditors = function () {
 		var editorIds = this.layout.root.getItemsById('editor')[0].parent.config.content
 			.filter(e => e.id !== "editor")
 			.map(e => e.id);
 		var editors = [];
 		editorIds.forEach(e => editors.push(this.layout.root.getItemsById(e)[0]))
-		for (var i = 0; i < editors.length; i ++) {
+		for (var i = 0; i < editors.length; i++) {
 			closeEditor(editors[i], this.layout.root);
 		}
 	};
 
-	this.setEditorDirty = function(resourcePath, dirty){
+	this.setEditorDirty = function (resourcePath, dirty) {
 		//is an editor available to stack new children to it?
-		if(this.layout.root.getItemsById('editor')[0]){
+		if (this.layout.root.getItemsById('editor')[0]) {
 			//is already open?
-			if(this.layout.root.getItemsById(resourcePath).length){
+			if (this.layout.root.getItemsById(resourcePath).length) {
 				var panel = this.layout.root.getItemsById(resourcePath)[0];
 				var title = panel.container.tab.titleElement.text();
 				if (!title.startsWith('*') && dirty) {
-					panel.setTitle('*'+title);
+					panel.setTitle('*' + title);
 				} else {
 					if (title.startsWith('*') && !dirty) {
 						panel.setTitle(title.substring(1, title.length));
@@ -405,14 +408,14 @@ function LayoutController(viewRegistry, messageHub){
 				}
 				panel.parent.setActiveContentItem(panel);
 			}
-		}	
+		}
 	};
-	
-	this.resize = function(){
+
+	this.resize = function () {
 		this.layout.updateSize();
 	};
-	
-	this.reset = function(){
+
+	this.reset = function () {
 		this.layout.destroy();
 		this.containerEl.empty();
 		this.layout.init(this.containerEl, this.viewNames);
@@ -420,7 +423,7 @@ function LayoutController(viewRegistry, messageHub){
 }
 
 function uuidv4() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-	(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-  );
+	return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+		(c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+	);
 }
