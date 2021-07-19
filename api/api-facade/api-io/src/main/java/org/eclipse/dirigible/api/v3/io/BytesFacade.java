c78d5bc94fcf091e05fe27dacc1ca366dc50f826
Teodor <tdermendjievft@gmail.com>
2021-07-19 11:43:46 +0300

    Add int - byte array conversion to BytesFacade

--- api/api-facade/api-io/src/main/java/org/eclipse/dirigible/api/v3/io/BytesFacade.java
+++ api/api-facade/api-io/src/main/java/org/eclipse/dirigible/api/v3/io/BytesFacade.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2010-2020 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.dirigible.api.v3.io;
 
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class BytesFacade {
 	
@@ -24,5 +26,22 @@ public class BytesFacade {
 		byte[] bytes = text.getBytes(charset);
 		return bytes;
 	}
+
+	public static byte[] intToByteArray(int value, String byteOrder) {
+		ByteBuffer buffer = ByteBuffer.allocate(4);
+		ByteOrder order = byteOrder == "BIG_ENDIAN" ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN;
+		buffer.order(order);
+		buffer.putInt(value);
+		return buffer.array();
+	}
+
+	public static int byteArrayToInt(byte[] data, String byteOrder) {
+		ByteBuffer buffer = ByteBuffer.allocate(4);
+		ByteOrder order = byteOrder == "BIG_ENDIAN" ? ByteOrder.BIG_ENDIAN : ByteOrder.LITTLE_ENDIAN;
+		buffer.order(order);
+		buffer.put(data);
+		return buffer.getInt(0);
+	}
+
 
 }
