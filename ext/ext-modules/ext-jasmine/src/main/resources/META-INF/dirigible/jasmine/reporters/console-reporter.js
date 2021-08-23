6808f09e2a0a58c22aaa8b00df4601a8b6377a71
ThuF <jordan.p.pavlov@gmail.com>
2021-08-23 19:24:37 +0300

    License headers updated

--- ext/ext-modules/ext-jasmine/src/main/resources/META-INF/dirigible/jasmine/reporters/console-reporter.js
+++ ext/ext-modules/ext-jasmine/src/main/resources/META-INF/dirigible/jasmine/reporters/console-reporter.js
@@ -1,13 +1,14 @@
-/*******************************************************************************
- * Copyright (c) 2010-2020 SAP and others.
+/*
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
- * Contributors:
- * SAP - initial API and implementation
- *******************************************************************************/
- 
+ *
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
 /* eslint-env node, dirigible */
 
 var print = function(prefix, result, prettyPrint, severity){
--- ide/ui/ide-extensions/src/main/resources/META-INF/dirigible/ide-extensions/extensions/menu/workspace-menu-new-template-extensionpoint.js
+++ ide/ui/ide-extensions/src/main/resources/META-INF/dirigible/ide-extensions/extensions/menu/workspace-menu-new-template-extensionpoint.js
@@ -1,12 +1,13 @@
 /*
- * Copyright (c) 2010-2020 SAP and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
- * Contributors:
- *   SAP - initial API and implementation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
  */
 exports.getTemplate = function() {
 	var template = {
