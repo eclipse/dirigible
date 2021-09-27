cea00ecabf37a8566805c209f95c3f7fb509e047
Fluctuationqt <outrageousxqt@gmail.com>
2021-09-27 17:24:24 +0300

    feat: start working on esm

---
+++ modules/engines/engine-javascript-graalvm/src/test/resources/META-INF/dirigible/graalvm/ecmaScriptImport.mjs
@@ -0,0 +1,6 @@
+//import * as base64 from 'utils/v4/base64.mjs';
+var base64req = require('utils/v4/base64.js');
+//var input = "PT4/";
+//var result = base64.decode(input);
+//console.log("decoded: " + result);
+//console.log("encoded: " + base64req.encode("asd"));
