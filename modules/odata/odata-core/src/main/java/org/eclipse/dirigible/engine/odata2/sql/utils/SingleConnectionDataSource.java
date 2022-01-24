f62f85c9c1bc70f461965106646a682edfe159dd
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-01-24 14:39:51 +0200

    chore: update licensing year

--- modules/odata/odata-core/src/main/java/org/eclipse/dirigible/engine/odata2/sql/utils/SingleConnectionDataSource.java
+++ modules/odata/odata-core/src/main/java/org/eclipse/dirigible/engine/odata2/sql/utils/SingleConnectionDataSource.java
@@ -6,7 +6,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2021 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
 package org.eclipse.dirigible.engine.odata2.sql.utils;
