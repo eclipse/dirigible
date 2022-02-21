723b4a1830d456513f1c0fb39caab508d66f2905
Fluctuationqt <outrageousxqt@gmail.com>
2022-02-21 09:21:33 +0200

    feat: add eslint bundle

--- ide/ui/ide-monaco/src/main/resources/META-INF/dirigible/ide-monaco/js/editor.js
+++ ide/ui/ide-monaco/src/main/resources/META-INF/dirigible/ide-monaco/js/editor.js
@@ -292,6 +292,92 @@ function createEditorInstance(readOnly = false) {
     });
 };
 
+function eslintVerify(code){
+    const config = {
+        "env": {
+           "browser": true,
+           "node": true
+        },
+        "parserOptions": {
+           "sourceType": "module",
+           "ecmaVersion": 2018,
+           "ecmaFeatures": {
+               "impliedStrict": true
+           }
+        },
+        "rules": {
+           "valid-jsdoc": 1,
+           "block-scoped-var": 2,
+           "curly": 2,
+           "default-case": 2,
+           "dot-location": [2, "property"],
+           "eqeqeq": 2,
+           "no-else-return": 1,
+           "no-eval": 2,
+           "no-loop-func": 1,
+           "no-multi-spaces": 1,
+           "no-param-reassign": 2,
+           "no-unused-expressions": 1,
+           "no-unused-vars": 2,
+           "no-warning-comments": 1,
+           "no-with": 2,
+           "strict": 1,
+           "no-shadow": 1,
+           "no-undef": 2,
+           "no-undefined": 2,
+           "no-use-before-define": 2,
+           "no-sync": 1,
+           "array-bracket-spacing": 2,
+           "block-spacing": 2,
+           "brace-style": [2, "1tbs"],
+           "comma-spacing": 2,
+           "comma-style": 2,
+           "computed-property-spacing": 2,
+           "eol-last": 1,
+           "func-call-spacing": 2,
+           "indent": ["error", "tab"],
+           "key-spacing": 2,
+           "keyword-spacing": 2,
+           "line-comment-position": 1,
+           "linebreak-style": 2,
+           "lines-around-comment": 2,
+           "lines-between-class-members": 2,
+           "new-parens": 2,
+           "no-array-constructor": 2,
+           "no-whitespace-before-property": 2,
+           "object-curly-newline": [2, {
+               "consistent": true
+           }],
+           "object-curly-spacing": 2,
+           "quotes": 1,
+           "semi": 2,
+           "space-before-blocks": 2,
+           "space-before-function-paren": [2, {
+               "anonymous": "always",
+               "named": "never",
+               "asyncArrow": "always"
+           }],
+           "space-in-parens": 2,
+           "space-infix-ops": 2,
+           "space-unary-ops": 2,
+           "spaced-comment": 1,
+           "switch-colon-spacing": 2,
+           "arrow-spacing": 2,
+           "prefer-const": 1,
+           "prefer-destructuring": 1,
+           "prefer-rest-params": 1,
+           "prefer-spread": 1,
+           "prefer-template": 1,
+           "require-await": 2,
+           "rest-spread-spacing": 2,
+           "template-curly-spacing": 2
+        }
+    };
+    console.log(eslintBundle);
+    const result = eslintBundle.runEslint(code, config);
+    console.error(result);
+}
+
 function createSaveAction() {
     return {
         // An unique identifier of the contributed action.
@@ -323,6 +409,7 @@ function createSaveAction() {
             editor.getAction('editor.action.formatDocument').run().then(() => {
                 let fileIO = new FileIO();
                 fileIO.saveText(editor.getModel().getValue());
+                eslintVerify(editor.getModel().getValue());
                 if (loadingOverview) loadingOverview.classList.add("hide");
             });
         }
