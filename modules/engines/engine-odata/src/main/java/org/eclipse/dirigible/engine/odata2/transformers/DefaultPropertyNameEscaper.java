3190dd8463a6dd484262cd0efa7df2850c34bd49
Marin Hadzhiev <57173196+MarinHadzhiev@users.noreply.github.com>
2022-03-08 18:54:43 +0200

    Introduce OData property name escaper (#1646)
    * Introduce OData property name escaper

---
+++ modules/engines/engine-odata/src/main/java/org/eclipse/dirigible/engine/odata2/transformers/DefaultPropertyNameEscaper.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.dirigible.engine.odata2.transformers;
+
+class DefaultPropertyNameEscaper implements ODataPropertyNameEscaper {
+
+    /**
+     * Replace unsupported olingo symbols with underscore symbol
+     * The olingo do not allow dot symbol to be part of the property name.
+     *
+     * @param propertyName
+     *          entity property name
+     * @return replaced string
+     * @see  org.apache.olingo.odata2.core.edm.provider.EdmNamedImplProv
+     */
+    @Override
+    public String escape(String propertyName) {
+        return propertyName.replace('.', '_');
+    }
+}
