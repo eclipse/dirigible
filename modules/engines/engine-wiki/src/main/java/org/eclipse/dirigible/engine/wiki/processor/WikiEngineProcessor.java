ce46aa740a33ae8a191114e863c64a7303730746
delchev <delchev@localhost>
2022-05-19 13:59:46 +0300

    flexmark-all added as alternative to mylyn for markdown

--- modules/engines/engine-wiki/src/main/java/org/eclipse/dirigible/engine/wiki/processor/WikiEngineProcessor.java
+++ modules/engines/engine-wiki/src/main/java/org/eclipse/dirigible/engine/wiki/processor/WikiEngineProcessor.java
@@ -12,6 +12,7 @@
 package org.eclipse.dirigible.engine.wiki.processor;
 
 import java.io.StringWriter;
+import java.util.Arrays;
 
 import org.eclipse.dirigible.repository.api.IRepositoryStructure;
 import org.eclipse.dirigible.repository.api.IResource;
@@ -19,6 +20,13 @@ import org.eclipse.mylyn.wikitext.confluence.ConfluenceLanguage;
 import org.eclipse.mylyn.wikitext.markdown.MarkdownLanguage;
 import org.eclipse.mylyn.wikitext.parser.MarkupParser;
 import org.eclipse.mylyn.wikitext.parser.builder.HtmlDocumentBuilder;
+
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 
 /**
  * Processing the incoming requests for the wiki pages.
@@ -76,13 +84,32 @@ public class WikiEngineProcessor {
 		markupParser.setBuilder(builder);
 		
 		if (path.endsWith(".md")) {
-			markupParser.setMarkupLanguage(new MarkdownLanguage());
+//			markupParser.setMarkupLanguage(new MarkdownLanguage());
+			return renderMarkdown(content);
 		} else if (path.endsWith(".confluence")) {
 			markupParser.setMarkupLanguage(new ConfluenceLanguage());
 		}
 		markupParser.parse(content);
 		String htmlContent = writer.toString();
 		return htmlContent;
+	}
+	
+	private String renderMarkdown(String content) {
+		MutableDataSet options = new MutableDataSet();
+
+        // uncomment to set optional extensions
+        options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create()));
+
+        // uncomment to convert soft-breaks to hard breaks
+        options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
+
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        // You can re-use parser and renderer instances
+        Node document = parser.parse(content);
+        String html = renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+        return html;
 	}
 
 }
