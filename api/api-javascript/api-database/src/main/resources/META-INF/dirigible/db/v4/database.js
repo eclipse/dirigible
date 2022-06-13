e22484a19034190b1a36056288bc5efc190f5742
Teodor <tdermendjievft@gmail.com>
2022-06-13 11:00:37 +0300

    Update db api

--- api/api-javascript/api-database/src/main/resources/META-INF/dirigible/db/v4/database.js
+++ api/api-javascript/api-database/src/main/resources/META-INF/dirigible/db/v4/database.js
@@ -770,7 +770,7 @@ function ResultSet(internalResultset) {
 		if (limited === undefined || limited === false) {
 			limited = false;
 		}
-		return org.eclipse.dirigible.databases.helpers.DatabaseResultSetHelper.toJson(this.native, limited);
+		return org.eclipse.dirigible.databases.helpers.DatabaseResultSetHelper.toJson(this.native, limited, false);
 	};
 
 	this.close = function () {
