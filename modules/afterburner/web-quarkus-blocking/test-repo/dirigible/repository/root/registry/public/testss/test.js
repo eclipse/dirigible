bff4bf52edf5722bf5a9e82f06c81957646bfeed
Vladimir Mutafov <vlado.mutafov@gmail.com>
2022-06-24 17:51:39 +0300

    feat: prepare quarkus blocking

---
+++ modules/afterburner/web-quarkus-blocking/test-repo/dirigible/repository/root/registry/public/testss/test.js
@@ -0,0 +1,7 @@
+//var response = require("http/v4/response");
+
+//response.println("Hello World1234444!");
+//response.flush();
+//response.close();
+//console.log("asdasdasd");
+globalThis.a = 5 + 5;
