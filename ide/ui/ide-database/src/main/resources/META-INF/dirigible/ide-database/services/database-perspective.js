f1a4a2ff8edacf516224e1762f9899b0d457c4f7
delchev <delchevn@gmail.com>
2022-06-27 12:06:31 +0300

    l&f adaptations

--- ide/ui/ide-database/src/main/resources/META-INF/dirigible/ide-database/services/database-perspective.js
+++ ide/ui/ide-database/src/main/resources/META-INF/dirigible/ide-database/services/database-perspective.js
@@ -9,12 +9,16 @@
  * SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
-exports.getPerspective = function () {
-	return {
-		name: "Database",
-		link: "../ide-database/index.html",
-		order: "110",
-		image: "database", // deprecated
-		icon: "../ide-database/images/database.svg",
-	};
+const perspectiveData = {
+	id: "database",
+	name: "Database",
+	link: "../ide-database/index.html",
+	order: "110",
+	image: "database", // deprecated
+	icon: "../ide-database/images/database.svg",
+};
+if (typeof exports !== 'undefined') {
+	exports.getPerspective = function () {
+		return perspectiveData;
+	}
 }
