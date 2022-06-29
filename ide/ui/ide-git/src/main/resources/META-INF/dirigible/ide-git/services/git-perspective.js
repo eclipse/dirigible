4ebc91f288925e81c9f718b3a2d49ecf89fbe7cc
ThuF <jordan.p.pavlov@gmail.com>
2022-06-29 21:07:08 +0300

    [IDE] Git - Adoption of Fundamentals Look & Feel

--- ide/ui/ide-git/src/main/resources/META-INF/dirigible/ide-git/services/git-perspective.js
+++ ide/ui/ide-git/src/main/resources/META-INF/dirigible/ide-git/services/git-perspective.js
@@ -9,12 +9,17 @@
  * SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
-exports.getPerspective = function () {
-	return {
-		name: "Git",
-		link: "../ide-git/index.html",
-		order: "105",
-		image: "code-fork", // deprecated
-		icon: "../ide-git/images/git.svg",
-	};
+const perspectiveData = {
+	id: "git",
+	name: "Git",
+	link: "../ide-git/index.html",
+	order: "105",
+	image: "code-fork", // deprecated
+	icon: "../ide-git/images/git.svg",
+};
+
+if (typeof exports !== 'undefined') {
+	exports.getPerspective = function () {
+		return perspectiveData;
+	}
 }
