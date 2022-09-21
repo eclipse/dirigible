62770497b78f08ecb8a53ce768079425b5af182a
Ivo Yakov <ivo.m.yakov@gmail.com>
2022-09-21 13:23:50 +0300

    fix dts file object type

--- ext/ext-api/api-qldb/src/main/resources/META-INF/dirigible/qldb/extensions/qldb.d.ts
+++ ext/ext-api/api-qldb/src/main/resources/META-INF/dirigible/qldb/extensions/qldb.d.ts
@@ -22,24 +22,24 @@ declare module "@dirigible/qldb" {
          *  Inserts the entry in the repository.
          *  On success returns the inserted entry.
          */
-        function insert(entry: Object): Object;
+        function insert(entry: object): object;
 
         /**
          *  Updates the entry in the repository.
          *  On success returns the updated entry.
          *  Notice: The entry must contain a documentId property.
          */
-        function update(entry: Object): Object;
+        function update(entry: object): object;
 
         /**
          *  Returns the repository entry with given documentId.
          */
-        function getById(documentId: string): Object;
+        function getById(documentId: string): object;
 
         /**
          *  Returns all the repository entries.
          */
-        function getAll(): Object[];
+        function getAll(): object[];
 
         /**
          *  Deletes the entry by given documentId.documentId.
@@ -52,12 +52,12 @@ declare module "@dirigible/qldb" {
          *  On success returns the documentId of the deleted object.
          *  Notice: The entry must contain a documentId property.
          */
-        function delete(entry: Object): string;
+        function delete(entry: object): string;
 
         /**
          *  Returns the entire history of transactions of the repository.
          */
-        function getHistory(): Object[];
+        function getHistory(): object[];
 
         /**
          *  Returns the ledger name the repository is working against.
