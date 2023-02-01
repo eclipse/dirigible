875710d4abe904817a9922f0ca236dc38f90b2c4
Vladimir Mutafov <vlado.mutafov@gmail.com>
2023-02-01 23:34:01 +0200

    feat: add d.ts files for java and enable java imports

--- components/ide-ui-monaco-extensions/src/main/resources/META-INF/dirigible/ide-monaco-extensions/api/dts.js
+++ components/ide-ui-monaco-extensions/src/main/resources/META-INF/dirigible/ide-monaco-extensions/api/dts.js
@@ -1,18 +1,20 @@
 /*
- * Copyright (c) 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * Copyright (c) 2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-FileCopyrightText: 2022 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
-let response = require("http/response");
-let dtsParser = require("ide-monaco-extensions/api/utils/dtsParser");
+const response = require("http/v4/response");
+const dtsParser = require("ide-monaco-extensions/api/utils/dtsParser");
+const javaDtsData = require("ide-monaco-extensions/api/java-dts-data");
 
-let dtsPaths = dtsParser.getDtsPaths();
-let dtsContent = dtsParser.getDtsFileContents(dtsPaths);
+const dtsPaths = dtsParser.getDtsPaths();
+const coreModulesDtsContent = dtsParser.getDtsFileContents(dtsPaths);
+const dtsContent = javaDtsData.dts + coreModulesDtsContent;
 response.setContentType('text/javascript');
 response.println(dtsContent);
