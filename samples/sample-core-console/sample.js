05f15f4c6c3f6c8813b9d75074aa55001ec81292
delchev <delchevn@gmail.com>
2023-03-18 20:38:07 +0200

    sample-core-console

---
+++ samples/sample-core-console/sample.js
@@ -0,0 +1,11 @@
+// Info
+console.info("Info message.");
+// Error
+console.error("Error message.");
+// Warning
+console.warn("Warning message.");
+
+// Arguments
+var firstName = "John";
+var lastName = "Smith";
+console.log(`First name: ${firstName}, last name: ${lastName}`);
