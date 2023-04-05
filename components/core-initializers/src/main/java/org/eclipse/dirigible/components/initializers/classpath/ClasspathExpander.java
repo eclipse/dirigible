111045ad0cc88b7518077390f30cd8b29eda225d
Vladimir Mutafov <vlado.mutafov@gmail.com>
2023-04-05 19:22:36 +0300

    fix: enable running DirigibleApplication directly from the IDE

--- components/core-initializers/src/main/java/org/eclipse/dirigible/components/initializers/classpath/ClasspathExpander.java
+++ components/core-initializers/src/main/java/org/eclipse/dirigible/components/initializers/classpath/ClasspathExpander.java
@@ -11,14 +11,15 @@
  */
 package org.eclipse.dirigible.components.initializers.classpath;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.JarURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
+import java.nio.file.Path;
 import java.util.Enumeration;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.eclipse.dirigible.repository.api.IRepository;
 import org.eclipse.dirigible.repository.api.IRepositoryStructure;
@@ -29,49 +30,80 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ClasspathExpander {
-	
-	/** The Constant logger. */
-	private static final Logger logger = LoggerFactory.getLogger(ClasspathExpander.class);
-	
-	private IRepository repository;
-	
-	@Autowired
-	public ClasspathExpander(IRepository repository) {
-		this.repository = repository;
-	}
-	
-	public void expandContent() {
-		expandContent("META-INF/dirigible");
-		expandContent("META-INF/resources/webjars");
-	}
-	
-	public void expandContent(String ROOT) {
-		try {
-			Enumeration<URL> urls = ClasspathContentInitializer.class.getClassLoader().getResources("META-INF");
-			while (urls.hasMoreElements()) {
-				URL url = urls.nextElement();
-				URLConnection urlConnection = url.openConnection();
-				if (urlConnection instanceof JarURLConnection) {
-					JarURLConnection jarUrlConnection = (JarURLConnection) (url.openConnection());
-					try (JarFile jar = jarUrlConnection.getJarFile();) {
-						Enumeration<JarEntry> entries = jar.entries();
-						while (entries.hasMoreElements()) {
-							JarEntry entry = entries.nextElement();
-							if (entry.getName().startsWith(ROOT)) {
-								if (!entry.isDirectory()) {
-									//if (logger.isDebugEnabled()) {logger.debug("resource found: " + entry);}
-									byte[] content = IOUtils.toByteArray(jar.getInputStream(entry));
-									String registryPath = entry.getName().substring(ROOT.length());
-									repository.createResource(IRepositoryStructure.PATH_REGISTRY_PUBLIC + IRepository.SEPARATOR + registryPath, content);
-								}
-							}
-						}
-					}
-				}
-			}
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
 
+    /**
+     * The Constant logger.
+     */
+    private static final Logger logger = LoggerFactory.getLogger(ClasspathExpander.class);
+
+    private final IRepository repository;
+
+    @Autowired
+    public ClasspathExpander(IRepository repository) {
+        this.repository = repository;
+    }
+
+    public void expandContent() {
+        expandContent("dirigible");
+        expandContent("resources" + File.separator + "webjars");
+    }
+
+    private void expandContent(String root) {
+        try {
+            Enumeration<URL> urls = ClasspathContentInitializer.class.getClassLoader().getResources("META-INF");
+
+            while (urls.hasMoreElements()) {
+                URL url = urls.nextElement();
+                try {
+                    URLConnection urlConnection = url.openConnection();
+                    if (urlConnection instanceof JarURLConnection) {
+                        handleJarURLConnection(root, urlConnection);
+                    } else {
+                        Path dirPath = Path.of(url.toURI()).resolve(root);
+                        handleLocalDirectory(dirPath);
+                    }
+                } catch (URISyntaxException | IOException e) {
+                    logDirectoryExpandingError(url.toString(), e);
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void handleJarURLConnection(String root, URLConnection urlConnection) throws IOException {
+        String jarRoot = "META-INF/" + root;
+        JarURLConnection jarUrlConnection = (JarURLConnection) urlConnection;
+        try (JarFile jar = jarUrlConnection.getJarFile()) {
+            Enumeration<JarEntry> entries = jar.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                if (entry.getName().startsWith(jarRoot)) {
+                    if (!entry.isDirectory()) {
+                        byte[] content = IOUtils.toByteArray(jar.getInputStream(entry));
+                        String registryPath = entry.getName().substring(jarRoot.length());
+                        repository.createResource(IRepositoryStructure.PATH_REGISTRY_PUBLIC + IRepository.SEPARATOR + registryPath, content);
+                    }
+                }
+            }
+        }
+    }
+
+    private void handleLocalDirectory(Path dirPath) {
+        try {
+            File maybeDir = dirPath.toFile();
+            if (!maybeDir.exists() || maybeDir.isFile()) {
+                return;
+            }
+            String registryPath = repository.getInternalResourcePath(IRepositoryStructure.PATH_REGISTRY_PUBLIC);
+            FileUtils.copyDirectory(maybeDir, new File(registryPath));
+        } catch (IOException e) {
+            logDirectoryExpandingError(dirPath.toString(), e);
+        }
+    }
+
+    private void logDirectoryExpandingError(String dirPath, Exception e) {
+        logger.error("Could not collect dir '" + dirPath + "'", e);
+    }
 }
