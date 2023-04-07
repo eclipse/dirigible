9c345fa236eeb56d8f61452aa0998e85c8d264af
Nedelcho Delchev <nedelcho.delchev@gmail.com>
2023-04-07 22:09:35 +0300

    Redis API Component

---
+++ components/api-redis/src/test/java/org/eclipse/dirigible/components/api/redis/RedisFacadeTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.eclipse.dirigible.components.api.redis;
+
+import org.eclipse.dirigible.components.api.redis.RedisFacade;
+import org.eclipse.dirigible.commons.config.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+import redis.clients.jedis.Jedis;
+
+import static org.junit.Assert.assertEquals;
+
+@WithMockUser
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ComponentScan(basePackages = { "org.eclipse.dirigible.components.*" })
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class RedisFacadeTest {
+
+    @Before
+    public void setUp(){
+        GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine"))
+                .withExposedPorts(6379);
+        redis.start();
+
+        String host = redis.getHost();
+        Integer port = redis.getFirstMappedPort();
+
+        Configuration.set("DIRIGIBLE_REDIS_CLIENT_URI", host + ":" + port);
+    }
+
+    @Test
+    public void getClient(){
+        Jedis client = RedisFacade.getClient();
+        client.set("key", "value");
+        assertEquals("value", client.get("key"));
+    }
+}
