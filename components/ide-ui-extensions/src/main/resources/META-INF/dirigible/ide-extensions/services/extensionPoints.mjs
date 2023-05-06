3778d8e46d7d29aea80c27b7486be65a47b1ca8a
delchev <delchevn@gmail.com>
2023-05-06 17:21:11 +0300

    extension_point name column updated

--- components/ide-ui-extensions/src/main/resources/META-INF/dirigible/ide-extensions/services/extensionPoints.mjs
+++ components/ide-ui-extensions/src/main/resources/META-INF/dirigible/ide-extensions/services/extensionPoints.mjs
@@ -6,10 +6,10 @@ let extensionPoints = [];
 let connection = null;
 try {
     connection = database.getConnection("SystemDB");
-    let statement = connection.prepareStatement("SELECT EXTENSIONPOINT_NAME FROM DIRIGIBLE_EXTENSION_POINTS");
+    let statement = connection.prepareStatement("SELECT ARTEFACT_NAME FROM DIRIGIBLE_EXTENSION_POINTS");
     let resultSet = statement.executeQuery();
     while (resultSet.next()) {
-        extensionPoints.push(resultSet.getString("EXTENSIONPOINT_NAME"));
+        extensionPoints.push(resultSet.getString("ARTEFACT_NAME"));
     }
 } finally {
     if (connection != null) {
