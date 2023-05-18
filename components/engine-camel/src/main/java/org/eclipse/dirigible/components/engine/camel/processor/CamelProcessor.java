164896a723ebba7b3e897813c058496c6e88b1ab
imyakov <ivo.yakov@ingka.com>
2023-05-18 20:35:29 +0300

    feat: reset context every time

--- components/engine-camel/src/main/java/org/eclipse/dirigible/components/engine/camel/processor/CamelProcessor.java
+++ components/engine-camel/src/main/java/org/eclipse/dirigible/components/engine/camel/processor/CamelProcessor.java
@@ -1,44 +1,57 @@
 package org.eclipse.dirigible.components.engine.camel.processor;
 
 import org.apache.camel.CamelContext;
-import org.apache.camel.ExtendedCamelContext;
-import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.spi.Resource;
 import org.apache.camel.spi.RoutesLoader;
+
+import org.apache.camel.spring.boot.SpringBootCamelContext;
 import org.apache.camel.support.ResourceHelper;
 import org.eclipse.dirigible.components.engine.camel.domain.Camel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @Component
-public class CamelProcessor extends RouteBuilder {
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+public class CamelProcessor {
+
+    private final SpringBootCamelContext context;
+
+    private final RoutesLoader loader;
+
+    private final Map<Long, Resource> camels = new HashMap<>();
+
+    @Autowired
+    public CamelProcessor(CamelContext context) {
+        this.context = context.adapt(SpringBootCamelContext.class);
+        loader = this.context.getRoutesLoader();
+    }
 
     public void process(Camel camel) {
-        CamelContext context = getContext();
-        String fileContent = new String(camel.getContent(), StandardCharsets.UTF_8);
-
-        ExtendedCamelContext extendedCamelContext = context.adapt(ExtendedCamelContext.class);
-        RoutesLoader loader = extendedCamelContext.getRoutesLoader();
-        Resource resource = ResourceHelper.fromString("any.yaml", fileContent);
-        try {
-            loader.loadRoutes(resource);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        Resource resource = ResourceHelper.fromBytes("any.yaml", camel.getContent());
+        camels.put(camel.getId(), resource);
+        camels.values().forEach(routesResource -> {
+            try {
+                loader.loadRoutes(routesResource);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public void clear() {
-        CamelContext context = getContext();
         try {
+            context.getRouteController().stopAllRoutes();
             context.getRouteController().removeAllRoutes();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @Override
-    public void configure() throws Exception {
-        // No-op
     }
 }
