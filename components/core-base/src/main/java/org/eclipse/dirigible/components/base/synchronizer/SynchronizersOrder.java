cb4c6dc03cca4dc7ea1463b5341f33eb3716d589
delchev <delchevn@gmail.com>
2023-06-05 19:30:27 +0300

    javadoc for synchronizers index

--- components/core-base/src/main/java/org/eclipse/dirigible/components/base/synchronizer/SynchronizersOrder.java
+++ components/core-base/src/main/java/org/eclipse/dirigible/components/base/synchronizer/SynchronizersOrder.java
@@ -1,43 +1,65 @@
 package org.eclipse.dirigible.components.base.synchronizer;
 
+/**
+ * The Interface SynchronizersOrder.
+ */
 public interface SynchronizersOrder {
 	
+	/** The extensionpoint. */
 	int EXTENSIONPOINT = 10;
 	
+	/** The extension. */
 	int EXTENSION = 20;
 	
+	/** The role. */
 	int ROLE = 30;
 	
+	/** The access. */
 	int ACCESS = 40;
 	
+	/** The job. */
 	int JOB = 50;
 	
+	/** The listener. */
 	int LISTENER = 60;
 	
+	/** The expose. */
 	int EXPOSE = 70;
 	
+	/** The openapi. */
 	int OPENAPI = 110;
 	
+	/** The websocket. */
 	int WEBSOCKET = 120;
 	
+	/** The datasource. */
 	int DATASOURCE = 200;
 	
+	/** The schema. */
 	int SCHEMA = 210;
 	
+	/** The table. */
 	int TABLE = 220;
 	
+	/** The view. */
 	int VIEW = 230;
 	
+	/** The entity. */
 	int ENTITY = 240;
 	
+	/** The bpmn. */
 	int BPMN = 300;
 	
+	/** The odata. */
 	int ODATA = 310;
 	
+	/** The csvim. */
 	int CSVIM = 400;
 	
+	/** The confluence. */
 	int CONFLUENCE = 410;
 	
+	/** The markdown. */
 	int MARKDOWN = 420;
 
 }
