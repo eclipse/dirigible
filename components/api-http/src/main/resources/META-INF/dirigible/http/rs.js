a0740151a8c24d7019a7998b30f7ea35f423f229
delchev <delchevn@gmail.com>
2023-06-13 15:47:21 +0300

    explicit string

--- components/api-http/src/main/resources/META-INF/dirigible/http/rs.js
+++ components/api-http/src/main/resources/META-INF/dirigible/http/rs.js
@@ -689,7 +689,7 @@ function calculateMatchedRouteWeight(matchedRoute) {
 
 function transformPathParamsDeclaredInBraces(pathDefinition) {
 	const pathParamsInBracesMatcher = /({(\w*\*?)})/g; // matches cases like '/api/{pathParam}' or '/api/{pathParam*}'
-	return pathDefinition.replace(pathParamsInBracesMatcher, ":$2"); // transforms matched cases to '/api/:pathParam' or '/api/:pathParam*'
+	return String(pathDefinition.replace(pathParamsInBracesMatcher, ":$2")); // transforms matched cases to '/api/:pathParam' or '/api/:pathParam*'
 }
 
 	//  content-type, consumes
