63ccf41c37413c4b2df5bd8e5f2ff0325d76f3c0
Yordan Pavlov <ThuF@users.noreply.github.com>
2023-06-23 19:31:04 +0300

    [IDE] Artefacts View (#2597)

---
+++ components/ide-ui-operations/src/main/resources/META-INF/dirigible/ide-operations/services/artefacts/artefacts.mjs
@@ -0,0 +1,11 @@
+import { extensions } from "@dirigible/extensions";
+import { response } from "@dirigible/http";
+
+const artefacts = [];
+const artefactsExtensions = extensions.getExtensions("ide-operations-artefacts");
+for (let i = 0; i < artefactsExtensions.length; i++) {
+    const { getArtefacts } = await import(`../../../${artefactsExtensions[i]}`);
+    artefacts.push(...getArtefacts());
+}
+
+response.println(JSON.stringify(artefacts));
---
+++ components/ide-ui-operations/src/main/resources/META-INF/dirigible/ide-operations/services/artefacts/security-access/artefacts-security-access.mjs
@@ -0,0 +1,12 @@
+import { query } from "@dirigible/db";
+import { Utils } from "../Utils.mjs";
+
+export const getArtefacts = () => {
+    const sql = `
+        SELECT 
+        ARTEFACT_TYPE, ARTEFACT_LOCATION, ARTEFACT_NAME, ARTEFACT_PHASE, ARTEFACT_RUNNING, ARTEFACT_STATUS
+        FROM DIRIGIBLE_SECURITY_ACCESS
+    `;
+    const resultset = query.execute(sql, [], "SystemDB");
+    return resultset.map(e => Utils.getArtefactStatus(e));
+};
