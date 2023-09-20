1710d997360d0f5a1e8141abec7311148d609330
StanZGenchev <stan.z.genchev@gmail.com>
2023-09-20 22:23:05 +0300

    Fixed #2769

--- components/ide-ui-databases/src/main/resources/META-INF/dirigible/ide-databases/databases.js
+++ components/ide-ui-databases/src/main/resources/META-INF/dirigible/ide-databases/databases.js
@@ -24,120 +24,42 @@ databasesView.controller('DatabaseController', ['$scope', '$http', 'messageHub',
     }
     $scope.listDatabases();
 
-    $scope.getDatabaseDetailsFormItems = (values = {}, isNew = true) => {
-        let ret = [];
-
-        if (isNew) ret.push({
-            id: 'name',
-            type: 'input',
-            label: 'Name',
-            required: true,
-            placeholder: 'Enter Name...',
-            value: values['name'] || ''
-        });
-
-        ret.push(
-            {
-                id: 'driver',
-                type: 'dropdown',
-                label: 'Driver',
-                required: true,
-                placeholder: 'Select Driver...',
-                value: values['driver'],
-                items: $scope.drivers.map(x => ({ label: x.text, value: x.value }))
-            },
-            {
-                id: 'url',
-                type: 'input',
-                label: 'URL',
-                required: true,
-                placeholder: 'Enter URL...',
-                value: values['url'] || ''
-            },
-            {
-                id: 'username',
-                type: 'input',
-                label: 'Username',
-                required: false,
-                placeholder: 'Enter Username...',
-                value: values['username'] || ''
-            },
-            {
-                id: 'password',
-                type: 'input',
-                inputType: 'password',
-                label: 'Password',
-                required: false,
-                placeholder: 'Enter Password...',
-                value: values['password'] || ''
-            },
-            {
-                id: 'parameters',
-                type: 'input',
-                label: 'Parameters',
-                required: false,
-                placeholder: 'Enter Parameters, e.g. name1=value1,name2=value2 ...',
-                value: $scope.joinParameters(values['properties']) || ''
-            }
-        );
-        return ret;
-    }
-    
-    $scope.joinParameters = function(parameters) {
-		let result = '';
-		if (parameters) {
-			parameters.forEach(function(current){
-				result += current.name + '=' + current.value + ',';
-			});
-			return result.slice(0, -1);
-		}
-		return result;
-	}
+    $scope.joinParameters = function (parameters) {
+        let result = '';
+        if (parameters) {
+            parameters.forEach(function (current) {
+                result += current.name + '=' + current.value + ',';
+            });
+            return result.slice(0, -1);
+        }
+        return result;
+    };
 
     $scope.newDatabase = function () {
-        messageHub.showFormDialog(
-            'createDatabaseDialog',
-            'New Database',
-            $scope.getDatabaseDetailsFormItems(),
-            [{
-                id: 'btnOK',
-                type: 'emphasized',
-                label: 'Create',
-                whenValid: true
-            },
-            {
-                id: 'btnCancel',
-                type: 'transparent',
-                label: 'Close',
-            }],
-            'ide-databases.database.create',
-            'Please, wait...'
+        messageHub.showDialogWindow(
+            "database-create-edit",
+            { editMode: false },
+            null,
+            false
         );
-    }
+    };
 
     messageHub.onDidReceiveMessage(
         'ide-databases.database.create',
         function (msg) {
-            if (msg.data.buttonId === 'btnOK') {
-                let database = msg.data.formData.reduce((ret, item) => {
-                    ret[item.id] = item.value;
-                    return ret;
-                }, {});
-                $http.post('/services/data/sources', JSON.stringify(database))
-                    .then(function (response) {
-                        messageHub.hideFormDialog('createDatabaseDialog');
-                        $scope.listDatabases();
-                    }, function (response) {
-                        messageHub.updateFormDialog(
-                            'createDatabaseDialog',
-                            msg.data.formData,
-                            'Please, wait...',
-                            response.data
-                        );
-                    });
-            } else {
-                messageHub.hideFormDialog('createDatabaseDialog');
+            if (msg.data) {
+                $http.post(
+                    '/services/data/sources',
+                    JSON.stringify(msg.data)
+                ).then(function () {
+                    $scope.listDatabases();
+                    messageHub.triggerEvent('ide-databases.explorer.refresh', true);
+                }, function (response) {
+                    console.error(response);
+                    messageHub.showAlertError('Error while creating database', 'Please look at the console for more information');
+                });
             }
+            messageHub.closeDialogWindow('database-create-edit');
         },
         true
     );
@@ -147,50 +69,40 @@ databasesView.controller('DatabaseController', ['$scope', '$http', 'messageHub',
             id: database.id,
             name: database.name
         };
-
-        messageHub.showFormDialog(
-            'editDatabaseDialog',
-            `Edit Database ${database.name}`,
-            $scope.getDatabaseDetailsFormItems(database, false),
-            [{
-                id: 'btnOK',
-                type: 'emphasized',
-                label: 'Update',
-                whenValid: true
-            },
+        messageHub.showDialogWindow(
+            "database-create-edit",
             {
-                id: 'btnCancel',
-                type: 'transparent',
-                label: 'Close',
-            }],
-            'ide-databases.database.edit',
-            'Please, wait...'
+                editMode: true,
+                database: {
+                    name: '',
+                    driver: database.driver,
+                    url: database.url,
+                    username: database.username,
+                    password: database.password,
+                    parameters: $scope.joinParameters(database.properties),
+                }
+            },
+            null,
+            false
         );
-    }
+    };
 
     messageHub.onDidReceiveMessage(
         'ide-databases.database.edit',
         function (msg) {
-            if (msg.data.buttonId === 'btnOK') {
-                let database = msg.data.formData.reduce((ret, item) => {
-                    ret[item.id] = item.value;
-                    return ret;
-                }, {});
-                $http.put('/services/data/sources/' + $scope.database.id, JSON.stringify({ name: $scope.database.name, ...database }))
-                    .then(function (response) {
-                        messageHub.hideFormDialog('editDatabaseDialog');
+            if (msg.data) {
+                let database = msg.data;
+                database.name = $scope.database.name;
+                $http.put('/services/data/sources/' + $scope.database.id, JSON.stringify(database))
+                    .then(function () {
                         $scope.listDatabases();
+                        messageHub.triggerEvent('ide-databases.explorer.refresh', true);
                     }, function (response) {
-                        messageHub.updateFormDialog(
-                            'editDatabaseDialog',
-                            msg.data.formData,
-                            'Please, wait...',
-                            response.data
-                        );
+                        console.error(response);
+                        messageHub.showAlertError('Error while updating database', 'Please look at the console for more information');
                     });
-            } else {
-                messageHub.hideFormDialog('editDatabaseDialog');
             }
+            messageHub.closeDialogWindow('database-create-edit');
         },
         true
     );
@@ -222,34 +134,16 @@ databasesView.controller('DatabaseController', ['$scope', '$http', 'messageHub',
         function (msg) {
             if (msg.data === 'btnOK' && $scope.database.id) {
                 $http.delete('/services/data/sources/' + $scope.database.id)
-                    .then(function (response) {
+                    .then(function () {
                         $scope.listDatabases();
+                        messageHub.triggerEvent('ide-databases.explorer.refresh', true);
                     }, function (response) {
                         console.error(response.data);
+                        messageHub.showAlertError('Error while deleting database', 'Please look at the console for more information');
                     });
             }
         },
         true
     );
-
-    $scope.drivers = [];
-    $scope.drivers.push({ "text": "H2 - org.h2.Driver", "value": "org.h2.Driver" });
-    $scope.drivers.push({ "text": "PostgreSQL - org.postgresql.Driver", "value": "org.postgresql.Driver" });
-    $scope.drivers.push({ "text": "MySQL - com.mysql.jdbc.Driver", "value": "com.mysql.jdbc.Driver" });
-    $scope.drivers.push({ "text": "SAP HANA - com.sap.db.jdbc.Driver", "value": "com.sap.db.jdbc.Driver" });
-    $scope.drivers.push({ "text": "MongoDB - org.eclipse.dirigible.mongodb.jdbc.Driver", "value": "org.eclipse.dirigible.mongodb.jdbc.Driver" });
-
-    $scope.urls = {};
-    $scope.urls["org.h2.Driver"] = "jdbc:h2:path/name";
-    $scope.urls["org.postgresql.Driver"] = "jdbc:postgresql://host:port/database";
-    $scope.urls["com.mysql.jdbc.Driver"] = "jdbc:mysql://host:port/database";
-    $scope.urls["com.sap.db.jdbc.Driver"] = "jdbc:sap://host:port/?encrypt=true&validateCertificate=false";
-    $scope.urls["org.eclipse.dirigible.mongodb.jdbc.Driver"] = "jdbc:mongodb://host:port/database";
-
-    $scope.driverChanged = function () {
-        $scope.database.url = $scope.urls[$scope.database.driver];
-        $scope.database.username = "";
-        $scope.database.password = "";
-    }
 
 }]);
---
+++ components/ide-ui-databases/src/main/resources/META-INF/dirigible/ide-databases/dialogs/database-dialog.js
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+const dbdialog = angular.module('dbdialog', ['ideUI', 'ideView']);
+
+dbdialog.controller('DBDialogController', ['$scope', 'messageHub', 'ViewParameters', function ($scope, messageHub, ViewParameters) {
+    $scope.state = {
+        isBusy: true,
+        error: false,
+        busyText: "Loading...",
+    };
+
+    $scope.editMode = false;
+
+    $scope.urls = {
+        "org.h2.Driver": "jdbc:h2:path/name",
+        "org.postgresql.Driver": "jdbc:postgresql://host:port/database",
+        "com.mysql.jdbc.Driver": "jdbc:mysql://host:port/database",
+        "com.sap.db.jdbc.Driver": "jdbc:sap://host:port/?encrypt=true&validateCertificate=false",
+        "org.eclipse.dirigible.mongodb.jdbc.Driver": "jdbc:mongodb://host:port/database",
+    };
+
+    $scope.drivers = [
+        { text: "H2 - org.h2.Driver", value: "org.h2.Driver" },
+        { text: "PostgreSQL - org.postgresql.Driver", value: "org.postgresql.Driver" },
+        { text: "MySQL - com.mysql.jdbc.Driver", value: "com.mysql.jdbc.Driver" },
+        { text: "SAP HANA - com.sap.db.jdbc.Driver", value: "com.sap.db.jdbc.Driver" },
+        { text: "MongoDB - org.eclipse.dirigible.mongodb.jdbc.Driver", value: "org.eclipse.dirigible.mongodb.jdbc.Driver" }
+    ];
+
+    $scope.database = {
+        name: '',
+        driver: '',
+        url: '',
+        username: '',
+        password: '',
+        parameters: '',
+    };
+
+    $scope.driverChanged = function () {
+        $scope.database.url = $scope.urls[$scope.database.driver];
+        $scope.database.username = "";
+        $scope.database.password = "";
+    };
+
+    function getTopic() {
+        if ($scope.editMode) return 'ide-databases.database.edit';
+        return 'ide-databases.database.create';
+    }
+
+    $scope.save = function () {
+        $scope.state.busyText = "Sending data..."
+        $scope.state.isBusy = true;
+        messageHub.postMessage(getTopic(), $scope.database, true);
+    };
+
+    $scope.cancel = function () {
+        messageHub.closeDialogWindow('database-create-edit');
+    };
+
+    $scope.dataParameters = ViewParameters.get();
+    if (!$scope.dataParameters.hasOwnProperty('editMode')) {
+        $scope.state.error = true;
+        $scope.errorMessage = "The 'editMode' parameter is missing.";
+    } else {
+        $scope.editMode = $scope.dataParameters.editMode;
+        if ($scope.editMode) {
+            if (!$scope.dataParameters.hasOwnProperty('database')) {
+                $scope.state.error = true;
+                $scope.errorMessage = "The 'database' parameter is missing.";
+            } else {
+                $scope.database = $scope.dataParameters.database;
+            }
+        }
+        $scope.state.isBusy = false;
+    }
+}]);
