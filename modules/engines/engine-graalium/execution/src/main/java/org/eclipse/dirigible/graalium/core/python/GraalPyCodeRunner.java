b681c2dbd010b37320b01cb4043c011d1ef779f1
delchev <delchevn@gmail.com>
2023-10-06 23:19:25 +0300

    merge fixed

--- modules/engines/engine-graalium/execution/src/main/java/org/eclipse/dirigible/graalium/core/python/GraalPyCodeRunner.java
+++ modules/engines/engine-graalium/execution/src/main/java/org/eclipse/dirigible/graalium/core/python/GraalPyCodeRunner.java
@@ -1,16 +1,15 @@
 package org.eclipse.dirigible.graalium.core.python;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
 import org.eclipse.dirigible.graalium.core.CodeRunner;
 import org.eclipse.dirigible.graalium.core.graal.ContextCreator;
 import org.eclipse.dirigible.graalium.core.graal.EngineCreator;
-import org.graalvm.polyglot.*;
-
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
 
 public class GraalPyCodeRunner implements CodeRunner<Source, Value> {
     private final Context context;
@@ -18,31 +17,10 @@ public class GraalPyCodeRunner implements CodeRunner<Source, Value> {
     public GraalPyCodeRunner(
             Path workingDirectoryPath,
             Path projectDirectoryPath,
-<<<<<<< HEAD
-<<<<<<< Upstream, based on f530b92c25bd5140f1917d1397dc6fd7ea035287
-<<<<<<< HEAD
-=======
->>>>>>> 943f21dcc743d4917a07a779fbb85d19b8d6986f
             Path pythonModulesPath,
             boolean debug
     ) {
         var engine = debug ? EngineCreator.getOrCreateDebuggableEngine() : EngineCreator.getOrCreateEngine();
-<<<<<<< HEAD
-=======
-            Path pythonModulesPath
-=======
-            Path pythonModulesPath,
-            boolean debug
->>>>>>> c30d33d feat: enable python debugging
-    ) {
-<<<<<<< Upstream, based on f530b92c25bd5140f1917d1397dc6fd7ea035287
-        var engine = EngineCreator.getOrCreateEngine();
->>>>>>> 47ad2a6b2d (feat: initial python support)
-=======
-        var engine = debug ? EngineCreator.getOrCreateDebuggableEngine() : EngineCreator.getOrCreateEngine();
->>>>>>> c30d33d feat: enable python debugging
-=======
->>>>>>> 943f21dcc743d4917a07a779fbb85d19b8d6986f
         var fs = new GraalPyFileSystem(workingDirectoryPath, FileSystems.getDefault());
         context = new ContextCreator(engine, workingDirectoryPath, projectDirectoryPath, pythonModulesPath, fs).createContext();
     }
