27f4ed247d4ae27ceab0b8096bd099f6c42296ca
Vladimir Mutafov <vlado.mutafov@gmail.com>
2023-11-01 23:43:13 +0200

    chore: remove unnecessary whitespace

--- components/api-http/src/test/resources/META-INF/dirigible/http-tests/request-get-path-translated.js
+++ components/api-http/src/test/resources/META-INF/dirigible/http-tests/request-get-path-translated.js
@@ -13,4 +13,4 @@ const request = require('http/request');
 const separator = require('io/files').separator;
 const assertTrue = require('test/assert').assertTrue;
 
-assertTrue(request.getPathTranslated().endsWith(`${separator }services${separator }js${separator}http-tests${separator}request-get-path-translated.js`));
+assertTrue(request.getPathTranslated().endsWith(`${separator}services${separator}js${separator}http-tests${separator}request-get-path-translated.js`));
