5edd1dd42e651c62ac4aa14b21ba04828ecb7227
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2023-11-07 17:12:36 +0200

    formatting
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- modules/database/database-mongodb-jdbc/src/main/java/org/eclipse/dirigible/mongodb/jdbc/Driver.java
+++ modules/database/database-mongodb-jdbc/src/main/java/org/eclipse/dirigible/mongodb/jdbc/Driver.java
@@ -117,8 +117,7 @@ public class Driver implements java.sql.Driver {
      * Gets the parent logger.
      *
      * @return the parent logger
-     * @throws SQLFeatureNotSupportedException the SQL feature not supported
-     * exception
+     * @throws SQLFeatureNotSupportedException the SQL feature not supported exception
      */
     public Logger getParentLogger() throws SQLFeatureNotSupportedException {
         throw new SQLFeatureNotSupportedException("The Driver uses slf4j for logging");
