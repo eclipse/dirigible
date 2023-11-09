38d82eaa9232ad829814014a1d8046343b9f3eaa
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2023-11-09 16:14:24 +0200

    fix doc
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/data/data-management/src/main/java/org/eclipse/dirigible/components/data/management/format/ResultSetCsvWriter.java
+++ components/data/data-management/src/main/java/org/eclipse/dirigible/components/data/management/format/ResultSetCsvWriter.java
@@ -1,5 +1,5 @@
 /*
- * \ * Copyright (c) 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
