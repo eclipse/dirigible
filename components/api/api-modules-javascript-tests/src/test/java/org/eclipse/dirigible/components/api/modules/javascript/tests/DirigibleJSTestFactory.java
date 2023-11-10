dba19f1e4e363b55266290eede15bed033c22c30
Vladimir Mutafov <vlado.mutafov@gmail.com>
2023-11-10 11:35:41 +0200

    fix: address pr comments

--- components/api/api-modules-javascript-tests/src/test/java/org/eclipse/dirigible/components/api/modules/javascript/tests/DirigibleJSTestFactory.java
+++ components/api/api-modules-javascript-tests/src/test/java/org/eclipse/dirigible/components/api/modules/javascript/tests/DirigibleJSTestFactory.java
@@ -14,6 +14,7 @@ import org.eclipse.dirigible.graalium.core.DirigibleJavascriptCodeRunner;
 import org.eclipse.dirigible.graalium.core.graal.globals.GlobalFunction;
 import org.eclipse.dirigible.graalium.core.javascript.GraalJSSourceCreator;
 import org.eclipse.dirigible.graalium.core.javascript.modules.ModuleType;
+import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 import org.junit.jupiter.api.DynamicContainer;
 import org.junit.jupiter.api.DynamicTest;
@@ -25,6 +26,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class DirigibleJSTestFactory {
 
@@ -45,11 +47,13 @@ public class DirigibleJSTestFactory {
     List<DynamicContainer> createTestContainers() {
         return findAllTestFilesInProject(testProjectName).stream()
                                                          .map(testFilePath -> {
-                                                             var testFilePathAsString = testFilePath.toString();
-                                                             var moduleType = testFilePathAsString.endsWith(".mjs") ? ModuleType.ESM
+                                                             String testFilePathAsString = testFilePath.toString();
+                                                             ModuleType moduleType = testFilePathAsString.endsWith(".mjs")
+                                                                     ? ModuleType.ESM
                                                                      : ModuleType.CJS;
-                                                             var source = new GraalJSSourceCreator(moduleType).createSource(testFilePath);
-                                                             var testFunction = new TestFunction();
+                                                             Source source =
+                                                                     new GraalJSSourceCreator(moduleType).createSource(testFilePath);
+                                                             TestFunction testFunction = new TestFunction();
                                                              codeRunner.getCodeRunner()
                                                                        .addGlobalFunctions(Collections.singletonList(testFunction));
                                                              codeRunner.run(source);
@@ -66,12 +70,12 @@ public class DirigibleJSTestFactory {
         Path projectPath = codeRunner.getSourceProvider()
                                      .getAbsoluteProjectPath(projectName);
         try {
-            try (var filesStream = Files.walk(projectPath)
-                                        .filter(path -> {
-                                            var pathAsString = path.toString();
-                                            return (pathAsString.endsWith(".mjs") || pathAsString.endsWith(".js"))
-                                                    && testProjectFileFilter.test(path);
-                                        })) {
+            try (Stream<Path> filesStream = Files.walk(projectPath)
+                                                 .filter(path -> {
+                                                     String pathAsString = path.toString();
+                                                     return (pathAsString.endsWith(".mjs") || pathAsString.endsWith(".js"))
+                                                             && testProjectFileFilter.test(path);
+                                                 })) {
                 return filesStream.toList();
             }
         } catch (IOException e) {
@@ -90,9 +94,9 @@ public class DirigibleJSTestFactory {
 
         @Override
         public Object execute(Value... arguments) {
-            var testName = arguments[0].asString();
-            var testFunc = arguments[1];
-            var dynamicTest = DynamicTest.dynamicTest(testName, testFunc::execute);
+            String testName = arguments[0].asString();
+            Value testFunc = arguments[1];
+            DynamicTest dynamicTest = DynamicTest.dynamicTest(testName, testFunc::execute);
             dynamicTests.add(dynamicTest);
             return null;
         }
