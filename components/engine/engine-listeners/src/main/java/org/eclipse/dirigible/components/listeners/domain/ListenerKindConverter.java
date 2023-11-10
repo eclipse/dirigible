99549e52a55203a19beb4b9c06a65c80b7658f11
Vladimir Mutafov <vlado.mutafov@gmail.com>
2023-11-10 11:55:30 +0200

    chore: refactor marshalling logic

--- components/engine/engine-listeners/src/main/java/org/eclipse/dirigible/components/listeners/domain/ListenerKindConverter.java
+++ components/engine/engine-listeners/src/main/java/org/eclipse/dirigible/components/listeners/domain/ListenerKindConverter.java
@@ -4,9 +4,17 @@ import javax.persistence.AttributeConverter;
 
 public class ListenerKindConverter implements AttributeConverter<ListenerKind, Character> {
 
+    private static final char QUEUE_CHAR = 'Q';
+    private static final char TOPIC_CHAR = 'T';
+
     @Override
     public Character convertToDatabaseColumn(ListenerKind from) {
-        return from.asChar();
+        if (ListenerKind.QUEUE.equals(from)) {
+            return QUEUE_CHAR;
+        } else if (ListenerKind.TOPIC.equals(from)) {
+            return TOPIC_CHAR;
+        }
+        throw new IllegalArgumentException("Unsupported listener kind: " + from);
     }
 
     @Override
@@ -14,6 +22,13 @@ public class ListenerKindConverter implements AttributeConverter<ListenerKind, C
         if (to == null) {
             throw new IllegalArgumentException("Listener kind char cannot be null");
         }
-        return ListenerKind.fromChar(to);
+
+        char uppercased = Character.toUpperCase(to);
+        if (uppercased == QUEUE_CHAR) {
+            return ListenerKind.QUEUE;
+        } else if (uppercased == TOPIC_CHAR) {
+            return ListenerKind.TOPIC;
+        }
+        throw new IllegalArgumentException("Unsupported listener kind: " + to);
     }
 }
