c357ea1a5a7a6696b8bfc62c1fc67270b95c7b5f
Vladimir Mutafov <vlado.mutafov@gmail.com>
2023-11-13 16:58:43 +0200

    fix: move functions in static class members and remove destinations api

--- components/api/api-modules-python/src/main/resources/META-INF/dirigible/python-modules/dirigible/core/configurations.py
+++ components/api/api-modules-python/src/main/resources/META-INF/dirigible/python-modules/dirigible/core/configurations.py
@@ -6,43 +6,58 @@
 # SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
 # SPDX-License-Identifier: EPL-2.0
 
-from org.eclipse.dirigible.commons.config import Configuration
+import java
 
-def get(key, defaultValue=None):
-    if defaultValue is not None:
-        return Configuration.get(key, defaultValue)
-    return Configuration.get(key)
+Configuration = java.type('org.eclipse.dirigible.commons.config')
 
-def set(key, value):
-    Configuration.set(key, value)
+class Configurations:
 
-def remove(key):
-    Configuration.remove(key)
+    @staticmethod
+    def get(key, defaultValue=None):
+        if defaultValue is not None:
+            return Configuration.get(key, defaultValue)
+        return Configuration.get(key)
 
-def getKeys():
-    keys = []
-    keysAsArray = Configuration.getKeys()
-    for i in range(len(keysAsArray)):
-        keys.append(keysAsArray[i])
-    return keys
+    @staticmethod
+    def set(key, value):
+        Configuration.set(key, value)
 
-def load(path):
-    Configuration.load(path)
+    @staticmethod
+    def remove(key):
+        Configuration.remove(key)
 
-def update():
-    Configuration.update()
+    @staticmethod
+    def getKeys():
+        keys = []
+        keysAsArray = Configuration.getKeys()
+        for i in range(len(keysAsArray)):
+            keys.append(keysAsArray[i])
+        return keys
 
-def getOS():
-    return Configuration.getOS()
+    @staticmethod
+    def load(path):
+        Configuration.load(path)
 
-def isOSWindows():
-    return Configuration.isOSWindows()
+    @staticmethod
+    def update():
+        Configuration.update()
 
-def isOSMac():
-    return Configuration.isOSMac()
+    @staticmethod
+    def getOS():
+        return Configuration.getOS()
 
-def isOSUNIX():
-    return Configuration.isOSUNIX()
+    @staticmethod
+    def isOSWindows():
+        return Configuration.isOSWindows()
 
-def isOSSolaris():
-    return Configuration.isOSSolaris()
+    @staticmethod
+    def isOSMac():
+        return Configuration.isOSMac()
+
+    @staticmethod
+    def isOSUNIX():
+        return Configuration.isOSUNIX()
+
+    @staticmethod
+    def isOSSolaris():
+        return Configuration.isOSSolaris()
