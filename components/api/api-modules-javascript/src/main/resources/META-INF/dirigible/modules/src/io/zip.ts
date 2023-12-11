2325e52c9c3b87f029a18e334581f3a3c3fcea64
tivmof <tomyivanov06@gmail.com>
2023-12-11 23:14:41 +0200

    Update: types of methods and parameters added and updated in zip.ts

--- components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/src/io/zip.ts
+++ components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/src/io/zip.ts
@@ -12,17 +12,18 @@
 /**
  * API Zip
  */
-import * as bytes from "@dirigible/io/bytes";
+import {bytes} from ".";
+import { InputStream, OutputStream } from "./streams";
 const ZipFacade = Java.type("org.eclipse.dirigible.components.api.io.ZipFacade");
 
-export function zip(sourcePath, zipTargetPath) {
+export function zip(sourcePath: string, zipTargetPath: string): void {
     ZipFacade.exportZip(sourcePath, zipTargetPath);
 };
-export function unzip(zipPath, targetPath) {
+export function unzip(zipPath: string, targetPath: string): void {
     ZipFacade.importZip(zipPath, targetPath);
 };
 
-export function createZipInputStream(inputStream) {
+export function createZipInputStream(inputStream: InputStream): ZipInputStream {
     const native = ZipFacade.createZipInputStream(inputStream.native);
     return new ZipInputStream(native);
 };
@@ -31,32 +32,32 @@ class ZipInputStream {
 
     constructor(private native) {}
 
-    getNextEntry() {
+    getNextEntry(): ZipEntry {
         const native = this.native.getNextEntry();
         const zipEntry = new ZipEntry(native);
         return zipEntry;
     };
 
-    read() {
+    read(): Array<bytes> {
         const native = ZipFacade.readNative(this.native);
         return bytes.toJavaScriptBytes(native);
     };
 
-    readNative() {
+    readNative(): Array<bytes> {
         return ZipFacade.readNative(this.native);
     };
 
-    readText() {
+    readText(): string {
         return ZipFacade.readText(this.native);
     };
 
-    close() {
+    close(): void {
         this.native.close();
     };
 
 };
 
-export function createZipOutputStream(outputStream) {
+export function createZipOutputStream(outputStream: OutputStream): ZipOutputStream {
     const native = ZipFacade.createZipOutputStream(outputStream.native);
     return new ZipOutputStream(native);
 };
@@ -65,31 +66,31 @@ class ZipOutputStream {
 
     constructor(private native) {}
 
-    createZipEntry(name) {
+    createZipEntry(name: string): ZipEntry {
         const nativeNext = ZipFacade.createZipEntry(name);
         const zipEntry = new ZipEntry(nativeNext);
         this.native.putNextEntry(nativeNext);
         return zipEntry;
     };
 
-    write(data) {
+    write(data): void {
         const native = bytes.toJavaBytes(data);
         ZipFacade.writeNative(this.native, native);
     };
 
-    writeNative(data) {
+    writeNative(data): void {
         ZipFacade.writeNative(this.native, data);
     };
 
-    writeText(text) {
+    writeText(text: string): void {
         ZipFacade.writeText(this.native, text);
     };
 
-    closeEntry() {
+    closeEntry(): void {
         this.native.closeEntry();
     };
 
-    close() {
+    close(): void {
         this.native.finish();
         this.native.flush();
         this.native.close();
@@ -104,19 +105,19 @@ class ZipEntry {
 
     constructor(private native) {}
 
-    getName() {
+    getName(): string {
         return this.native.getName();
     };
 
-    getSize() {
+    getSize(): number {
         return this.native.getSize();
     };
 
-    getCompressedSize() {
+    getCompressedSize(): number {
         return this.native.getCompressedSize();
     };
 
-    getTime() {
+    getTime(): Date {
         return this.native.getTime();
     };
 
@@ -128,11 +129,11 @@ class ZipEntry {
         return this.native.getComment();
     };
 
-    isDirectory() {
+    isDirectory(): boolean {
         return this.native.isDirectory();
     };
 
-    isValid() {
+    isValid(): typeof this.native {
         return this.native !== null;
     };
 
