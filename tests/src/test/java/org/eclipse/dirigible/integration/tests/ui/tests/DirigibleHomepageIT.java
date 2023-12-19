fffd052a804d547bdb828855a19bf1b8e0868fd4
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2023-12-19 18:21:25 +0200

    fix integration tests execution
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- tests/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/DirigibleHomepageIT.java
+++ tests/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/DirigibleHomepageIT.java
@@ -14,7 +14,10 @@ import org.eclipse.dirigible.integration.tests.ui.Dirigible;
 import org.eclipse.dirigible.integration.tests.ui.framework.HtmlElementType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
 class DirigibleHomepageIT extends UserInterfaceIntegrationTest {
 
     private static final String ECLIPSE_DIRIGIBLE_HEADER = "Eclipse Dirigible";
