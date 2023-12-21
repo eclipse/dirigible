02b16c9f0dca84d6e55b3e2838c28407f73e09ab
Nedelcho Delchev <nedelcho.delchev@gmail.com>
2023-12-21 14:47:37 +0200

    fix snapshot version and test

--- components/api/api-s3/src/test/java/org/eclipse/dirigible/components/api/s3/S3FacadeTest.java
+++ components/api/api-s3/src/test/java/org/eclipse/dirigible/components/api/s3/S3FacadeTest.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible
+ * contributors SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.dirigible.components.api.s3;
 
 import org.junit.jupiter.api.AfterAll;
@@ -30,7 +40,7 @@ public class S3FacadeTest {
 
     private static S3Client s3Client;
 
-    private static final String BUCKET_NAME = "test-bucket";
+    private static final String BUCKET_NAME = "cmis-bucket";
 
     private static DockerImageName localstackImage = DockerImageName.parse("localstack/localstack:latest");
 
