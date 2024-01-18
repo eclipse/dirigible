0fc71ece70e77deefc8e540c25b636fcc2f4400d
tivmof <tomyivanov06@gmail.com>
2024-01-18 03:16:00 +0200

    Update: added types to parameters and methods in engines.ts

--- components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/src/template/engines.ts
+++ components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/src/template/engines.ts
@@ -21,60 +21,63 @@ const MUSTACHE_FILE_EXTENSION = ".mustache";
 
 const TemplateEnginesFacade = Java.type("org.eclipse.dirigible.components.api.templates.TemplateEnginesFacade");
 
-export function getDefaultEngine() {
-    return getVelocityEngine();
-};
+export class Engines{
 
-export function getMustacheEngine() {
-    const engine = TemplateEnginesFacade.getMustacheEngine();
-    return new TemplateEngine(engine, "mustache");
-};
+    public static getDefaultEngine(): TemplateEngine {
+        return this.getVelocityEngine();
+    };
 
-export function getVelocityEngine() {
-    const engine = TemplateEnginesFacade.getVelocityEngine();
-    return new TemplateEngine(engine, "velocity");
-};
+    public static getMustacheEngine(): TemplateEngine {
+        const engine = TemplateEnginesFacade.getMustacheEngine();
+        return new TemplateEngine(engine, "mustache");
+    };
 
-export function getJavascriptEngine() {
-    const engine = TemplateEnginesFacade.getJavascriptEngine();
-    return new TemplateEngine(engine, "javascript");
-};
+    public static getVelocityEngine(): TemplateEngine {
+        const engine = TemplateEnginesFacade.getVelocityEngine();
+        return new TemplateEngine(engine, "velocity");
+    };
 
-export function generate(template, parameters) {
-    return getDefaultEngine().generate(template, parameters);
-};
+    public static getJavascriptEngine(): TemplateEngine {
+        const engine = TemplateEnginesFacade.getJavascriptEngine();
+        return new TemplateEngine(engine, "javascript");
+    };
 
-export function generateFromFile(location, parameters) {
-    const resource = repository.getResource(REGISTRY_PUBLIC + location);
-    if (resource.exists()) {
-        const isMustacheTemplate = location.endsWith(MUSTACHE_FILE_EXTENSION);
-        const engine = isMustacheTemplate ? getMustacheEngine() : getDefaultEngine();
-        const template = resource.getText();
-        return engine.generate(template, parameters);
-    }
-    return null;
-};
+    public static generate(template: string, parameters: object) {
+        return this.getDefaultEngine().generate(template, parameters);
+    };
+
+    public static generateFromFile(location: string, parameters: object) {
+        const resource = repository.getResource(REGISTRY_PUBLIC + location);
+        if (resource.exists()) {
+            const isMustacheTemplate = location.endsWith(MUSTACHE_FILE_EXTENSION);
+            const engine = isMustacheTemplate ? this.getMustacheEngine() : this.getDefaultEngine();
+            const template = resource.getText();
+            return engine.generate(template, parameters);
+        }
+        return null;
+    };
+}
 
 class TemplateEngine {
 
     private sm: any;
     private em: any;
 
-    constructor(private engine, type) {
+    constructor(private engine: any, type: any) {
         this.sm = type === "mustache" ? "{{" : null;
         this.em = type === "mustache" ? "}}" : null;
     }
 
 
-    generate(template, parameters) {
+    generate(template: string, parameters: object) {
         return this.engine.generate(template, JSON.stringify(parameters), this.sm, this.em);
     };
 
-    setSm(sm) {
+    setSm(sm: any) {
         this.sm = sm;
     };
 
-    setEm(em) {
+    setEm(em: any) {
         this.em = em;
     };
 }
