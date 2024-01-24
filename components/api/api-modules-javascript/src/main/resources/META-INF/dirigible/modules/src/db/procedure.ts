3944831bf9b9deaca819ef37a0d4de897aaf1671
ThuF <jordan.p.pavlov@gmail.com>
2024-01-24 18:56:07 +0200

    [API] Procedure - Migration

--- components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/src/db/procedure.ts
+++ components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/src/db/procedure.ts
@@ -16,83 +16,95 @@
 import { Update } from "./update";
 import * as database from "./database";
 
-export function create(sql, datasourceName) {
-    let parameters = [];
-    Update.execute(sql, parameters, datasourceName);
-};
+export interface ProcedureParameter {
+	readonly type: string;
+	readonly value: any;
+}
 
-export function execute(sql, parameters, datasourceName) {
-    let result = [];
+export class Procedure {
 
-    let connection = null;
-    let callableStatement = null;
-    let resultSet = null;
-
-    if (parameters === undefined || parameters === null) {
-        parameters = [];
+    public static create(sql: string, datasourceName?: string): void {
+        Update.execute(sql, [], datasourceName);
     }
-    try {
-        let hasMoreResults = false;
 
-        connection = database.getConnection(datasourceName);
-        callableStatement = connection.prepareCall(sql);
-        let mappedParameters = parameters.map((parameter) => {
-            let mappedParameter = {
-                value: parameter,
-                type: ""
-            };
-            let parameterType = typeof parameter;
-            if (parameterType === "object") {
-                mappedParameter = parameter;
-            } else if (parameterType === "string") {
-                mappedParameter.type = "string";
-            } else if (parameterType === "number") {
-                mappedParameter.type = parameter % 1 === 0 ? "int" : "double";
-            } else {
-                throw new Error(`Procedure Call - Unsupported parameter type [${parameterType}]`);
-            }
-            return mappedParameter;
-        });
-        for (let i = 0; i < mappedParameters.length; i++) {
-            switch (mappedParameters[i].type) {
-                case "string":
-                    callableStatement.setString(i + 1, mappedParameters[i].value);
-                    break;
-                case "int":
-                case "integer":
-                case "number":
-                    callableStatement.setInt(i + 1, mappedParameters[i].value);
-                    break;
-                case "float":
-                    callableStatement.setFloat(i + 1, mappedParameters[i].value);
-                    break;
-                case "double":
-                    callableStatement.setDouble(i + 1, mappedParameters[i].value);
-                    break;
-            }
-        }
-        resultSet = callableStatement.executeQuery();
+    public static execute(sql: string, parameters: (string | number | ProcedureParameter)[] = [], datasourceName?: string): any[] {
+        const result = [];
 
-        do {
-            result.push(JSON.parse(resultSet.toJson()));
-            hasMoreResults = callableStatement.getMoreResults();
-            if (hasMoreResults) {
-                resultSet.close();
-                resultSet = callableStatement.getResultSet();
-            }
-        } while (hasMoreResults)
+        let connection = null;
+        let callableStatement = null;
+        let resultSet = null;
 
-        callableStatement.close();
-    } finally {
-        if (resultSet != null) {
-            resultSet.close();
-        }
-        if (callableStatement != null) {
+        try {
+            let hasMoreResults = false;
+
+            connection = database.getConnection(datasourceName);
+            callableStatement = connection.prepareCall(sql);
+            let mappedParameters = parameters.map((parameter) => {
+                let mappedParameter: ProcedureParameter = {
+                    value: parameter,
+                    type: ""
+                };
+                let parameterType = typeof parameter;
+                if (parameterType === "object") {
+                    mappedParameter = parameter as ProcedureParameter;
+                } else if (parameterType === "string") {
+                    // @ts-ignore
+                    mappedParameter.type = "string";
+                } else if (parameterType === "number") {
+                    // @ts-ignore
+                    mappedParameter.type = parameter % 1 === 0 ? "int" : "double";
+                } else {
+                    throw new Error(`Procedure Call - Unsupported parameter type [${parameterType}]`);
+                }
+                return mappedParameter;
+            });
+            for (let i = 0; i < mappedParameters.length; i++) {
+                switch (mappedParameters[i].type) {
+                    case "string":
+                        callableStatement.setString(i + 1, mappedParameters[i].value);
+                        break;
+                    case "int":
+                    case "integer":
+                    case "number":
+                        callableStatement.setInt(i + 1, mappedParameters[i].value);
+                        break;
+                    case "float":
+                        callableStatement.setFloat(i + 1, mappedParameters[i].value);
+                        break;
+                    case "double":
+                        callableStatement.setDouble(i + 1, mappedParameters[i].value);
+                        break;
+                }
+            }
+            resultSet = callableStatement.executeQuery();
+
+            do {
+                result.push(JSON.parse(resultSet.toJson()));
+                hasMoreResults = callableStatement.getMoreResults();
+                if (hasMoreResults) {
+                    resultSet.close();
+                    resultSet = callableStatement.getResultSet();
+                }
+            } while (hasMoreResults)
+
             callableStatement.close();
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (callableStatement != null) {
+                callableStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
         }
-        if (connection != null) {
-            connection.close();
-        }
+        return result;
     }
-    return result;
-};
+}
+
+// @ts-ignore
+if (typeof module !== 'undefined') {
+	// @ts-ignore
+	module.exports = Procedure;
+}
