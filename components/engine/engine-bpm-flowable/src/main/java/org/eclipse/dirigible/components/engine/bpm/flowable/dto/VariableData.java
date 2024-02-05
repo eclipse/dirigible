7236a9cb41f60127af9cce46f4739bd534049e49
Aleksandar Zlatkovski <entrery.kn@gmail.com>
2024-02-05 21:35:14 +0200

    Feature#3333: Add process variable impl

---
+++ components/engine/engine-bpm-flowable/src/main/java/org/eclipse/dirigible/components/engine/bpm/flowable/dto/VariableData.java
@@ -0,0 +1,23 @@
+package org.eclipse.dirigible.components.engine.bpm.flowable.dto;
+
+public class VariableData {
+    private String name;
+
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
