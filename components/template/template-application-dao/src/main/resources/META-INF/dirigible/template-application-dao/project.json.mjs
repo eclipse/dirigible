5b7094f3e0550fee883ed73f02d1a2f1f3b9ea6a
delchev <delchevn@gmail.com>
2024-02-14 10:22:43 +0200

    existing project.json fix

--- components/template/template-application-dao/src/main/resources/META-INF/dirigible/template-application-dao/project.json.mjs
+++ components/template/template-application-dao/src/main/resources/META-INF/dirigible/template-application-dao/project.json.mjs
@@ -2,12 +2,37 @@ import { workspace } from "sdk/platform";
 
 export function generate(json) {
 	const parameters = JSON.parse(json);
+	
+	let projectFile = {
+		"guid": parameters.projectName,
+		"actions": [
+			{
+				"name": "Build TypeScript",
+				"commands": [
+					{
+						"os": "unix",
+						"command": "tsc"
+					},
+					{
+						"os": "windows",
+						"command": "cmd /c tsc"
+					}
+				],
+				"registry": "true"
+			}
+		]
+	}
+	const newProjectFile = JSON.stringify(projectFile);
 
 	let currenctWorkspace = workspace.getWorkspace(parameters.workspaceName);
 	let currentProject = currenctWorkspace.getProject(parameters.projectName);
 	let maybeProjectFile = currentProject.getFile("project.json");
 	if (maybeProjectFile.exists()) {
-		let projectFile = JSON.parse(maybeProjectFile.getText());
+		const projectFileContent = maybeProjectFile.getText();
+		if (projectFileContent.trim() === "") {
+			return newProjectFile;
+		}
+		let projectFile = JSON.parse(projectFileContent);
 		if (!projectFile.actions) {
 			projectFile.actions = [];
 			projectFile.actions.push({
@@ -25,27 +50,10 @@ export function generate(json) {
 				"registry": "true"
 			});
 			return JSON.stringify(projectFile);
+		} else {
+			return projectFileContent;
 		}
 	} else {
-		let projectFile = {
-			"guid": parameters.projectName,
-			"actions": [
-				{
-					"name": "Build TypeScript",
-					"commands": [
-						{
-							"os": "unix",
-							"command": "tsc"
-						},
-						{
-							"os": "windows",
-							"command": "cmd /c tsc"
-						}
-					],
-					"registry": "true"
-				}
-			]
-		}
-		return JSON.stringify(projectFile);
+		return newProjectFile;
 	}
 }
