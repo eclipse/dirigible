d27093547af0f632b6632d254384be2797d25009
MrGoblings <emomomchev@gmail.com>
2024-02-15 08:36:28 +0200

    made client into a class with static methods

--- components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/src/git/client.ts
+++ components/api/api-modules-javascript/src/main/resources/META-INF/dirigible/modules/src/git/client.ts
@@ -12,94 +12,96 @@
 
 const GitFacade = Java.type("org.eclipse.dirigible.components.api.git.GitFacade");
 
-export function initRepository(user: string, email: string, workspaceName: string, projectName: string, repositoryName: string, commitMessage: string): void {
-    GitFacade.initRepository(user, email, workspaceName, projectName, repositoryName, commitMessage);
-}
+export class Client {
+    public static initRepository(user: string, email: string, workspaceName: string, projectName: string, repositoryName: string, commitMessage: string): void {
+        GitFacade.initRepository(user, email, workspaceName, projectName, repositoryName, commitMessage);
+    }
 
-export function commit(user: string, userEmail: string, workspaceName: string, repositoryName: string, commitMessage: string, add: boolean): void  {
-    GitFacade.commit(user, userEmail, workspaceName, repositoryName, commitMessage, add);
-}
+    public static commit(user: string, userEmail: string, workspaceName: string, repositoryName: string, commitMessage: string, add: boolean): void  {
+        GitFacade.commit(user, userEmail, workspaceName, repositoryName, commitMessage, add);
+    }
 
-export function getGitRepositories(workspaceName: string): any[] /* ProjectDescriptor[] */ {
-    return GitFacade.getGitRepositories(workspaceName);
-}
+    public static getGitRepositories(workspaceName: string): any[] /* ProjectDescriptor[] */ {
+        return GitFacade.getGitRepositories(workspaceName);
+    }
 
-export function getHistory(repositoryName: string, workspaceName: string, path: string): any[] /* GitCommitInfo[] */ {
-    return GitFacade.getHistory(repositoryName, workspaceName, path);
-}
+    public static getHistory(repositoryName: string, workspaceName: string, path: string): any[] /* GitCommitInfo[] */ {
+        return GitFacade.getHistory(repositoryName, workspaceName, path);
+    }
 
-export function deleteRepository(workspaceName: string, repositoryName: string): void {
-    GitFacade.deleteRepository(workspaceName, repositoryName);
-}
+    public static deleteRepository(workspaceName: string, repositoryName: string): void {
+        GitFacade.deleteRepository(workspaceName, repositoryName);
+    }
 
-export function cloneRepository(workspaceName: string, repositoryUri: string, username: string, password: string, branch: string): any /* cloneRepository */ {
-    return GitFacade.cloneRepository(workspaceName, repositoryUri, username, password, branch);
-}
+    public static cloneRepository(workspaceName: string, repositoryUri: string, username: string, password: string, branch: string): any /* cloneRepository */ {
+        return GitFacade.cloneRepository(workspaceName, repositoryUri, username, password, branch);
+    }
 
-export function pull(workspaceName: string, repositoryName: string, username: string, password: string) {
-    GitFacade.pull(workspaceName, repositoryName, username, password);
-}
+    public static pull(workspaceName: string, repositoryName: string, username: string, password: string) {
+        GitFacade.pull(workspaceName, repositoryName, username, password);
+    }
 
-export function push(workspaceName: string, repositoryName: string, username: string, password: string): void {
-    return GitFacade.push(workspaceName, repositoryName, username, password);
-}
+    public static push(workspaceName: string, repositoryName: string, username: string, password: string): void {
+        return GitFacade.push(workspaceName, repositoryName, username, password);
+    }
 
-export function checkout(workspaceName: string, repositoryName: string, branchName: string): void {
-    GitFacade.checkout(workspaceName, repositoryName, branchName);
-}
+    public static checkout(workspaceName: string, repositoryName: string, branchName: string): void {
+        GitFacade.checkout(workspaceName, repositoryName, branchName);
+    }
 
-export function createBranch(workspaceName: string, repositoryName: string, branchName: string, startingPoint: string): void {
-    GitFacade.createBranch(workspaceName, repositoryName, branchName, startingPoint);
-}
+    public static createBranch(workspaceName: string, repositoryName: string, branchName: string, startingPoint: string): void {
+        GitFacade.createBranch(workspaceName, repositoryName, branchName, startingPoint);
+    }
 
-export function deleteBranch(workspaceName: string, repositoryName: string, branchName: string): void {
-    GitFacade.createBranch(workspaceName, repositoryName, branchName);
-}
+    public static deleteBranch(workspaceName: string, repositoryName: string, branchName: string): void {
+        GitFacade.createBranch(workspaceName, repositoryName, branchName);
+    }
 
-export function renameBranch(workspaceName: string, repositoryName: string, oldName: string, newName: string): void {
-    GitFacade.createBranch(workspaceName, repositoryName, oldName, newName);
-}
+    public static renameBranch(workspaceName: string, repositoryName: string, oldName: string, newName: string): void {
+        GitFacade.createBranch(workspaceName, repositoryName, oldName, newName);
+    }
 
-export function createRemoteBranch(workspaceName: string, repositoryName: string, branchName: string, startingPoint: string, username: string, password: string): void {
-    GitFacade.createRemoteBranch(workspaceName, repositoryName, branchName, startingPoint, username, password);
-}
+    public static createRemoteBranch(workspaceName: string, repositoryName: string, branchName: string, startingPoint: string, username: string, password: string): void {
+        GitFacade.createRemoteBranch(workspaceName, repositoryName, branchName, startingPoint, username, password);
+    }
 
-export function deleteRemoteBranch(workspaceName: string, repositoryName: string, branchName: string, username: string, password: string): void {
-    GitFacade.createRemoteBranch(workspaceName, repositoryName, branchName, username, password);
-}
+    public static deleteRemoteBranch(workspaceName: string, repositoryName: string, branchName: string, username: string, password: string): void {
+        GitFacade.createRemoteBranch(workspaceName, repositoryName, branchName, username, password);
+    }
 
-export function hardReset(workspaceName: string, repositoryName: string): void {
-    GitFacade.hardReset(workspaceName, repositoryName);
-}
+    public static hardReset(workspaceName: string, repositoryName: string): void {
+        GitFacade.hardReset(workspaceName, repositoryName);
+    }
 
-export function rebase(workspaceName: string, repositoryName: string, branchName: string): void {
-    GitFacade.rebase(workspaceName, repositoryName, branchName);
-}
+    public static rebase(workspaceName: string, repositoryName: string, branchName: string): void {
+        GitFacade.rebase(workspaceName, repositoryName, branchName);
+    }
 
-export function status(workspaceName: string, repositoryName: string): any /* Status */ {
-    return GitFacade.status(workspaceName, repositoryName);
-}
+    public static status(workspaceName: string, repositoryName: string): any /* Status */ {
+        return GitFacade.status(workspaceName, repositoryName);
+    }
 
-export function getBranch(workspaceName: string, repositoryName: string): string {
-    return GitFacade.getBranch(workspaceName, repositoryName);
-}
+    public static getBranch(workspaceName: string, repositoryName: string): string {
+        return GitFacade.getBranch(workspaceName, repositoryName);
+    }
 
-export function getLocalBranches(workspaceName: string, repositoryName: string): any[] /* GitBranch[] */ {
-    return GitFacade.getLocalBranches(workspaceName, repositoryName);
-}
+    public static getLocalBranches(workspaceName: string, repositoryName: string): any[] /* GitBranch[] */ {
+        return GitFacade.getLocalBranches(workspaceName, repositoryName);
+    }
 
-export function getRemoteBranches(workspaceName: string, repositoryName: string): any[] /* GitBranch[] */ {
-    return GitFacade.getRemoteBranches(workspaceName, repositoryName);
-}
+    public static getRemoteBranches(workspaceName: string, repositoryName: string): any[] /* GitBranch[] */ {
+        return GitFacade.getRemoteBranches(workspaceName, repositoryName);
+    }
 
-export function getUnstagedChanges(workspaceName: string, repositoryName: string): any /* GitChangedFiles */ {
-    return GitFacade.getUnstagedChanges(workspaceName, repositoryName);
-}
+    public static getUnstagedChanges(workspaceName: string, repositoryName: string): any /* GitChangedFiles */ {
+        return GitFacade.getUnstagedChanges(workspaceName, repositoryName);
+    }
 
-export function getStagedChanges(workspaceName: string, repositoryName: string): any /* GitChangedFiles */ {
-    return GitFacade.getStagedChanges(workspaceName, repositoryName);
-}
+    public static getStagedChanges(workspaceName: string, repositoryName: string): any /* GitChangedFiles */ {
+        return GitFacade.getStagedChanges(workspaceName, repositoryName);
+    }
 
-export function getFileContent(workspaceName: string, repositoryName: string, filePath: string, revStr: string): string {
-    return GitFacade.getFileContent(workspaceName, repositoryName, filePath, revStr);
+    public static getFileContent(workspaceName: string, repositoryName: string, filePath: string, revStr: string): string {
+        return GitFacade.getFileContent(workspaceName, repositoryName, filePath, revStr);
+    }
 }
