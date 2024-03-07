b820919b34d3c30d9477f1b9c6bb2a575a9598c4
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-03-07 14:14:36 +0200

    add doc
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/core/core-base/src/main/java/org/eclipse/dirigible/components/base/tenant/DefaultTenant.java
+++ components/core/core-base/src/main/java/org/eclipse/dirigible/components/base/tenant/DefaultTenant.java
@@ -6,9 +6,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.beans.factory.annotation.Qualifier;
 
+/**
+ *
+ * use it to inject the default tenant {@link org.eclipse.dirigible.components.base.tenant.Tenant}
+ *
+ */
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Qualifier
 public @interface DefaultTenant {
-    // use it to inject the default tenant
+    //
 }
