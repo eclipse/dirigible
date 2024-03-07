d5143aa6d04673339b853d4e66d82e8b90fe1db2
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-03-07 13:10:56 +0200

    refactoring
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/security/security-keycloak/src/main/java/org/eclipse/dirigible/components/security/keycloak/CustomOidcUserService.java
+++ components/security/security-keycloak/src/main/java/org/eclipse/dirigible/components/security/keycloak/CustomOidcUserService.java
@@ -9,6 +9,8 @@ import org.eclipse.dirigible.components.base.http.roles.Roles;
 import org.eclipse.dirigible.components.base.tenant.TenantContext;
 import org.eclipse.dirigible.components.tenants.domain.User;
 import org.eclipse.dirigible.components.tenants.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -24,6 +26,8 @@ import org.springframework.util.StringUtils;
 @Service
 public class CustomOidcUserService extends OidcUserService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomOidcUserService.class);
+
     private static final OAuth2Error USER_NOT_IN_TENANT_ERR = new OAuth2Error("user_not_registered_in_tenant");
 
     private final UserService userService;
@@ -36,25 +40,40 @@ public class CustomOidcUserService extends OidcUserService {
 
     @Override
     public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
-
         OidcUser oidcUser = super.loadUser(userRequest);
-        if (isTrialEnabled()) {
-            return createTrialUser(userRequest, oidcUser);
-        }
 
-        String username = oidcUser.getName();
-        String tenantId = tenantContext.getCurrentTenant()
-                                       .getId();
-        User user = userService.findUserByUsernameAndTenantId(username, tenantId)
-                               .orElseThrow(() -> {
-                                   String message = "User with username [" + username + "] in tenant [" + tenantId + "] was not found";
-                                   return new OAuth2AuthenticationException(USER_NOT_IN_TENANT_ERR, message);
-                               });
-        Set<String> roleNames = userService.getUserRoleNames(user);
+        User user = getTenantUser(oidcUser);
+        Set<String> roleNames = getRoleNames(user);
         Set<GrantedAuthority> roleAuthorities = toAuthorities(roleNames);
 
         return createOidcUser(userRequest, oidcUser, roleAuthorities);
 
+    }
+
+    private User getTenantUser(OidcUser oidcUser) {
+        if (isTrialEnabled()) {
+            LOGGER.debug("Trial enabled - user [{}] will not be checked for the current tenant.", oidcUser.getName());
+            return null;
+        }
+        String username = oidcUser.getName();
+        String tenantId = tenantContext.getCurrentTenant()
+                                       .getId();
+
+        return userService.findUserByUsernameAndTenantId(username, tenantId)
+                          .orElseThrow(() -> {
+                              String message = "User [" + username + "] was not found in tenant [" + tenantId + "].";
+                              return new OAuth2AuthenticationException(USER_NOT_IN_TENANT_ERR, message);
+                          });
+    }
+
+    private Set<String> getRoleNames(User user) {
+        if (isTrialEnabled()) {
+            LOGGER.debug("Trial enabled - returning all available system roles for the current user.");
+            return Arrays.stream(Roles.values())
+                         .map(Roles::getRoleName)
+                         .collect(Collectors.toSet());
+        }
+        return userService.getUserRoleNames(user);
     }
 
     private boolean isTrialEnabled() {
@@ -62,22 +81,11 @@ public class CustomOidcUserService extends OidcUserService {
         return Boolean.valueOf(configValue);
     }
 
-    /**
-     * for trial <br>
-     * - assign all available roles to all users<br>
-     * - skip checking whether the user is registered for the tenant
-     *
-     * @param userRequest
-     * @param oidcUser
-     * @return
-     */
-    private OidcUser createTrialUser(OidcUserRequest userRequest, OidcUser oidcUser) {
-        Set<String> roleNames = Arrays.stream(Roles.values())
-                                      .map(Roles::getRoleName)
-                                      .collect(Collectors.toSet());
-        Set<GrantedAuthority> roleAuthorities = toAuthorities(roleNames);
+    private Set<GrantedAuthority> toAuthorities(Collection<String> roleNames) {
+        return roleNames.stream()
+                        .map(r -> new SimpleGrantedAuthority(r))
+                        .collect(Collectors.toSet());
 
-        return createOidcUser(userRequest, oidcUser, roleAuthorities);
     }
 
     private OidcUser createOidcUser(OidcUserRequest userRequest, OidcUser oidcUser, Set<GrantedAuthority> additionalAuthorities) {
@@ -87,13 +95,6 @@ public class CustomOidcUserService extends OidcUserService {
                                                   .getUserNameAttributeName();
         return StringUtils.hasText(userNameAttributeName) ? new CustomOidcUser(oidcUser, userNameAttributeName, additionalAuthorities)
                 : new CustomOidcUser(oidcUser, additionalAuthorities);
-    }
-
-    private Set<GrantedAuthority> toAuthorities(Collection<String> roleNames) {
-        return roleNames.stream()
-                        .map(r -> new SimpleGrantedAuthority(r))
-                        .collect(Collectors.toSet());
-
     }
 
 }
