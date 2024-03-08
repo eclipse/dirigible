9bd3dad0c301dd948779ac7bcf08dd285e203681
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-03-08 20:20:28 +0200

    Revert "add logs"
    This reverts commit a6bd166f3ffea40afec694f2d7d92904b4544e43.

--- tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/api/java/messaging/BackgroundListenersIT.java
+++ tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/api/java/messaging/BackgroundListenersIT.java
@@ -100,9 +100,7 @@ class BackgroundListenersIT extends IntegrationTest {
 
             MessagingFacade.sendToTopic(TOPIC_NAME, testMessage);
 
-            System.out.println("!!! Stopping the broker");
             broker.stop();
-            System.out.println("!!! Broker stopped");
 
             Awaitility.await()
                       .atMost(3, TimeUnit.SECONDS)
