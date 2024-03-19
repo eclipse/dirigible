2760f8830ec5a010697d0f03c740faf8a47c8cbf
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-03-19 14:21:54 +0200

    cleanup
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/engine/engine-listeners/src/main/java/org/eclipse/dirigible/components/listeners/service/Listener.java
+++ components/engine/engine-listeners/src/main/java/org/eclipse/dirigible/components/listeners/service/Listener.java
@@ -1,8 +1,5 @@
 package org.eclipse.dirigible.components.listeners.service;
 
-import org.eclipse.dirigible.components.base.spring.BeanProvider;
-import org.eclipse.dirigible.components.listeners.domain.ListenerKind;
-
 import java.util.Objects;
 
 class Listener {
@@ -47,24 +44,5 @@ class Listener {
     @Override
     public int hashCode() {
         return Objects.hash(type, destination, handlerPath);
-    }
-
-    static Listener fromEntity(org.eclipse.dirigible.components.listeners.domain.Listener entity) {
-        ListenerType type = fromEntityType(entity.getKind());
-
-        DestinationNameManager destinationNameManager = BeanProvider.getBean(DestinationNameManager.class);
-        String destination = destinationNameManager.toTenantName(entity.getName());
-        return new Listener(type, destination, entity.getHandler());
-    }
-
-    private static ListenerType fromEntityType(ListenerKind kind) {
-        if (null == kind) {
-            throw new IllegalArgumentException("Listener kind cannot be null");
-        }
-        return switch (kind) {
-            case QUEUE -> ListenerType.QUEUE;
-            case TOPIC -> ListenerType.TOPIC;
-            default -> throw new IllegalArgumentException("Unsupported listener kind: " + kind);
-        };
     }
 }
