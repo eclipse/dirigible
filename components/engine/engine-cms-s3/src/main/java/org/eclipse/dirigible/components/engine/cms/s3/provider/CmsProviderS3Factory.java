8a2979b6aeeb4588975dabd0dbeb4f38ca3c7527
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-03-22 16:02:28 +0200

    cleanup
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/engine/engine-cms-s3/src/main/java/org/eclipse/dirigible/components/engine/cms/s3/provider/CmsProviderS3Factory.java
+++ components/engine/engine-cms-s3/src/main/java/org/eclipse/dirigible/components/engine/cms/s3/provider/CmsProviderS3Factory.java
@@ -1,8 +1,5 @@
 package org.eclipse.dirigible.components.engine.cms.s3.provider;
 
-import org.eclipse.dirigible.components.base.tenant.DefaultTenant;
-import org.eclipse.dirigible.components.base.tenant.Tenant;
-import org.eclipse.dirigible.components.base.tenant.TenantContext;
 import org.eclipse.dirigible.components.engine.cms.CmsProvider;
 import org.eclipse.dirigible.components.engine.cms.CmsProviderFactory;
 import org.eclipse.dirigible.components.engine.cms.CmsProviderInitializationException;
@@ -12,24 +9,9 @@ import org.springframework.stereotype.Component;
 @Component("cms-provider-s3")
 class CmsProviderS3Factory implements CmsProviderFactory {
 
-    private final TenantContext tenantContext;
-    private final Tenant defaultTenant;
-
-    CmsProviderS3Factory(TenantContext tenantContext, @DefaultTenant Tenant defaultTenant) {
-        this.tenantContext = tenantContext;
-        this.defaultTenant = defaultTenant;
-    }
-
     @Override
     public CmsProvider create() throws CmsProviderInitializationException {
         CmisS3Session session = new CmisS3Session();
         return new CmsProviderS3(session);
-    }
-
-    private String getTenantFolder() {
-        String tenantId = tenantContext.isNotInitialized() ? defaultTenant.getId()
-                : tenantContext.getCurrentTenant()
-                               .getId();
-        return tenantId;
     }
 }
