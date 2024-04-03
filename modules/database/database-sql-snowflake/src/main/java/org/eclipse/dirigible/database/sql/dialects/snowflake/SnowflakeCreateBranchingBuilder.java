67c2611ac176331c142c3122af12c7a68082e128
delchev <delchevn@gmail.com>
2024-04-03 13:43:18 +0300

    branchig adaptation

--- modules/database/database-sql-snowflake/src/main/java/org/eclipse/dirigible/database/sql/dialects/snowflake/SnowflakeCreateBranchingBuilder.java
+++ modules/database/database-sql-snowflake/src/main/java/org/eclipse/dirigible/database/sql/dialects/snowflake/SnowflakeCreateBranchingBuilder.java
@@ -38,4 +38,54 @@ public class SnowflakeCreateBranchingBuilder extends CreateBranchingBuilder {
         return new SnowflakeCreateTableBuilder(this.getDialect(), table, ISqlDialect.KEYWORD_HYBRID);
     }
 
+    /**
+     * Hybrid table.
+     *
+     * @param table the table
+     * @return the creates the table builder
+     */
+    public SnowflakeCreateTableBuilder hybridTable(String table) {
+        return new SnowflakeCreateTableBuilder(this.getDialect(), table, ISqlDialect.KEYWORD_HYBRID);
+    }
+
+    /**
+     * Dynamic table.
+     *
+     * @param table the table
+     * @return the creates the table builder
+     */
+    public SnowflakeCreateTableBuilder dynamicTable(String table) {
+        return new SnowflakeCreateTableBuilder(this.getDialect(), table, ISqlDialect.KEYWORD_DYNAMIC);
+    }
+
+    /**
+     * Event table.
+     *
+     * @param table the table
+     * @return the creates the table builder
+     */
+    public SnowflakeCreateTableBuilder eventTable(String table) {
+        return new SnowflakeCreateTableBuilder(this.getDialect(), table, ISqlDialect.KEYWORD_EVENT);
+    }
+
+    /**
+     * External table.
+     *
+     * @param table the table
+     * @return the creates the table builder
+     */
+    public SnowflakeCreateTableBuilder externalTable(String table) {
+        return new SnowflakeCreateTableBuilder(this.getDialect(), table, ISqlDialect.KEYWORD_EXTERNAL);
+    }
+
+    /**
+     * Iceberg table.
+     *
+     * @param table the table
+     * @return the creates the table builder
+     */
+    public SnowflakeCreateTableBuilder icebergTable(String table) {
+        return new SnowflakeCreateTableBuilder(this.getDialect(), table, ISqlDialect.KEYWORD_ICEBERG);
+    }
+
 }
