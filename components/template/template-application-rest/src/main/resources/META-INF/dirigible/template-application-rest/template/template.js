6c25184e8b0c47254aa60f2c207ced9f88ef9cb5
ThuF <jordan.p.pavlov@gmail.com>
2024-04-18 11:40:47 +0300

    [EDM] Generate OpenAPI - Add OpenAPI specific optional parameters during generation

--- components/template/template-application-rest/src/main/resources/META-INF/dirigible/template-application-rest/template/template.js
+++ components/template/template-application-rest/src/main/resources/META-INF/dirigible/template-application-rest/template/template.js
@@ -61,6 +61,133 @@ exports.getTemplate = function (parameters) {
 
     let templateParameters = [];
     templateParameters = templateParameters.concat(daoTemplate.parameters);
+    templateParameters.push
+
+    templateParameters.push({
+        name: "addOpenApiInfo",
+        label: "OpenAPI Info (Optional)",
+        type: "checkbox",
+    });
+    templateParameters.push({
+        name: "openApiTitle",
+        label: "OpenAPI Title",
+        placeholder: "Enter API title, if not provided the application title is taken",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
+    templateParameters.push({
+        name: "openApiVersion",
+        label: "OpenAPI Version",
+        placeholder: "Enter API version, default value is 1.0.0",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
+    templateParameters.push({
+        name: "openApiDescription",
+        label: "OpenAPI Description",
+        placeholder: "Enter API description, if not provided the application description is taken",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
+    templateParameters.push({
+        name: "openApiTermsOfService",
+        label: "OpenAPI Terms Of Service",
+        placeholder: "Enter API terms of service",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
+    templateParameters.push({
+        name: "openApiContactEmail",
+        label: "OpenAPI Contact Email",
+        placeholder: "Enter API contact email",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
+    templateParameters.push({
+        name: "openApiContactUrl",
+        label: "OpenAPI Contact URL",
+        placeholder: "Enter API contact URL",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
+    templateParameters.push({
+        name: "openApiLicenseName",
+        label: "OpenAPI License Name",
+        placeholder: "Enter API license name",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
+    templateParameters.push({
+        name: "openApiLicenseUrl",
+        label: "OpenAPI License URL",
+        placeholder: "Enter API license URL",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
+    templateParameters.push({
+        name: "openApiExternalDocsDescription",
+        label: "OpenAPI External Docs Description",
+        placeholder: "Enter API external docs description",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
+    templateParameters.push({
+        name: "openApiExternalDocsUrl",
+        label: "OpenAPI External Docs URL",
+        placeholder: "Enter API external docs URL",
+        required: false,
+        ui: {
+            hide: {
+                property: "addOpenApiInfo",
+                value: false
+            }
+        }
+    });
 
     return {
         name: "Application - REST",
