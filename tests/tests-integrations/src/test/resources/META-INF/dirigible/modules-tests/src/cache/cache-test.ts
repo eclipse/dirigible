b9ca2759d3928c35e36aa2e1b09aecb68a1edafe
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-05-08 15:36:22 +0200

    Improve cache API (#3895)
    * improve cache API
    
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>
    
    * fix json
    
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>
    
    * fix ts
    
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>
    
    * fix test
    
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>
    
    * add more checks
    
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>
    
    * increase cache size and expire time
    
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>
    
    ---------
    
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- tests/tests-integrations/src/test/resources/META-INF/dirigible/modules-tests/src/cache/cache-test.ts
+++ tests/tests-integrations/src/test/resources/META-INF/dirigible/modules-tests/src/cache/cache-test.ts
@@ -25,3 +25,62 @@ test('cache-api-test', () => {
     assertFalse(caches.contains(KEY))
     assertEquals(null, caches.get(KEY));
 });
+
+test('cache-api-boolean', () => {
+
+    const KEY = "boolean-key";
+    const VALUE = true;
+
+    caches.clear();
+
+    caches.set(KEY, VALUE);
+    assertEquals(VALUE, caches.get(KEY));
+});
+
+test('cache-api-number', () => {
+
+    const KEY = "number-key";
+    const VALUE = 123;
+
+    caches.clear();
+
+    caches.set(KEY, VALUE);
+    assertEquals(VALUE, caches.get(KEY));
+});
+
+test('cache-api-string', () => {
+
+    const KEY = "string-key";
+    const VALUE = "This is a string value";
+
+    caches.clear();
+
+    caches.set(KEY, VALUE);
+    assertEquals(VALUE, caches.get(KEY));
+});
+
+test('cache-api-object', () => {
+
+    const KEY = "object-key";
+    const VALUE = {
+        name : "Ivan",
+        age: 35,
+        parent:  {
+           name : "Petar",
+           age: 55
+        }
+    };
+
+    caches.clear();
+
+    caches.set(KEY, VALUE);
+
+    const cachedValue = caches.get(KEY);
+    assertTrue(cachedValue != null);
+
+    assertEquals("Ivan", cachedValue.name);
+    assertEquals(35, cachedValue.age);
+
+    assertEquals("Petar", cachedValue.parent.name);
+    assertEquals(55, cachedValue.parent.age);
+});
