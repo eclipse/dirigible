d4ab7b046aae78bc0162704dda644286f6d2d173
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-05-21 13:38:30 +0300

    handle missing table metadata properly (currently it results to NPE)
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/engine/engine-odata/src/main/java/org/eclipse/dirigible/components/odata/transformers/OData2ODataMTransformer.java
+++ components/engine/engine-odata/src/main/java/org/eclipse/dirigible/components/odata/transformers/OData2ODataMTransformer.java
@@ -9,27 +9,22 @@
  */
 package org.eclipse.dirigible.components.odata.transformers;
 
+import org.eclipse.dirigible.commons.config.Configuration;
+import org.eclipse.dirigible.components.data.structures.domain.Table;
+import org.eclipse.dirigible.components.data.structures.domain.TableColumn;
+import org.eclipse.dirigible.components.data.structures.domain.TableConstraintForeignKey;
+import org.eclipse.dirigible.components.odata.api.*;
+import org.eclipse.dirigible.components.odata.domain.OData;
+import org.eclipse.dirigible.database.sql.ISqlKeywords;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
-
-import org.eclipse.dirigible.commons.config.Configuration;
-import org.eclipse.dirigible.components.data.structures.domain.Table;
-import org.eclipse.dirigible.components.data.structures.domain.TableColumn;
-import org.eclipse.dirigible.components.data.structures.domain.TableConstraintForeignKey;
-import org.eclipse.dirigible.components.odata.api.ODataAssociation;
-import org.eclipse.dirigible.components.odata.api.ODataAssociationEnd;
-import org.eclipse.dirigible.components.odata.api.ODataEntity;
-import org.eclipse.dirigible.components.odata.api.ODataParameter;
-import org.eclipse.dirigible.components.odata.api.ODataProperty;
-import org.eclipse.dirigible.components.odata.api.TableMetadataProvider;
-import org.eclipse.dirigible.components.odata.domain.OData;
-import org.eclipse.dirigible.database.sql.ISqlKeywords;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * The Class OData2ODataMTransformer.
@@ -76,7 +71,9 @@ public class OData2ODataMTransformer {
 
         for (ODataEntity entity : model.getEntities()) {
             Table tableMetadata = tableMetadataProvider.getTableMetadata(entity);
-
+            if (null == tableMetadata) {
+                throw new IllegalArgumentException("Cannot find table metadata for entity: " + entity);
+            }
             StringBuilder buff = new StringBuilder();
             buff.append("{\n")
                 .append("\t\"edmType\": \"")
@@ -163,7 +160,6 @@ public class OData2ODataMTransformer {
                     .append("],\n");
             }
 
-
             // Process FK relations from DB if they exist
             Map<String, List<TableConstraintForeignKey>> groupRelationsByToTableName = tableMetadata.getConstraints()
                                                                                                     .getForeignKeys()
@@ -205,7 +201,6 @@ public class OData2ODataMTransformer {
                 buff.append(String.join(",\n", assembleRefFromFK))
                     .append(",\n");
             }
-
 
             // Process Associations from .odata file
             List<ODataAssociation> assWhereEntityIsFROMRole = model.getAssociations()
@@ -446,7 +441,6 @@ public class OData2ODataMTransformer {
 
         return refSection;
     }
-
 
 }
 
