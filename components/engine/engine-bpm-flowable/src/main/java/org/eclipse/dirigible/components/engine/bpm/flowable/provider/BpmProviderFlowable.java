673f63678d2613d04272be8b9b39a445d8020cb6
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-07-05 12:59:51 +0300

    update logged message
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/engine/engine-bpm-flowable/src/main/java/org/eclipse/dirigible/components/engine/bpm/flowable/provider/BpmProviderFlowable.java
+++ components/engine/engine-bpm-flowable/src/main/java/org/eclipse/dirigible/components/engine/bpm/flowable/provider/BpmProviderFlowable.java
@@ -365,7 +365,7 @@ public class BpmProviderFlowable implements BpmProvider {
     }
 
     public void cleanup() {
-        logger.info("!!!! Cleaning [{}]", this.getClass());
+        logger.info("Cleaning [{}]...", this.getClass());
         processEngine.close();
     }
 
