d69b5d81f8523b23c4efe5dd3ac043309aa181a0
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-07-08 15:33:57 +0300

    fix import
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- tests/tests-framework/src/main/java/org/eclipse/dirigible/tests/util/SecurityUtil.java
+++ tests/tests-framework/src/main/java/org/eclipse/dirigible/tests/util/SecurityUtil.java
@@ -1,9 +1,18 @@
+/*
+ * Copyright (c) 2010-2024 Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: Eclipse Dirigible contributors SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.dirigible.tests.util;
 
 import org.eclipse.dirigible.components.base.tenant.DefaultTenant;
+import org.eclipse.dirigible.components.base.tenant.Tenant;
 import org.eclipse.dirigible.components.security.domain.Role;
 import org.eclipse.dirigible.components.security.service.RoleService;
-import org.eclipse.dirigible.components.tenants.domain.Tenant;
 import org.eclipse.dirigible.components.tenants.domain.User;
 import org.eclipse.dirigible.components.tenants.service.TenantService;
 import org.eclipse.dirigible.components.tenants.service.UserService;
