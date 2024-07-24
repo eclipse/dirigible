bffa3f374a97534eafa30dc5a0c0e4f8af5cf419
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-07-24 11:13:36 +0300

    ViewCreateProcessor.java - fail on error
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/data/data-structures/src/main/java/org/eclipse/dirigible/components/data/structures/synchronizer/view/ViewCreateProcessor.java
+++ components/data/data-structures/src/main/java/org/eclipse/dirigible/components/data/structures/synchronizer/view/ViewCreateProcessor.java
@@ -9,16 +9,16 @@
  */
 package org.eclipse.dirigible.components.data.structures.synchronizer.view;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 import org.eclipse.dirigible.commons.config.Configuration;
 import org.eclipse.dirigible.components.data.structures.domain.View;
 import org.eclipse.dirigible.components.database.DatabaseParameters;
 import org.eclipse.dirigible.database.sql.SqlFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * The View Create Processor.
@@ -52,19 +52,14 @@ public class ViewCreateProcessor {
                                    .view(viewName)
                                    .asSelect(viewModel.getQuery())
                                    .build();
-            if (logger.isInfoEnabled()) {
-                logger.info(sql);
-            }
+            logger.info("About to execute sql [{}]", sql);
             PreparedStatement statement = connection.prepareStatement(sql);
             try {
                 statement.executeUpdate();
-            } catch (SQLException e) {
-                if (logger.isErrorEnabled()) {
-                    logger.error(sql);
-                }
-                if (logger.isErrorEnabled()) {
-                    logger.error(e.getMessage(), e);
-                }
+            } catch (SQLException ex) {
+                String errorMessage = "Failed to create view [" + viewModel + "] using sql [" + sql + "]";
+                logger.error(errorMessage, viewModel, sql, ex);
+                throw new SQLException(errorMessage, ex);
             } finally {
                 if (statement != null) {
                     statement.close();
