c2a29b7acfaa0c345a94314480f4420367af3de6
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-07-30 15:04:27 +0300

    fix snowflake communication
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- components/data/data-source-snowpark/src/main/java/org/eclipse/dirigible/components/data/source/snowpark/DataSourceInitializerContributorSnowpark.java
+++ components/data/data-source-snowpark/src/main/java/org/eclipse/dirigible/components/data/source/snowpark/DataSourceInitializerContributorSnowpark.java
@@ -9,21 +9,23 @@
  */
 package org.eclipse.dirigible.components.data.source.snowpark;
 
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.dirigible.commons.config.Configuration;
+import org.eclipse.dirigible.components.data.sources.domain.DataSource;
+import org.eclipse.dirigible.components.data.sources.manager.DataSourceInitializerContributor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Properties;
-import org.eclipse.dirigible.components.data.sources.domain.DataSource;
-import org.eclipse.dirigible.components.data.sources.manager.DataSourceInitializerContributor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * The Class DataSourceInitializerContributorSnowpark.
  */
-@Configuration
+@org.springframework.context.annotation.Configuration
 public class DataSourceInitializerContributorSnowpark implements DataSourceInitializerContributor {
     /** The Constant logger. */
     private static final Logger logger = LoggerFactory.getLogger(DataSourceInitializerContributorSnowpark.class);
@@ -38,37 +40,46 @@ public class DataSourceInitializerContributorSnowpark implements DataSourceIniti
     public void contribute(DataSource dataSource, Properties properties) {
         if (!dataSource.getName()
                        .startsWith("SNOWFLAKE")) {
+            logger.info("[{}] will NOT contribute to datasource [{}]", this, dataSource.getName());
             return;
         }
         Map<String, String> env = System.getenv();
         try {
             String url;
+            properties.put("dataSource.CLIENT_SESSION_KEEP_ALIVE", true);
+            setPropertyIfExists("SNOWFLAKE_ACCOUNT", "dataSource.account", properties);
+            setPropertyIfExists("SNOWFLAKE_WAREHOUSE", "dataSource.warehouse", properties);
+            setPropertyIfExists("SNOWFLAKE_DATABASE", "dataSource.db", properties);
+            setPropertyIfExists("SNOWFLAKE_SCHEMA", "dataSource.schema", properties);
+            setPropertyIfExists("SNOWFLAKE_ROLE", "dataSource.role", properties);
+
             if (Files.exists(Paths.get("/snowflake/session/token"))) {
-                properties.put("dataSource.CLIENT_SESSION_KEEP_ALIVE", true);
-                properties.put("dataSource.account", env.get("SNOWFLAKE_ACCOUNT"));
                 properties.put("dataSource.authenticator", "OAUTH");
                 properties.put("dataSource.token", new String(Files.readAllBytes(Paths.get("/snowflake/session/token"))));
-                properties.put("dataSource.warehouse", env.getOrDefault("SNOWFLAKE_WAREHOUSE", ""));
-                properties.put("dataSource.db", env.get("SNOWFLAKE_DATABASE"));
-                properties.put("dataSource.schema", env.get("SNOWFLAKE_SCHEMA"));
                 properties.put("dataSource.insecureMode", true);
-                url = "jdbc:snowflake://" + env.get("SNOWFLAKE_HOST") + ":" + env.get("SNOWFLAKE_PORT");
-                properties.put("jdbcUrl", url);
-                properties.put("dataSource.url", url);
+                url = "jdbc:snowflake://" + Configuration.get("SNOWFLAKE_HOST") + ":" + Configuration.get("SNOWFLAKE_PORT");
             } else {
-                properties.put("dataSource.CLIENT_SESSION_KEEP_ALIVE", true);
-                properties.put("dataSource.account", env.getOrDefault("SNOWFLAKE_ACCOUNT", ""));
-                properties.put("dataSource.user", env.getOrDefault("SNOWFLAKE_USERNAME", dataSource.getUsername()));
-                properties.put("dataSource.password", env.getOrDefault("SNOWFLAKE_PASSWORD", dataSource.getPassword()));
-                properties.put("dataSource.warehouse", env.getOrDefault("SNOWFLAKE_WAREHOUSE", ""));
-                properties.put("dataSource.db", env.getOrDefault("SNOWFLAKE_DATABASE", ""));
-                properties.put("dataSource.schema", env.getOrDefault("SNOWFLAKE_SCHEMA", ""));
+                setPropertyIfExists("SNOWFLAKE_USERNAME", "dataSource.user", properties);
+                setPropertyIfExists("SNOWFLAKE_PASSWORD", "dataSource.password", properties);
+
                 url = env.getOrDefault("SNOWFLAKE_URL", dataSource.getUrl());
-                properties.put("jdbcUrl", url);
-                properties.put("dataSource.url", url);
             }
+
+            properties.put("jdbcUrl", url);
+            properties.put("dataSource.url", url);
+
         } catch (IOException ex) {
             logger.error("Invalid configuration for the datasource: [{}]", dataSource.getName(), ex);
+        }
+    }
+
+    private void setPropertyIfExists(String configName, String propertyName, Properties properties) {
+        String value = Configuration.get(configName);
+        if (StringUtils.isNotBlank(value)) {
+            logger.debug("Setting property [{}] from config [{}]", propertyName, configName);
+            properties.put(propertyName, value);
+        } else {
+            logger.debug("Will NOT set property [{}] since config [{}] value is [{}]", propertyName, configName, value);
         }
     }
 }
