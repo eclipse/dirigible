080a6d7e14f3f6aded7d8c131da869d9f2199d54
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-07-31 16:14:12 +0300

    fix
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- tests/tests-integrations/src/test/resources/META-INF/dirigible/modules-tests/src/security/security-test.ts
+++ tests/tests-integrations/src/test/resources/META-INF/dirigible/modules-tests/src/security/security-test.ts
@@ -2,5 +2,5 @@ import { user } from "sdk/security";
 import { assertEquals } from "sdk/junit"
 
 test('get-user-test', () => {
-	assertEquals('Unexpected user' user.getName(), 'guest');
+	assertEquals('Unexpected user', user.getName(), 'guest');
 });
