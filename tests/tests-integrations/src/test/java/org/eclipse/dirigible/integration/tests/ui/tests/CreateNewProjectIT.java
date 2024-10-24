e4002dd34f403ad8f8a5c84c4109a3a87a7a46ce
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-10-24 11:21:05 +0300

    cleanup
    Signed-off-by: Iliyan Velichkov <velichkov.iliyan@gmail.com>

--- tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/CreateNewProjectIT.java
+++ tests/tests-integrations/src/test/java/org/eclipse/dirigible/integration/tests/ui/tests/CreateNewProjectIT.java
@@ -13,8 +13,6 @@ import org.junit.jupiter.api.Test;
 
 class CreateNewProjectIT extends UserInterfaceIntegrationTest {
 
-    private static final String ECLIPSE_DIRIGIBLE_HEADER = "Eclipse Dirigible";
-
     @Test
     void testCreateNewBlankProject() {
         ide.createNewBlankProject("create-project-ui-test");
