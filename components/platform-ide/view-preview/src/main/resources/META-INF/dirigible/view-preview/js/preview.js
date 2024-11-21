56999b3dfbda25d2e91c2b4daaa411a430fa94b8
StanZGenchev <stan.z.genchev@gmail.com>
2024-11-21 14:13:02 +0200

    Migrated console, preview, properties and added a classic theme
    Signed-off-by: StanZGenchev <stan.z.genchev@gmail.com>

---
+++ components/platform-ide/view-preview/src/main/resources/META-INF/dirigible/view-preview/js/preview.js
@@ -0,0 +1,288 @@
+/*
+ * Copyright (c) 2024 Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: Eclipse Dirigible contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+const previewView = angular.module('preview', ['blimpKit', 'platformView']);
+previewView.controller('PreviewController', ($scope, ViewParameters, ButtonStates) => {
+    const defaultParameters = {
+        'container': 'layout',
+        'perspectiveId': 'workbench'
+    };
+    const workspaceApi = new WorkspaceApi();
+    const dialogApi = new DialogApi();
+    let customParamsId = `${brandingInfo.keyPrefix}.preview.customParameters`;
+    let urlLockedId = `${brandingInfo.keyPrefix}.preview.urlLocked`;
+    let inDebugPerspective = false;
+    $scope.dataParameters = ViewParameters.get();
+    if ($scope.dataParameters.hasOwnProperty('perspectiveId') && $scope.dataParameters.perspectiveId === 'debugger') {
+        inDebugPerspective = true;
+        customParamsId = `${brandingInfo.keyPrefix}.debugger.preview.customParameters`;
+        urlLockedId = `${brandingInfo.keyPrefix}.debugger.preview.urlLocked`;
+    }
+
+    $scope.urlLocked = localStorage.getItem(urlLockedId) === 'true';
+
+    function initCustomParameters() {
+        const saved = localStorage.getItem(customParamsId);
+        if (saved) {
+            $scope.customParameters = JSON.parse(saved);
+        } else {
+            $scope.customParameters = defaultParameters;
+            localStorage.setItem(customParamsId, JSON.stringify(defaultParameters));
+        }
+    }
+
+    initCustomParameters();
+
+    $scope.iframe = document.getElementById('preview-iframe');
+    $scope.history = {
+        idx: -1,
+        state: []
+    };
+
+    $scope.urlLockedToggle = () => {
+        $scope.urlLocked = !$scope.urlLocked;
+        localStorage.setItem(urlLockedId, `${$scope.urlLocked}`);
+    };
+
+    $scope.reload = () => {
+        let iframeDocument = $scope.iframe.contentDocument || $scope.contentWindow.document;
+        if (iframeDocument) {
+            iframeDocument.location.reload(true);
+        }
+    };
+
+    $scope.getParams = () => JSON.stringify($scope.customParameters);
+
+    $scope.getCurrentUrl = () => $scope.history.state[$scope.history.idx];
+
+    $scope.hasBack = () => $scope.history.idx > 0;
+
+    $scope.hasForward = () => $scope.history.idx < $scope.history.state.length - 1;
+
+    $scope.goBack = () => {
+        if ($scope.hasBack()) {
+            const url = $scope.history.state[--$scope.history.idx];
+            $scope.replaceLocationUrl(url);
+        }
+    };
+
+    $scope.goForward = () => {
+        if ($scope.hasForward()) {
+            const url = $scope.history.state[++$scope.history.idx];
+            $scope.replaceLocationUrl(url);
+        }
+    };
+
+    $scope.gotoUrl = (url, shouldReload = true) => {
+        const currentUrl = $scope.getCurrentUrl();
+        if (currentUrl && currentUrl === url) {
+            if (shouldReload)
+                $scope.reload();
+            return;
+        };
+
+        if ($scope.history.idx >= 0)
+            $scope.history.state.length = $scope.history.idx + 1;
+
+        $scope.history.state.push(url);
+        $scope.history.idx++;
+
+        $scope.replaceLocationUrl(url);
+    };
+
+    $scope.replaceLocationUrl = (url) => {
+        $scope.previewUrl = url;
+        $scope.iframe.contentWindow.location.replace(url);
+    };
+
+    $scope.inputUrlKeyUp = (e) => {
+        switch (e.key) {
+            case 'Escape': // cancel url edit
+                const currentUrl = $scope.getCurrentUrl();
+                $scope.previewUrl = currentUrl || '';
+                break;
+            case 'Enter':
+                $scope.previewUrl = e.target.value;
+                if ($scope.previewUrl) {
+                    $scope.gotoUrl($scope.previewUrl);
+                }
+                break;
+        }
+    };
+
+    $scope.makeUrlFromPath = (resourcePath) => {
+        let url = window.location.protocol + '//' + window.location.host + window.location.pathname.substring(window.location.pathname.indexOf('/web/'), 0);
+        const type = resourcePath.substring(resourcePath.lastIndexOf('.') + 1);
+        const isOData = resourcePath.endsWith('.odata');
+        const isOpenAPI = resourcePath.endsWith('.openapi');
+        if (isOData) {
+            url = window.location.protocol + '//' + window.location.host + '/odata/v2/';
+        } else if (isOpenAPI) {
+            url = `${window.location.protocol}//${window.location.host}/services/web/ide-swagger/ui/index.html?openapi=/services/web${resourcePath}`;
+        } else {
+            switch (type) {
+                case 'rhino':
+                    url += '/rhino';
+                    break;
+                case 'nashorn':
+                    url += '/nashorn';
+                    break;
+                case 'v8':
+                    url += '/v8';
+                    break;
+                case 'graalvm':
+                    url += '/graalvm';
+                    break;
+                case 'ts':
+                    url += '/ts';
+                    break;
+                case 'js':
+                    url += '/js';
+                    break;
+                case 'mjs':
+                    url += '/js';
+                    break;
+                case 'ts':
+                    url += '/js';
+                    break;
+                case 'xsjs':
+                    url += '/xsjs';
+                    break;
+                case 'py':
+                    url += '/py';
+                    break;
+                case 'md':
+                    url += '/wiki';
+                    break;
+                case 'markdown':
+                    url += '/wiki';
+                    break;
+                case 'confluence':
+                    url += '/wiki';
+                    break;
+                case 'command':
+                    url += '/command';
+                    break;
+                case 'xsodata':
+                    url += '/web';
+                    break;
+                case 'edm':
+                case 'dsm':
+                case 'bpmn':
+                case 'job':
+                case 'xsjob':
+                case 'calculationview':
+                case 'websocket':
+                case 'hdi':
+                case 'hdbtable':
+                case 'hdbstructurе':
+                case 'hdbview':
+                case 'hdbtablefunction':
+                case 'hdbprocedure':
+                case 'hdbschema':
+                case 'hdbsynonym':
+                case 'hdbdd':
+                case 'hdbsequence':
+                case 'hdbcalculationview':
+                case 'xsaccess':
+                case 'xsprivileges':
+                case 'xshttpdest':
+                case 'listener':
+                case 'extensionpoint':
+                case 'extension':
+                case 'table':
+                case 'view':
+                case 'access':
+                case 'roles':
+                case 'sh':
+                case 'csv':
+                case 'csvim':
+                case 'hdbti':
+                case 'camel':
+                case 'form':
+                case 'report':
+                    return;
+                default:
+                    url += '/web';
+            }
+            url += resourcePath;
+        }
+        if (inDebugPerspective) url += '?debug=true';
+        return url;
+    };
+
+    $scope.customizeParameters = (custom) => {
+        dialogApi.showFormDialog({
+            title: 'Set custom ViewParameters',
+            subheader: 'Data will be accessible through the "ViewParameters.get()" function.',
+            form: {
+                'cvp': {
+                    label: 'JSON data',
+                    controlType: 'textarea',
+                    placeholder: defaultParameters,
+                    value: custom ?? JSON.stringify($scope.customParameters, null, 2),
+                    rows: 10,
+                    focus: true,
+                },
+            },
+            submitLabel: 'Apply',
+            cancelLabel: 'Cancel'
+        }).then((form) => {
+            if (form) {
+                try {
+                    const customData = JSON.parse(form['cvp']);
+                    localStorage.setItem(customParamsId, JSON.stringify(customData));
+                    $scope.$evalAsync(() => {
+                        $scope.customParameters = customData;
+                        $scope.reload();
+                    });
+                } catch (error) {
+                    dialogApi.showAlert({
+                        title: 'Invalid input',
+                        message: 'Not a valid JSON',
+                        type: AlertTypes.Error,
+                        preformatted: false,
+                        buttons: [
+                            { id: 'edit', label: 'Edit', state: ButtonStates.Emphasized },
+                            { id: 'close', label: 'Close' }
+                        ]
+                    }).then((buttonId) => {
+                        if (buttonId === 'edit') $scope.customizeParameters(form['cvp']);
+                    });
+                }
+            }
+        });
+    };
+
+    workspaceApi.onFileSelected((fileDescriptor) => {
+        if ($scope.urlLocked) return;
+        const url = $scope.makeUrlFromPath(fileDescriptor.path);
+        if (url) $scope.$evalAsync(() => { $scope.gotoUrl(url, false) });
+    });
+
+    workspaceApi.onPublished((fileDescriptor) => {
+        if ($scope.urlLocked) return;
+
+        if (fileDescriptor.path) {
+            const url = $scope.makeUrlFromPath(fileDescriptor.path);
+            if (url) $scope.$evalAsync(() => { $scope.gotoUrl(url) });
+        } else $scope.$evalAsync(() => { $scope.reload() });
+    });
+
+    workspaceApi.onUnpublished((fileDescriptor) => {
+        if ($scope.urlLocked) return;
+
+        if (fileDescriptor.path) {
+            const url = $scope.makeUrlFromPath(fileDescriptor.path);
+            if (url) $scope.$evalAsync(() => { $scope.gotoUrl(url) });
+        } else $scope.$evalAsync(() => { $scope.reload() });
+    });
+});
