cffebc9f19e144fbdf9f66ca2e4064793a110388
MrGoblings <mrgoblings@gmail.com>
2024-12-04 08:52:08 +0000

    chore: logging enhanced

--- components/resources/resources-portal/src/main/resources/META-INF/dirigible/portal/api/NavigationExtension/NavigationService.js
+++ components/resources/resources-portal/src/main/resources/META-INF/dirigible/portal/api/NavigationExtension/NavigationService.js
@@ -15,7 +15,7 @@ for (let i = 0; i < navigationExtensions.length; i++) {
         const navigation = getNavigation();
         navigationList.push(navigation);
     } catch (err) {
-        console.error(`${err}\npath: ${path}`);
+        console.error(`NavigationService: ${err}\npath: ${path}`);
     }
 }
 
