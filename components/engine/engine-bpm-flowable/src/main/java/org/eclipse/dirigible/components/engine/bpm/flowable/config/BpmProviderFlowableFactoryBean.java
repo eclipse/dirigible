215e0f05033cf9f49cc0d175fc7e707bdef1538b
delchev <delchevn@gmail.com>
2024-12-06 17:44:51 +0200

    licenses and javadocs

--- components/engine/engine-bpm-flowable/src/main/java/org/eclipse/dirigible/components/engine/bpm/flowable/config/BpmProviderFlowableFactoryBean.java
+++ components/engine/engine-bpm-flowable/src/main/java/org/eclipse/dirigible/components/engine/bpm/flowable/config/BpmProviderFlowableFactoryBean.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2010-2024 Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: Eclipse Dirigible contributors SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.dirigible.components.engine.bpm.flowable.config;
 
 import org.eclipse.dirigible.components.engine.bpm.flowable.provider.BpmProviderFlowable;
@@ -13,14 +22,24 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
+/**
+ * The Class BpmProviderFlowableFactoryBean.
+ */
 @Component
 class BpmProviderFlowableFactoryBean implements FactoryBean<BpmProviderFlowable>, DisposableBean, ApplicationContextAware {
 
+    /** The Constant LOGGER. */
     private static final Logger LOGGER = LoggerFactory.getLogger(BpmProviderFlowableFactoryBean.class);
 
+    /** The bpm provider flowable. */
     private BpmProviderFlowable bpmProviderFlowable;
+    
+    /** The application context. */
     private ApplicationContext applicationContext;
 
+    /**
+     * Destroy.
+     */
     @Override
     public void destroy() {
         LOGGER.info("Destroying bean...");
@@ -30,16 +49,31 @@ class BpmProviderFlowableFactoryBean implements FactoryBean<BpmProviderFlowable>
         }
     }
 
+    /**
+     * Sets the application context.
+     *
+     * @param applicationContext the new application context
+     */
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
     }
 
+    /**
+     * Checks if is singleton.
+     *
+     * @return true, if is singleton
+     */
     @Override
     public boolean isSingleton() {
         return true;
     }
 
+    /**
+     * Gets the object.
+     *
+     * @return the object
+     */
     @Override
     public synchronized BpmProviderFlowable getObject() {
         if (null == bpmProviderFlowable) {
@@ -52,6 +86,11 @@ class BpmProviderFlowableFactoryBean implements FactoryBean<BpmProviderFlowable>
         return bpmProviderFlowable;
     }
 
+    /**
+     * Gets the object type.
+     *
+     * @return the object type
+     */
     @Override
     public Class<BpmProviderFlowable> getObjectType() {
         return BpmProviderFlowable.class;
--- components/engine/engine-jobs/src/main/java/org/eclipse/dirigible/components/jobs/telemetry/JobExecutionsDurationListener.java
+++ components/engine/engine-jobs/src/main/java/org/eclipse/dirigible/components/jobs/telemetry/JobExecutionsDurationListener.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2010-2024 Eclipse Dirigible contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: Eclipse Dirigible contributors SPDX-License-Identifier: EPL-2.0
+ */
 package org.eclipse.dirigible.components.jobs.telemetry;
 
 import io.opentelemetry.api.OpenTelemetry;
@@ -12,13 +21,31 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 import java.time.Instant;
 
+/**
+ * The listener interface for receiving jobExecutionsDuration events.
+ * The class that is interested in processing a jobExecutionsDuration
+ * event implements this interface, and the object created
+ * with that class is registered with a component using the
+ * component's <code>addJobExecutionsDurationListener<code> method. When
+ * the jobExecutionsDuration event occurs, that object's appropriate
+ * method is invoked.
+ *
+ * @see JobExecutionsDurationEvent
+ */
 @Component
 public class JobExecutionsDurationListener extends OpenTelemetryListener {
 
+    /** The Constant START_TIME_KEY. */
     private static final String START_TIME_KEY = "startTime";
 
+    /** The histogram. */
     private final LongHistogram histogram;
 
+    /**
+     * Instantiates a new job executions duration listener.
+     *
+     * @param openTelemetry the open telemetry
+     */
     JobExecutionsDurationListener(OpenTelemetry openTelemetry) {
         this.histogram = openTelemetry.getMeter(QuartzMetricConstants.METER_SCOPE_NAME)
                                       .histogramBuilder("quartz_job_execution_time")
@@ -27,11 +54,22 @@ public class JobExecutionsDurationListener extends OpenTelemetryListener {
                                       .build();
     }
 
+    /**
+     * Job to be executed.
+     *
+     * @param context the context
+     */
     @Override
     public void jobToBeExecuted(JobExecutionContext context) {
         context.put(START_TIME_KEY, Instant.now());
     }
 
+    /**
+     * Job was executed.
+     *
+     * @param context the context
+     * @param jobException the job exception
+     */
     @Override
     public void jobWasExecuted(JobExecutionContext context, JobExecutionException jobException) {
         Instant startTime = (Instant) context.get(START_TIME_KEY);
